3e1cff127a14f5260af71df13f9a0e980f94cabd
kuqmua <serezhashmakov@outlook.com>
2022-04-19 18:52:10 +0300

    get_commit_link

---
+++ src/helpers/git/get_commit_link.rs
@@ -0,0 +1,14 @@
+use crate::helpers::git::lazy_static_git_info::GIT_INFO;
+
+#[deny(
+    clippy::indexing_slicing,
+    clippy::unwrap_used,
+    clippy::integer_arithmetic,
+    clippy::float_arithmetic
+)]
+pub fn get_commit_link() -> String {
+    format!(
+        "https://{}/blob/{}/",
+        GIT_INFO.repo_link, GIT_INFO.commit_id
+    )
+}
