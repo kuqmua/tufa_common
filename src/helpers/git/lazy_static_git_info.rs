43f425f67cfc5e5d47f512a25f3c1db6105d616f
kuqmua <serezhashmakov@outlook.com>
2022-04-20 22:17:59 +0300

    remove lazy static

--- src/helpers/git/lazy_static_git_info.rs
+++
@@ -1,6 +0,0 @@
-use crate::helpers::git::git_info::GitInformation;
-use lazy_static::lazy_static;
-
-lazy_static! {
-    pub static ref GIT_INFO: GitInformation = GitInformation::get_git_commit_info("../");
-}
