bf9a9706895f2326ac10ea295899f7180a8578fa
kuqmua <serezhashmakov@outlook.com>
2022-11-05 19:48:17 +0300

    tufa_common::server::http_request::

--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -1,10 +1,7 @@
 pub mod create_dir_if_it_doesnt_exist;
 pub mod git;
 pub mod handle_status_code;
-pub mod http_request;
-pub mod mongo;
 pub mod numeric;
-pub mod postgres;
 
 pub mod resource;
 pub mod write_bytes_into_file_async_tokio;
--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,3 +1,6 @@
 pub mod file_system;
+pub mod http_request;
+pub mod mongo;
+pub mod postgres;
 pub mod prints;
 pub mod redis;
