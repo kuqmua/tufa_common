e072f7abfccb76b3c8380a939fc6a8a35284023d
kuqmua <serezhashmakov@outlook.com>
2022-11-06 19:03:44 +0300

    another move

--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -1,3 +1,1 @@
-pub mod create_dir_if_it_doesnt_exist;
 pub mod git;
-pub mod handle_status_code;
---
+++ src/server/file_system/create_dir_if_it_doesnt_exist.rs
@@ -0,0 +1,56 @@
+use crate::lazy_static::config::CONFIG;
+use crate::lazy_static::git_info::GIT_INFO;
+use crate::traits::init_error_with_possible_trace::InitErrorWithPossibleTrace;
+use crate::traits::where_was_trait::WhereWasTrait;
+use crate::where_was::WhereWas;
+use impl_display_for_error_struct::ImplDisplayForErrorStruct;
+use impl_error_with_tracing_for_struct_without_get_source::ImplErrorWithTracingForStructWithoutGetSourceFromCrate;
+use impl_get_source_without_method::ImplGetSourceWithoutMethodFromCrate;
+use impl_get_where_was_one_or_many_one_for_error_struct::ImplGetWhereWasOneOrManyOneForErrorStructFromCrate;
+use init_error::InitErrorFromCrate;
+
+#[derive(
+    Debug,
+    InitErrorFromCrate,
+    ImplErrorWithTracingForStructWithoutGetSourceFromCrate,
+    ImplGetWhereWasOneOrManyOneForErrorStructFromCrate,
+    ImplGetSourceWithoutMethodFromCrate,
+    ImplDisplayForErrorStruct,
+)]
+pub struct CreateDirIfItDoesntExistError {
+    pub source: std::io::Error,
+    pub where_was: WhereWas,
+}
+
+#[deny(
+    clippy::indexing_slicing,
+    clippy::unwrap_used,
+    clippy::integer_arithmetic,
+    clippy::float_arithmetic
+)]
+pub fn create_dir_if_it_doesnt_exist(
+    path: &str,
+    source_place_type: &crate::config_mods::source_place_type::SourcePlaceType,
+    should_trace: bool,
+) -> Result<(), Box<CreateDirIfItDoesntExistError>> {
+    if std::path::Path::new(path).exists() {
+        return Ok(());
+    }
+    if let Err(e) = std::fs::create_dir_all(path) {
+        return Err(Box::new(
+            CreateDirIfItDoesntExistError::init_error_with_possible_trace(
+                e,
+                WhereWas {
+                    time: std::time::SystemTime::now()
+                        .duration_since(std::time::UNIX_EPOCH)
+                        .expect("cannot convert time to unix_epoch"),
+                    location: *core::panic::Location::caller(),
+                },
+                source_place_type,
+                &GIT_INFO.data,
+                should_trace,
+            ),
+        ));
+    }
+    Ok(())
+}
