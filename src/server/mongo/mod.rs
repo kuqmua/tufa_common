f0403bc16c2f120481f1f6239d28c7bdd5173083
kuqmua <serezhashmakov@outlook.com>
2022-11-12 22:34:05 +0300

    move mongo_check_availability to tufa_common

--- src/server/mongo/mod.rs
+++ src/server/mongo/mod.rs
@@ -1,1 +1,1 @@
-
+pub mod mongo_check_availability;
---
+++ src/server/mongo/mongo_check_availability.rs
@@ -0,0 +1,109 @@
+use crate::common::where_was::WhereWas;
+use crate::global_variables::compile_time::git_info::GIT_INFO;
+use crate::global_variables::runtime::config::CONFIG;
+use crate::traits::get_source::GetSource;
+use crate::traits::init_error_with_possible_trace::InitErrorWithPossibleTrace;
+use crate::traits::where_was_trait::WhereWasTrait;
+use impl_display_for_error_struct::ImplDisplayForErrorStruct;
+use impl_display_for_simple_error_enum::ImplDisplayForSimpleErrorEnum;
+use impl_error_with_tracing_for_struct_with_get_source_without_get_where_was::ImplErrorWithTracingForStructWithGetSourceWithoutGetWhereWasFromCrate;
+use impl_get_source_with_method::ImplGetSourceWithMethodFromCrate;
+use impl_get_source_without_method::ImplGetSourceWithoutMethodFromCrate;
+use impl_get_where_was_one_or_many_one_for_error_struct::ImplGetWhereWasOneOrManyOneForErrorStructFromCrate;
+use init_error::InitErrorFromCrate;
+use mongodb::options::ClientOptions;
+use mongodb::Client;
+use std::time::Duration;
+
+#[derive(
+    Debug,
+    ImplGetSourceWithMethodFromCrate,
+    ImplDisplayForErrorStruct,
+    InitErrorFromCrate,
+    ImplErrorWithTracingForStructWithGetSourceWithoutGetWhereWasFromCrate,
+    ImplGetWhereWasOneOrManyOneForErrorStructFromCrate,
+)]
+pub struct MongoCheckAvailabilityError {
+    source: MongoCheckAvailabilityErrorEnum,
+    where_was: WhereWas,
+}
+
+#[derive(Debug, ImplGetSourceWithoutMethodFromCrate, ImplDisplayForSimpleErrorEnum)]
+pub enum MongoCheckAvailabilityErrorEnum {
+    ClientOptionsParse(mongodb::error::Error),
+    ClientWithOptions(mongodb::error::Error),
+    ListCollectionNames(mongodb::error::Error),
+}
+
+#[deny(
+    clippy::indexing_slicing,
+    clippy::unwrap_used,
+    clippy::integer_arithmetic,
+    clippy::float_arithmetic
+)]
+pub async fn mongo_check_availability(
+    mongo_url: &str,
+    should_trace: bool,
+) -> Result<(), Box<MongoCheckAvailabilityError>> {
+    match ClientOptions::parse(mongo_url).await {
+        Err(e) => {
+            return Err(Box::new(
+                MongoCheckAvailabilityError::init_error_with_possible_trace(
+                    MongoCheckAvailabilityErrorEnum::ClientOptionsParse(e),
+                    WhereWas {
+                        time: std::time::SystemTime::now()
+                            .duration_since(std::time::UNIX_EPOCH)
+                            .expect("cannot convert time to unix_epoch"),
+                        location: *core::panic::Location::caller(),
+                    },
+                    &CONFIG.source_place_type,
+                    &GIT_INFO,
+                    should_trace,
+                ),
+            ));
+        }
+        Ok(mut client_options) => {
+            client_options.connect_timeout =
+                Some(Duration::from_millis(CONFIG.mongo_connection_timeout));
+            match Client::with_options(client_options) {
+                Err(e) => Err(Box::new(
+                    MongoCheckAvailabilityError::init_error_with_possible_trace(
+                        MongoCheckAvailabilityErrorEnum::ClientWithOptions(e),
+                        WhereWas {
+                            time: std::time::SystemTime::now()
+                                .duration_since(std::time::UNIX_EPOCH)
+                                .expect("cannot convert time to unix_epoch"),
+                            location: *core::panic::Location::caller(),
+                        },
+                        &CONFIG.source_place_type,
+                        &GIT_INFO,
+                        should_trace,
+                    ),
+                )),
+                Ok(client) => {
+                    if let Err(e) = client
+                        .database(&CONFIG.mongo_providers_logs_db_name)
+                        .list_collection_names(None)
+                        .await
+                    {
+                        return Err(Box::new(
+                            MongoCheckAvailabilityError::init_error_with_possible_trace(
+                                MongoCheckAvailabilityErrorEnum::ListCollectionNames(e),
+                                WhereWas {
+                                    time: std::time::SystemTime::now()
+                                        .duration_since(std::time::UNIX_EPOCH)
+                                        .expect("cannot convert time to unix_epoch"),
+                                    location: *core::panic::Location::caller(),
+                                },
+                                &CONFIG.source_place_type,
+                                &GIT_INFO,
+                                should_trace,
+                            ),
+                        ));
+                    }
+                    Ok(())
+                }
+            }
+        }
+    }
+}
