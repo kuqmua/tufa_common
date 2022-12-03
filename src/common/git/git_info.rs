babb5fbe01761a8767940954c02ad29606e45241
kuqmua <serezhashmakov@outlook.com>
2022-12-03 13:27:51 +0300

    GitInformationWithoutLifetimes

--- src/common/git/git_info.rs
+++ src/common/git/git_info.rs
@@ -11,3 +11,28 @@ pub struct GitInformation<'a> {
     pub timezone: &'a str,
     pub message: &'a str,
 }
+
+impl GitInformation<'static> {
+    pub fn get_runtime_version(&self) -> GitInformationWithoutLifetimes {
+        GitInformationWithoutLifetimes {
+            commit_id: String::from(self.commit_id),
+            repo_link: String::from(self.repo_link),
+            author: String::from(self.author),
+            author_email: String::from(self.author_email),
+            commit_unix_time: String::from(self.commit_unix_time),
+            timezone: String::from(self.timezone),
+            message: String::from(self.message),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Eq, Hash, PartialEq)]
+pub struct GitInformationWithoutLifetimes {
+    pub commit_id: String,
+    pub repo_link: String,
+    pub author: String,
+    pub author_email: String,
+    pub commit_unix_time: String,
+    pub timezone: String,
+    pub message: String,
+}
