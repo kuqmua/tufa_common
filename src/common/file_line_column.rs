fd7f7272ed81ee4f9fa4f6406d093c9acdec0354
kuqmua <serezhashmakov@outlook.com>
2022-12-14 00:04:56 +0300

    GenerateGetterTraitsForStructFieldsFromCrate usage for FileLineColumn

--- src/common/file_line_column.rs
+++ src/common/file_line_column.rs
@@ -1,26 +1,8 @@
-// use generate_getter_traits_for_struct_fields::GenerateGetterTraitsForStructFieldsFromCrate;
+use generate_getter_traits_for_struct_fields::GenerateGetterTraitsForStructFieldsFromCrate;
 
-#[derive(Debug, Clone)] //todo fields implementations
+#[derive(Debug, Clone, GenerateGetterTraitsForStructFieldsFromCrate)] //todo fields implementations
 pub struct FileLineColumn {
     pub file: String, //&'a str
     pub line: u32,
     pub column: u32,
-}
-
-impl crate::traits::fields::GetFile for FileLineColumn {
-    fn get_file(&self) -> &String {
-        &self.file
-    }
-}
-
-impl crate::traits::fields::GetLine for FileLineColumn {
-    fn get_line(&self) -> &u32 {
-        &self.line
-    }
-}
-
-impl crate::traits::fields::GetColumn for FileLineColumn {
-    fn get_column(&self) -> &u32 {
-        &self.column
-    }
 }
