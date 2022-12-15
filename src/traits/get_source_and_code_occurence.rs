dd39ae087dd84f5d104e576ab97ecc568f9e6ee3
kuqmua <serezhashmakov@outlook.com>
2022-12-15 20:46:25 +0300

    impl<SelfGeneric> GetSourceAndCodeOccurence for SelfGeneric

--- src/traits/get_source_and_code_occurence.rs
+++ src/traits/get_source_and_code_occurence.rs
@@ -1,5 +1,20 @@
 pub trait GetSourceAndCodeOccurence {
     fn get_code_occurence_with_source(
         &self,
-    ) -> &crate::common::source_and_code_occurence::SourceAndCodeOccurence;
+    ) -> crate::common::source_and_code_occurence::SourceAndCodeOccurence;
+}
+
+impl<SelfGeneric> GetSourceAndCodeOccurence for SelfGeneric
+where
+    Self:
+        crate::traits::get_source::GetSource + crate::traits::get_code_occurence::GetCodeOccurence,
+{
+    fn get_code_occurence_with_source(
+        &self,
+    ) -> crate::common::source_and_code_occurence::SourceAndCodeOccurence {
+        crate::common::source_and_code_occurence::SourceAndCodeOccurence {
+            source: self.get_source(),
+            code_occurence: self.get_code_occurence().clone(), //todo
+        }
+    }
 }
