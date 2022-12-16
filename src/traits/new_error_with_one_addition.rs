8591c10e2a14c2c8b73d59287b800569e6be3109
kuqmua <serezhashmakov@outlook.com>
2022-12-16 21:37:53 +0300

    impl<HashMapKeyGeneric, HashMapValueGeneric, ConfigGeneric>     PrepareLogSourceAndCodeOccurence<ConfigGeneric>     for std::collections::HashMap<HashMapKeyGeneric, HashMapValueGeneric>

--- src/traits/new_error_with_one_addition.rs
+++ src/traits/new_error_with_one_addition.rs
@@ -1,3 +1,4 @@
+use crate::common::code_occurence;
 use crate::traits::code_occurence_methods::CodeOccurenceLog;
 use crate::traits::code_occurence_methods::CodeOccurenceNewErrorWithOneAddition;
 
@@ -44,3 +45,37 @@ where
         error
     }
 }
+// //
+// impl<SourceGeneric, ConfigGeneric, ReturnSelfGeneric>
+//     NewErrorWithOneAddition<SourceGeneric, ConfigGeneric, ReturnSelfGeneric> for ReturnSelfGeneric
+// where
+//     SourceGeneric:
+//         crate::traits::get_source::GetSource + crate::traits::get_code_occurence::GetCodeOccurence,
+//     ReturnSelfGeneric: crate::traits::init_error::InitError<SourceGeneric>
+//         + crate::traits::log_error_code_occurence::LogErrorCodeOccurence<ConfigGeneric>,
+//     ConfigGeneric: crate::traits::fields::GetSourcePlaceType
+//         + crate::traits::fields::GetLogType
+//         + crate::traits::get_color::ErrorColorBold,
+// {
+//     fn new_error_with_one_addition(
+//         source: HashMap<String, SourceGeneric>,
+//         config: ConfigGeneric,
+//         git_info: &crate::common::git::git_info::GitInformationWithoutLifetimes,
+//         file: String,
+//         line: u32,
+//         column: u32,
+//         should_trace: bool,
+//     ) -> ReturnSelfGeneric {
+//         // let code_occurence =
+//         //     crate::common::code_occurence::CodeOccurence::new_error_with_one_addition(
+//         //         git_info, file, line, column, &source,
+//         //     );
+//         let code_occurence =
+//             crate::common::code_occurence::CodeOccurence::new(git_info, file, line, column);
+//         let error = ReturnSelfGeneric::init_error(source, code_occurence);
+//         if let true = should_trace {
+//             error.log_error_code_occurence(config);
+//         }
+//         error
+//     }
+// }
--- src/traits/prepare_log_source_and_code_occurence.rs
+++ src/traits/prepare_log_source_and_code_occurence.rs
@@ -78,3 +78,83 @@ where
         occurence
     }
 }
+
+impl<HashMapKeyGeneric, HashMapValueGeneric, ConfigGeneric>
+    PrepareLogSourceAndCodeOccurence<ConfigGeneric>
+    for std::collections::HashMap<HashMapKeyGeneric, HashMapValueGeneric>
+where
+    ConfigGeneric: crate::traits::fields::GetSourcePlaceType + crate::traits::fields::GetLogType,
+    HashMapKeyGeneric: std::fmt::Display,
+    HashMapValueGeneric:
+        crate::traits::get_source::GetSource + crate::traits::get_code_occurence::GetCodeOccurence,
+{
+    fn prepare_log_source_and_code_occurence(
+        &self,
+        config_generic: ConfigGeneric,
+        key_option: Option<String>,
+    ) -> String {
+        let log_type = config_generic.get_log_type();
+        let mut prepared_log_handle =
+            self.iter()
+                .fold(String::from(""), |mut acc, (hashmap_key, hashmap_value)| {
+                    let code_occurence = hashmap_value.get_code_occurence();
+                    let capacity = code_occurence.occurences.values().fold(0, |mut acc, elem| {
+                        acc += elem.len();
+                        acc
+                    });
+                    let mut vec: Vec<crate::common::code_occurence::OccurenceFilter> =
+                        Vec::with_capacity(capacity);
+                    code_occurence.occurences.iter().for_each(|(git_info, v)| {
+                        v.iter().for_each(|e| {
+                            vec.push(crate::common::code_occurence::OccurenceFilter {
+                                increment: e.increment,
+                                time: e.time_file_line_column.time,
+                                occurence: e.time_file_line_column.get_code_path(
+                                    git_info,
+                                    config_generic.get_source_place_type(),
+                                ),
+                            })
+                        })
+                    });
+                    //vec.reverse();//todo check reserve or not
+                    vec.sort_by(|a, b| a.increment.cmp(&b.increment));
+                    let mut occurences = Vec::with_capacity(capacity + 1);
+                    match &key_option {
+                        Some(key) => {
+                            occurences.push(format!(
+                                "[key: {}] {}{}",
+                                key,
+                                hashmap_value.get_source(),
+                                log_type.symbol()
+                            ));
+                        }
+                        None => {
+                            occurences.push(format!(
+                                "{}{}",
+                                hashmap_value.get_source(),
+                                log_type.symbol()
+                            ));
+                        }
+                    }
+                    vec.into_iter().for_each(|e| {
+                        occurences.push(format!(
+                            "{} {}{}",
+                            e.readable_time_string(),
+                            e.occurence,
+                            log_type.symbol()
+                        ));
+                    });
+                    let mut occurence =
+                        occurences.iter().fold(String::from(""), |mut acc, elem| {
+                            acc.push_str(elem);
+                            acc
+                        });
+                    log_type.pop_last(&mut occurence);
+                    occurence = format!("{}{}", occurence, log_type.symbol());
+                    acc.push_str(&occurence);
+                    acc
+                });
+        prepared_log_handle.pop();
+        prepared_log_handle
+    }
+}
