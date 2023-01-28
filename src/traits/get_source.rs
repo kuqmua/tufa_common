9577144e1625d8fe9f65e6c3e437a6845cd38e81
kuqmua <serezhashmakov@outlook.com>
2023-01-29 00:59:12 +0300

    GetOriginSourceAsString hashmap + vec

--- src/traits/get_source.rs
+++ src/traits/get_source.rs
@@ -8,48 +8,47 @@ pub trait GetOriginSourceAsString {
     fn get_origin_source_as_string(&self) -> String;
 }
 
-// impl<HashMapKeyGeneric, HashMapValueGeneric, ConfigGeneric> GetOriginSourceAsString
-//     for std::collections::HashMap<HashMapKeyGeneric, HashMapValueGeneric>
-// where
-//     HashMapKeyGeneric: std::fmt::Display,
-//     HashMapValueGeneric: crate::traits::get_source::GetOriginSourceAsString
-//         + crate::traits::get_code_occurence::GetCodeOccurenceOldWay,
-// {
-//     fn get_origin_source_as_string(&self) -> String {
-//         let mut source_as_string = self.iter().fold(String::from(""), |mut acc, (key, value)| {
-//             acc.push_str(&format!(
-//                 "[key: \n]{} \n{} {}\n",
-//                 key,
-//                 value.get_origin_source_as_string(),
-//                 value
-//                     .get_code_occurence_old_way()
-//                     .to_string_handle_code_occurence(config)
-//             ));
-//             acc
-//         });
-//         source_as_string.pop();
-//         source_as_string
-//     }
-// }
+impl<HashMapKeyGeneric, HashMapValueGeneric> GetOriginSourceAsString
+    for std::collections::HashMap<HashMapKeyGeneric, HashMapValueGeneric>
+where
+    HashMapKeyGeneric: std::fmt::Display,
+    HashMapValueGeneric: std::fmt::Display,
+{
+    fn get_origin_source_as_string(&self) -> String {
+        self.iter().fold(String::from(""), |mut acc, (key, value)| {
+            acc.push_str(&format!(
+                "{} [\n{}]\n",
+                key,
+                value
+                    .to_string()
+                    .lines()
+                    .fold(String::from(""), |mut acc, line| {
+                        acc.push_str(&format!(" {}\n", line));
+                        acc
+                    })
+            ));
+            acc
+        })
+    }
+}
 
-// impl<VecElementGeneric> GetOriginSourceAsString
-//     for Vec<VecElementGeneric>
-// where
-//     VecElementGeneric: crate::traits::get_source::GetOriginSourceAsString
-//         + crate::traits::get_code_occurence::GetCodeOccurenceOldWay,
-// {
-//     fn get_origin_source_as_string(&self) -> String {
-//         let mut source_as_string = self.iter().fold(String::from(""), |mut acc, vec_element| {
-//             acc.push_str(&format!(
-//                 "{}\n {}\n",
-//                 vec_element.get_origin_source_as_string(),
-//                 vec_element
-//                     .get_code_occurence_old_way()
-//                     .to_string_handle_code_occurence(config),
-//             ));
-//             acc
-//         });
-//         source_as_string.pop();
-//         source_as_string
-//     }
-// }
+impl<VecElementGeneric> GetOriginSourceAsString for Vec<VecElementGeneric>
+where
+    VecElementGeneric: std::fmt::Display,
+{
+    fn get_origin_source_as_string(&self) -> String {
+        format!(
+            "[\n{}]",
+            self.iter().fold(String::from(""), |mut acc, vec_element| {
+                acc.push_str(&vec_element.to_string().lines().fold(
+                    String::from(""),
+                    |mut acc, vec_element| {
+                        acc.push_str(&format!(" {}\n", vec_element));
+                        acc
+                    },
+                ));
+                acc
+            })
+        )
+    }
+}
