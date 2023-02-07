2ed6ee352a15377036aaf008dd8dcd2f3b7c7985
kuqmua <serezhashmakov@outlook.com>
2023-02-07 20:18:32 +0300

    pub trait PrepareForLogWithConfig + pub trait PrepareForLogWithoutConfig

--- src/traits/prepare_for_log.rs
+++ src/traits/prepare_for_log.rs
@@ -1,32 +1,62 @@
-pub trait PrepareForLog {
-    fn prepare_for_log(
+pub trait PrepareForLogWithConfig {
+    fn prepare_for_log_with_config(
         &self,
-        path: String,
-        time: String,
-        hostname: &String,
-        process_id: &u32,
-    ) -> String {
-        format!("{} {} {} pid: {}", path, time, hostname, process_id)
-    }
+        config: impl crate::traits::fields::GetTimezone,
+    ) -> String;
 }
 
-impl<SelfGeneric> PrepareForLog for SelfGeneric
+impl<SelfGeneric> PrepareForLogWithConfig for SelfGeneric
 where
     SelfGeneric: crate::traits::fields::GetFile
         + crate::traits::fields::GetLine
         + crate::traits::fields::GetColumn
-        + crate::traits::fields::GetCodePathWithoutConfig
+        + crate::traits::get_code_path_without_config::GetCodePathWithoutConfig
         + crate::traits::get_duration::GetDuration
         + crate::traits::get_hostname::GetHostname
         + crate::traits::get_process_id::GetProcessId,
 {
-    fn prepare_for_log(
+    fn prepare_for_log_with_config(
         &self,
-        // path: String,
-        // time: String,
-        // hostname: &String,
-        // process_id: &u32,
+        config: impl crate::traits::fields::GetTimezone,
     ) -> String {
-        format!("{} {} {} pid: {}", path, time, hostname, process_id)
+        format!(
+            "{} {} {} pid: {}",
+            self.get_code_path_without_config(),
+            chrono::DateTime::<chrono::Utc>::from(std::time::UNIX_EPOCH + self.get_duration())
+                .with_timezone(&chrono::FixedOffset::east_opt(*config.get_timezone()).unwrap())
+                .format("%Y-%m-%d %H:%M:%S")
+                .to_string(),
+            self.get_hostname(),
+            self.get_process_id()
+        )
+    }
+}
+
+pub trait PrepareForLogWithoutConfig {
+    fn prepare_for_log_without_config(&self) -> String;
+}
+
+impl<SelfGeneric> PrepareForLogWithoutConfig for SelfGeneric
+where
+    SelfGeneric: crate::traits::fields::GetFile
+        + crate::traits::fields::GetLine
+        + crate::traits::fields::GetColumn
+        //above its for crate::traits::get_code_path_without_config::GetCodePathWithoutConfig
+        + crate::traits::get_code_path_without_config::GetCodePathWithoutConfig
+        + crate::traits::get_duration::GetDuration //todo - think about
+        + crate::traits::get_hostname::GetHostname
+        + crate::traits::get_process_id::GetProcessId,
+{
+    fn prepare_for_log_without_config(&self) -> String {
+        format!(
+            "{} {} {} pid: {}",
+            self.get_code_path_without_config(),
+            chrono::DateTime::<chrono::Utc>::from(std::time::UNIX_EPOCH + self.get_duration())
+                .with_timezone(&chrono::FixedOffset::east_opt(10800).unwrap())
+                .format("%Y-%m-%d %H:%M:%S")
+                .to_string(),
+            self.get_hostname(),
+            self.get_process_id()
+        )
     }
 }
