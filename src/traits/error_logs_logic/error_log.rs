694b857e6fb71e0153497830e23f7be4d8f1b2a2
kuqmua <serezhashmakov@outlook.com>
2023-02-12 15:23:59 +0300

    remove trait ErrorLogLifetimeWithDeserialize

--- src/traits/error_logs_logic/error_log.rs
+++ src/traits/error_logs_logic/error_log.rs
@@ -24,30 +24,3 @@ where
         );
     }
 }
-
-pub trait ErrorLogLifetimeWithDeserialize<'a, ConfigGeneric> {
-    fn error_log_lifetime_with_deserialize(&self, config: &ConfigGeneric);
-}
-
-impl<'a, SelfGeneric, ConfigGeneric>
-    crate::traits::error_logs_logic::error_log::ErrorLogLifetimeWithDeserialize<'a, ConfigGeneric> for SelfGeneric
-where
-    ConfigGeneric: crate::traits::get_color::ErrorColorBold
-        + crate::traits::fields::GetServerPort
-        + crate::traits::fields::GetSourcePlaceType
-        + crate::traits::fields::GetTimezone
-        + crate::traits::fields::GetServerIp,
-    SelfGeneric: crate::traits::error_logs_logic::to_string_with_config::ToStringWithConfigLifetimeWithDeserialize<
-        'a,
-        ConfigGeneric,
-    >,
-{
-    fn error_log_lifetime_with_deserialize(&self, config: &ConfigGeneric) {
-        eprintln!(
-            "{}",
-            config
-                .get_error_color_bold()
-                .paint(self.to_string_with_config_lifetime_with_deserialize(config))
-        );
-    }
-}
