6b7810d5bbd81c44608bf45272b58c555217c4d7
kuqmua <serezhashmakov@outlook.com>
2023-03-03 20:04:00 +0300

    #[display_is_not_implemented] attribute support for ImplErrorOccurence

--- src/repositories_types/tufa_server/telemetry/init_subcriber_error_enum.rs
+++ src/repositories_types/tufa_server/telemetry/init_subcriber_error_enum.rs
@@ -2,7 +2,7 @@ use strum_macros::Display;
 use tracing::dispatcher::SetGlobalDefaultError;
 use tracing::log::SetLoggerError;
 
-#[derive(Debug, thiserror::Error, serde::Serialize, error_occurence::ImplErrorOccurence)]
+#[derive(Debug, thiserror::Error, serde::Serialize)] //, error_occurence::ImplErrorOccurence
 pub enum InitSubcriberErrorEnum<'a> {
     SetLogger {
         error: String,
@@ -12,4 +12,30 @@ pub enum InitSubcriberErrorEnum<'a> {
         error: String,
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
     },
+}
+
+impl<'a> std::fmt::Display for InitSubcriberErrorEnum<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "kwko")
+    }
+}
+
+// todo - implement conversion to serialize\Deserialize version
+
+#[derive(Debug)]
+pub struct Kekw {}
+
+#[derive(Debug, thiserror::Error, error_occurence::ImplErrorOccurence)] //, serde::Serialize, error_occurence::ImplErrorOccurence
+pub enum Seven<'a> {
+    Something {
+        #[display_is_not_implemented]
+        error: Kekw,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+}
+
+impl crate::traits::display_foreign_type::DisplayForeignType for Kekw {
+    fn display_foreign_type(&self) -> String {
+        String::from("kekw")
+    }
 }
--- src/traits/display_foreign_type.rs
+++ src/traits/display_foreign_type.rs
@@ -1,3 +1,3 @@
-trait DisplayForeignType {
-    fn display_foreign_type(&self) -> String;
+pub trait DisplayForeignType {
+    fn display_foreign_type(&self) -> String; //todo - maybe use &str here?
 }
