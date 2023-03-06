4f1cfb87bd604a531b38bcab544f22801142c3e0
kuqmua <serezhashmakov@outlook.com>
2023-03-06 15:15:54 +0300

    next steps

--- src/repositories_types/tufa_server/init_dbs_logic/init_mongo.rs
+++ src/repositories_types/tufa_server/init_dbs_logic/init_mongo.rs
@@ -10,5 +10,6 @@ pub enum InitMongoWrapperError<'a> {
 pub enum InitMongoWrapperErrorEnum<'a> {
     ClientOptionsParseWrapper(crate::repositories_types::tufa_server::mongo_integration::mongo_client_options_parse::MongoClientOptionsParseOriginError<'a>),
     ClientWithOptionsWrapper(crate::repositories_types::tufa_server::mongo_integration::mongo_client_with_options::MongoClientWithOptionsOriginError<'a>),
+    CollectionIsNotEmpty(crate::repositories_types::tufa_server::mongo_integration::mongo_check_collection_is_not_empty::MongoCheckCollectionIsNotEmptyError<'a>),
     InsertManyErrorWrapper(crate::repositories_types::tufa_server::mongo_integration::mongo_insert_many::MongoInsertManyOriginError<'a>),
 }
--- src/repositories_types/tufa_server/mongo_integration/mongo_check_collection_is_not_empty.rs
+++ src/repositories_types/tufa_server/mongo_integration/mongo_check_collection_is_not_empty.rs
@@ -1,19 +1,20 @@
 #[derive(Debug, thiserror::Error, error_occurence::ImplErrorOccurence)]
-pub enum MongoClientWithOptionsOriginError<'a> {
+pub enum MongoCheckCollectionIsNotEmptyError<'a> {
     Mongo {
-        inner_errors: std::collections::HashMap<String, MongoClientWithOptionsOriginErrorEnum<'a>>,
+        inner_errors:
+            std::collections::HashMap<String, MongoCheckCollectionIsNotEmptyErrorEnum<'a>>,
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
     },
 }
 
 #[derive(Debug, thiserror::Error, error_occurence::ImplErrorOccurence)]
-pub enum MongoClientWithOptionsOriginErrorEnum<'a> {
-    CountDocumentsOrigin(MongoClientWithOptionsOriginErrorEnumCountDocuments<'a>),
-    IsNotEmptyOrigin(MongoClientWithOptionsOriginErrorEnumIsNotEmptyOrigin<'a>),
+pub enum MongoCheckCollectionIsNotEmptyErrorEnum<'a> {
+    CountDocumentsOrigin(MongoCheckCollectionIsNotEmptyErrorEnumCountDocuments<'a>),
+    IsNotEmptyOrigin(MongoCheckCollectionIsNotEmptyErrorEnumIsNotEmptyOrigin<'a>),
 }
 
 #[derive(Debug, thiserror::Error, error_occurence::ImplErrorOccurence)]
-pub enum MongoClientWithOptionsOriginErrorEnumCountDocuments<'a> {
+pub enum MongoCheckCollectionIsNotEmptyErrorEnumCountDocuments<'a> {
     CountDocuments {
         error: mongodb::error::Error,
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
@@ -21,7 +22,7 @@ pub enum MongoClientWithOptionsOriginErrorEnumCountDocuments<'a> {
 }
 
 #[derive(Debug, thiserror::Error, error_occurence::ImplErrorOccurence)]
-pub enum MongoClientWithOptionsOriginErrorEnumIsNotEmptyOrigin<'a> {
+pub enum MongoCheckCollectionIsNotEmptyErrorEnumIsNotEmptyOrigin<'a> {
     IsNotEmptyOrigin {
         error: u64,
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
