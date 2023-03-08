173bfc7408af501dfc88bba723985b7df2d14b29
kuqmua <serezhashmakov@outlook.com>
2023-03-08 09:52:10 +0300

    impl DisplayForeignType for Vec<VecElementGeneric> & std::collections::HashMap<HashMapKeyGeneric, HashMapValueGeneric>

--- src/traits/display_foreign_type.rs
+++ src/traits/display_foreign_type.rs
@@ -1,3 +1,43 @@
 pub trait DisplayForeignType {
     fn display_foreign_type(&self) -> String; //todo - maybe use &str here?
 }
+
+impl<VecElementGeneric> DisplayForeignType for Vec<VecElementGeneric>
+where
+    VecElementGeneric: DisplayForeignType,
+{
+    fn display_foreign_type(&self) -> String {
+        crate::traits::error_logs_logic::helpers::stringified_lines_error_vec(self.iter().fold(
+            String::from(""),
+            |mut acc, vec_element| {
+                acc.push_str(
+                    &crate::traits::error_logs_logic::helpers::lines_space_backslash_addition(
+                        vec_element.display_foreign_type(),
+                    ),
+                );
+                acc
+            },
+        ))
+    }
+}
+
+impl<HashMapKeyGeneric, HashMapValueGeneric> DisplayForeignType
+    for std::collections::HashMap<HashMapKeyGeneric, HashMapValueGeneric>
+where
+    HashMapKeyGeneric: std::fmt::Display,
+    HashMapValueGeneric: DisplayForeignType,
+{
+    fn display_foreign_type(&self) -> String {
+        let mut stringified = self.iter().fold(String::from(""), |mut acc, (key, value)| {
+            acc.push_str(
+                &crate::traits::error_logs_logic::helpers::stringified_lines_error_hashmap_element(
+                    key,
+                    value.display_foreign_type(),
+                ),
+            );
+            acc
+        });
+        stringified.pop();
+        stringified
+    }
+}
