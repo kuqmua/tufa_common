f5417893ba9fe82e9c6eee920d592e13d14b02ca
kuqmua <serezhashmakov@outlook.com>
2023-04-01 19:21:55 +0300

    renaming

--- src/dev.rs
+++ src/dev.rs
@@ -2050,20 +2050,20 @@ impl<'a> NamedError<'a> {
                 n: { n },
                 o: { o },
                 p: {
-                    use crate::traits::error_logs_logic::hashmap_impl_display_display_foreign_type_into_hashmap_impl_display_string::HashmapImplDisplayDisplayForeignTypeIntoHashmapImplDisplayString;
-                    p.hashmap_impl_display_display_foreign_type_into_hashmap_impl_display_string()
+                    use crate::traits::error_logs_logic::hashmap_display_display_foreign_type_into_hashmap_display_string::HashmapDisplayDisplayForeignTypeIntoHashmapDisplayString;
+                    p.hashmap_display_display_foreign_type_into_hashmap_display_string()
                 },
                 q: {
-                    use crate::traits::error_logs_logic::hashmap_impl_display_display_foreign_type_into_hashmap_impl_display_string::HashmapImplDisplayDisplayForeignTypeIntoHashmapImplDisplayString;
-                    q.hashmap_impl_display_display_foreign_type_into_hashmap_impl_display_string()
+                    use crate::traits::error_logs_logic::hashmap_display_display_foreign_type_into_hashmap_display_string::HashmapDisplayDisplayForeignTypeIntoHashmapDisplayString;
+                    q.hashmap_display_display_foreign_type_into_hashmap_display_string()
                 },
                 r: {
-                    use crate::traits::error_logs_logic::hashmap_impl_display_display_foreign_type_into_hashmap_impl_display_string::HashmapImplDisplayDisplayForeignTypeIntoHashmapImplDisplayString;
-                    r.hashmap_impl_display_display_foreign_type_into_hashmap_impl_display_string()
+                    use crate::traits::error_logs_logic::hashmap_display_display_foreign_type_into_hashmap_display_string::HashmapDisplayDisplayForeignTypeIntoHashmapDisplayString;
+                    r.hashmap_display_display_foreign_type_into_hashmap_display_string()
                 },
                 s: {
-                    use crate::traits::error_logs_logic::hashmap_impl_display_display_foreign_type_into_hashmap_impl_display_string::HashmapImplDisplayDisplayForeignTypeIntoHashmapImplDisplayString;
-                    s.hashmap_impl_display_display_foreign_type_into_hashmap_impl_display_string()
+                    use crate::traits::error_logs_logic::hashmap_display_display_foreign_type_into_hashmap_display_string::HashmapDisplayDisplayForeignTypeIntoHashmapDisplayString;
+                    s.hashmap_display_display_foreign_type_into_hashmap_display_string()
                 },
                 t: {
                     t.into_iter()
--- src/traits/error_logs_logic/hashmap_display_display_foreign_type_into_hashmap_display_string.rs
+++ src/traits/error_logs_logic/hashmap_display_display_foreign_type_into_hashmap_display_string.rs
@@ -1,17 +1,17 @@
-pub trait HashmapImplDisplayDisplayForeignTypeIntoHashmapImplDisplayString<HashMapKeyGeneric> {
-    fn hashmap_impl_display_display_foreign_type_into_hashmap_impl_display_string(
+pub trait HashmapDisplayDisplayForeignTypeIntoHashmapDisplayString<HashMapKeyGeneric> {
+    fn hashmap_display_display_foreign_type_into_hashmap_display_string(
         self,
     ) -> std::collections::HashMap<HashMapKeyGeneric, String>;
 }
 
 impl<HashMapKeyGeneric, HashMapValueGeneric>
-    HashmapImplDisplayDisplayForeignTypeIntoHashmapImplDisplayString<HashMapKeyGeneric>
+    HashmapDisplayDisplayForeignTypeIntoHashmapDisplayString<HashMapKeyGeneric>
     for std::collections::HashMap<HashMapKeyGeneric, HashMapValueGeneric>
 where
     HashMapKeyGeneric: std::fmt::Display + std::cmp::Eq + std::hash::Hash,
     HashMapValueGeneric: crate::traits::display_foreign_type::DisplayForeignType,
 {
-    fn hashmap_impl_display_display_foreign_type_into_hashmap_impl_display_string(
+    fn hashmap_display_display_foreign_type_into_hashmap_display_string(
         self,
     ) -> std::collections::HashMap<HashMapKeyGeneric, String> {
         self.into_iter()
