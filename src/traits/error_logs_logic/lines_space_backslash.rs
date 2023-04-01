e8f39b803a3655e93eb18ea00030013492ed553e
kuqmua <serezhashmakov@outlook.com>
2023-04-01 12:16:11 +0300

    trait LinesSpaceBackslash

---
+++ src/traits/error_logs_logic/lines_space_backslash.rs
@@ -0,0 +1,17 @@
+pub trait LinesSpaceBackslash {
+    fn lines_space_backslash(&self) -> String;
+}
+
+impl<'a, SelfGeneric> LinesSpaceBackslash for SelfGeneric
+where
+    SelfGeneric: std::fmt::Display,
+{
+    fn lines_space_backslash(&self) -> String {
+        self.to_string()
+            .lines()
+            .fold(String::from(""), |mut acc, line| {
+                acc.push_str(&format!(" {}\n", line));
+                acc
+            })
+    }
+}
--- src/traits/error_logs_logic/mod.rs
+++ src/traits/error_logs_logic/mod.rs
@@ -14,6 +14,7 @@ pub mod hashmap_impl_display_impl_display_to_string;
 pub mod hashmap_impl_display_to_string_with_config_to_string;
 pub mod hashmap_impl_display_to_string_without_config_to_string;
 pub mod helpers;
+pub mod lines_space_backslash;
 pub mod source_to_string_with_config;
 pub mod source_to_string_without_config;
 #[cfg(test)]
