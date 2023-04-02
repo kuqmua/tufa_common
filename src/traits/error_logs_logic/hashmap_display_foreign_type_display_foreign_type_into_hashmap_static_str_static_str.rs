91cd9516137bcd471db92adffb5e7013841dc43c
kuqmua <serezhashmakov@outlook.com>
2023-04-02 21:11:14 +0300

    renaming

--- src/traits/error_logs_logic/hashmap_display_foreign_type_display_foreign_type_into_hashmap_static_str_static_str.rs
+++ src/traits/error_logs_logic/hashmap_display_foreign_type_display_foreign_type_into_hashmap_static_str_static_str.rs
@@ -1,18 +1,18 @@
-pub trait HashmapDisplayForeignTypeDisplayForeignTypeIntoHashMapStringString {
-    fn hashmap_display_foreign_type_display_foreign_type_into_hashmap_string_string(
+pub trait HashmapDisplayForeignTypeDisplayForeignTypeIntoHashMapStaticStrStaticStr {
+    fn hashmap_display_foreign_type_display_foreign_type_into_hashmap_static_str_static_str(
         self,
     ) -> std::collections::HashMap<&'static str, &'static str>;
 }
 
 impl<HashMapKeyGeneric, HashMapValueGeneric>
-    HashmapDisplayForeignTypeDisplayForeignTypeIntoHashMapStringString
+    HashmapDisplayForeignTypeDisplayForeignTypeIntoHashMapStaticStrStaticStr
     for std::collections::HashMap<HashMapKeyGeneric, HashMapValueGeneric>
 where
     HashMapKeyGeneric:
         crate::traits::display_foreign_type::DisplayForeignType + std::cmp::Eq + std::hash::Hash,
     HashMapValueGeneric: crate::traits::display_foreign_type::DisplayForeignType,
 {
-    fn hashmap_display_foreign_type_display_foreign_type_into_hashmap_string_string(
+    fn hashmap_display_foreign_type_display_foreign_type_into_hashmap_static_str_static_str(
         self,
     ) -> std::collections::HashMap<&'static str, &'static str> {
         self.into_iter()
