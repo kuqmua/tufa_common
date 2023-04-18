5f49c2f9a4bdbd5ab83ae08dc644892caf564a7d
kuqmua <serezhashmakov@outlook.com>
2023-04-18 23:11:40 +0300

    next steps

--- src/repositories_types/tufa_server/telemetry/init_subcriber_error_enum.rs
+++ src/repositories_types/tufa_server/telemetry/init_subcriber_error_enum.rs
@@ -1,12 +1,12 @@
-#[derive(Debug, thiserror::Error, error_occurence::ImplErrorOccurence)]
+#[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
 pub enum InitSubcriberErrorEnum<'a> {
     SetGlobalDefault {
-        #[display_foreign_type]
+        #[eo_display_foreign_type]
         error: tracing::dispatcher::SetGlobalDefaultError,
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
     },
     SetLogger {
-        #[display_foreign_type]
+        #[eo_display_foreign_type]
         error: tracing::log::SetLoggerError,
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
     },
@@ -15,13 +15,13 @@ pub enum InitSubcriberErrorEnum<'a> {
 impl crate::traits::display_foreign_type::DisplayForeignType
     for tracing::dispatcher::SetGlobalDefaultError
 {
-    fn display_foreign_type(&self) -> String {
-        String::from("SetGlobalDefaultError")
+    fn display_foreign_type(&self) -> &'static str {
+        "SetGlobalDefaultError"
     }
 }
 
 impl crate::traits::display_foreign_type::DisplayForeignType for tracing::log::SetLoggerError {
-    fn display_foreign_type(&self) -> String {
-        String::from("SetLoggerError")
+    fn display_foreign_type(&self) -> &'static str {
+        "SetLoggerError"
     }
 }
