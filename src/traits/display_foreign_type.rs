96a83497c402e655688ec39f93cd1c663452302e
kuqmua <serezhashmakov@outlook.com>
2023-04-19 19:19:12 +0300

    impl crate::traits::display_foreign_type::DisplayForeignType for sqlx::Error

--- src/traits/display_foreign_type.rs
+++ src/traits/display_foreign_type.rs
@@ -2,42 +2,25 @@ pub trait DisplayForeignType {
     fn display_foreign_type(&self) -> &'static str;
 }
 
-// impl<VecElementGeneric> DisplayForeignType for Vec<VecElementGeneric>
-// where
-//     VecElementGeneric: DisplayForeignType,
-// {
-//     fn display_foreign_type(&self) -> &'static str {
-//         crate::traits::error_logs_logic::helpers::stringified_lines_error_vec(self.iter().fold(
-//             String::from(""),
-//             |mut acc, vec_element| {
-//                 acc.push_str(
-//                     &crate::traits::error_logs_logic::helpers::lines_space_backslash_addition(
-//                         vec_element.display_foreign_type(),
-//                     ),
-//                 );
-//                 acc
-//             },
-//         ))
-//     }
-// }
-
-// impl<HashMapKeyGeneric, HashMapValueGeneric> DisplayForeignType
-//     for std::collections::HashMap<HashMapKeyGeneric, HashMapValueGeneric>
-// where
-//     HashMapKeyGeneric: std::fmt::Display,
-//     HashMapValueGeneric: DisplayForeignType,
-// {
-//     fn display_foreign_type(&self) -> String {
-//         let mut stringified = self.iter().fold(String::from(""), |mut acc, (key, value)| {
-//             acc.push_str(
-//                 &crate::traits::error_logs_logic::helpers::stringified_lines_error_hashmap_element(
-//                     key,
-//                     value.display_foreign_type(),
-//                 ),
-//             );
-//             acc
-//         });
-//         stringified.pop();
-//         stringified
-//     }
-// }
+impl crate::traits::display_foreign_type::DisplayForeignType for sqlx::Error {
+    fn display_foreign_type(&self) -> &'static str {
+        match self {
+            sqlx::Error::Configuration(_) => "sqlx::Error::Configuration",
+            sqlx::Error::Database(_) => "sqlx::Error::Database",
+            sqlx::Error::Io(_) => "sqlx::Error::Io",
+            sqlx::Error::Tls(_) => "sqlx::Error::Tls",
+            sqlx::Error::Protocol(_) => "sqlx::Error::Protocol",
+            sqlx::Error::RowNotFound => "sqlx::Error::RowNotFound",
+            sqlx::Error::TypeNotFound { type_name } => "sqlx::Error::TypeNotFound",
+            sqlx::Error::ColumnIndexOutOfBounds { index, len } => "sqlx::Error::ColumnIndexOutOfBounds",
+            sqlx::Error::ColumnNotFound(_) => "sqlx::Error::ColumnNotFound",
+            sqlx::Error::ColumnDecode { index, source } => "sqlx::Error::ColumnDecode",
+            sqlx::Error::Decode(_) => "sqlx::Error::Decode",
+            sqlx::Error::PoolTimedOut => "sqlx::Error::PoolTimedOut",
+            sqlx::Error::PoolClosed => "sqlx::Error::PoolClosed",
+            sqlx::Error::WorkerCrashed => "sqlx::Error::WorkerCrashed",
+            sqlx::Error::Migrate(_) => "sqlx::Error::Migrate",
+            _ => "unknown sqlx::Error",
+        }
+    }
+}
