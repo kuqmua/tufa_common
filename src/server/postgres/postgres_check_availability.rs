241b5c592fdc54d3affa8266ad1fbbaef0d13643
kuqmua <serezhashmakov@outlook.com>
2023-04-19 19:22:43 +0300

    next steps

--- src/server/postgres/postgres_check_availability.rs
+++ src/server/postgres/postgres_check_availability.rs
@@ -9,9 +9,10 @@ use sqlx::postgres::PgPoolOptions;
 use sqlx::Error;
 use std::time::Duration;
 
-#[derive(Debug, thiserror::Error, error_occurence::ImplErrorOccurence)]
+#[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
 pub enum PostgresCheckAvailabilityError<'a> {
     Postgres {
+        #[eo_display_foreign_type]
         error: sqlx::Error,
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
     },
