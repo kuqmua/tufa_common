70a7dac2cd812f5a8fb81e92e8252bfa299eae5d
kuqmua <serezhashmakov@outlook.com>
2023-04-20 19:22:40 +0300

    lil tweaks

--- src/server/net/net_check_availability.rs
+++ src/server/net/net_check_availability.rs
@@ -7,17 +7,20 @@ use impl_get_source::ImplGetSourceFromTufaCommon;
 use impl_get_where_was_origin_or_wrapper::ImplGetWhereWasOriginOrWrapperFromTufaCommon;
 use init_error::InitErrorFromTufaCommon;
 
-#[derive(Debug, thiserror::Error, error_occurence::ImplErrorOccurence)]
+#[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
 pub enum NetCheckAvailabilityError<'a> {
     ReqwestGetOrigin {
+        #[eo_display_foreign_type]
         error: reqwest::Error,
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
     },
     ClientOrigin {
+        #[eo_display_foreign_type]
         error: reqwest::StatusCode,
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
     },
     ServerOrigin {
+        #[eo_display_foreign_type]
         error: reqwest::StatusCode,
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
     },
