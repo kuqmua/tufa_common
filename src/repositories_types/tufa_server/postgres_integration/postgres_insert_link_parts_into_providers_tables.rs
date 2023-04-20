45f18fd335e58969cbdcd52e1474bb1f9709b1c2
kuqmua <serezhashmakov@outlook.com>
2023-04-20 19:32:49 +0300

    next steps

--- src/repositories_types/tufa_server/postgres_integration/postgres_insert_link_parts_into_providers_tables.rs
+++ src/repositories_types/tufa_server/postgres_integration/postgres_insert_link_parts_into_providers_tables.rs
@@ -1,17 +1,24 @@
-#[derive(Debug, thiserror::Error, error_occurence::ImplErrorOccurence)]
+#[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
 pub enum PostgresInsertLinkPartsIntoProvidersTablesOriginError<'a> {
     Postgres {
+        #[eo_hashmap_key_display_value_error_occurence]
         inner_errors: std::collections::HashMap<
             String,
-            PostgresInsertLinkPartsIntoProvidersTablesOriginErrorEnum<'a>,
+            PostgresInsertLinkPartsIntoProvidersTablesOriginErrorEnumUnnamed<'a>,
         >,
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
     },
 }
 
-#[derive(Debug, thiserror::Error, error_occurence::ImplErrorOccurence)]
+#[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
+pub enum PostgresInsertLinkPartsIntoProvidersTablesOriginErrorEnumUnnamed<'a> {
+    Something(PostgresInsertLinkPartsIntoProvidersTablesOriginErrorEnum<'a>),
+}
+
+#[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
 pub enum PostgresInsertLinkPartsIntoProvidersTablesOriginErrorEnum<'a> {
     Postgres {
+        #[eo_display_foreign_type]
         error: sqlx::Error,
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
     },
