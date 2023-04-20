8b112e94d15782f23fc3985d8f31226a07166ca9
kuqmua <serezhashmakov@outlook.com>
2023-04-20 19:43:47 +0300

    next steps

--- src/repositories_types/tufa_server/preparation/check_availability.rs
+++ src/repositories_types/tufa_server/preparation/check_availability.rs
@@ -1,26 +1,30 @@
-#[derive(Debug, thiserror::Error, error_occurence::ImplErrorOccurence)]
+#[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
 pub enum CheckAvailabilityError<'a> {
     Net {
+        #[eo_error_occurence]
         error: crate::server::net::net_check_availability::NetCheckAvailabilityError<'a>,
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
     },
     Postgres {
+        #[eo_error_occurence]
         error: crate::server::postgres::postgres_check_availability::PostgresCheckAvailabilityError<
             'a,
         >,
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
     },
     Mongo {
+        #[eo_error_occurence]
         error: crate::server::mongo::mongo_check_availability::MongoCheckAvailabilityError<'a>,
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
     },
     Many {
+        #[eo_vec_error_occurence]
         inner_errors: Vec<CheckAvailabilityErrorEnum<'a>>,
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
     },
 }
 
-#[derive(Debug, thiserror::Error, error_occurence::ImplErrorOccurence)]
+#[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
 pub enum CheckAvailabilityErrorEnum<'a> {
     Net(crate::server::net::net_check_availability::NetCheckAvailabilityError<'a>),
     Postgres(
