9e6baba3972e4dd7d4e7f4186c5be7679c408e46
kuqmua <serezhashmakov@outlook.com>
2023-04-20 19:45:23 +0300

    next steps

--- src/repositories_types/tufa_server/preparation/prepare_server.rs
+++ src/repositories_types/tufa_server/preparation/prepare_server.rs
@@ -1,6 +1,7 @@
-#[derive(Debug, thiserror::Error, error_occurence::ImplErrorOccurence)]
+#[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
 pub enum PrepareServerError<'a> {
     CheckAvailability {
+        #[eo_error_occurence]
         inner_error: crate::repositories_types::tufa_server::preparation::check_availability::CheckAvailabilityError<'a>,
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
     },
