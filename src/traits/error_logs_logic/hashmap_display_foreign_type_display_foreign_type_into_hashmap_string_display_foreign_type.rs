a7cb98160e8797a6598f5fb0c9d43af35a510aeb
kuqmua <serezhashmakov@outlook.com>
2023-04-21 15:00:13 +0300

    hashmap_display_foreign_type_display_foreign_type_into_hashmap_string_display_foreign_type

---
+++ src/traits/error_logs_logic/hashmap_display_foreign_type_display_foreign_type_into_hashmap_string_display_foreign_type.rs
@@ -0,0 +1,27 @@
+pub trait HashMapDisplayForeignTypeDisplayForeignTypeIntoHashMapStringDisplayForeignType<HashMapValueGeneric> {
+    fn hashmap_display_foreign_type_display_foreign_type_into_hashmap_string_display_foreign_type(
+        self,
+    ) -> std::collections::HashMap<String, HashMapValueGeneric>;
+}
+
+impl<HashMapKeyGeneric, HashMapValueGeneric>
+    HashMapDisplayForeignTypeDisplayForeignTypeIntoHashMapStringDisplayForeignType<HashMapValueGeneric>
+    for std::collections::HashMap<HashMapKeyGeneric, HashMapValueGeneric>
+where
+    HashMapKeyGeneric:
+        crate::traits::display_foreign_type::DisplayForeignType + std::cmp::Eq + std::hash::Hash,
+    HashMapValueGeneric: crate::traits::display_foreign_type::DisplayForeignType,
+{
+    fn hashmap_display_foreign_type_display_foreign_type_into_hashmap_string_display_foreign_type(
+        self,
+    ) -> std::collections::HashMap<String, HashMapValueGeneric> {
+        self.into_iter()
+            .map(|(k, v)| {
+                (
+                    k.display_foreign_type().to_string(),//todo remove .to_string() from.display_foreign_type().to_string()
+                    v,
+                )
+            })
+            .collect()
+    }
+}
