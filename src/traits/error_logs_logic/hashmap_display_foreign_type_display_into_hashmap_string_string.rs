40bba7515d570900c80996d798d572dea0de13b4
kuqmua <serezhashmakov@outlook.com>
2023-04-22 00:26:57 +0300

    pub trait HashMapDisplayForeignTypeDisplayIntoHashMapStringString

---
+++ src/traits/error_logs_logic/hashmap_display_foreign_type_display_into_hashmap_string_string.rs
@@ -0,0 +1,27 @@
+pub trait HashMapDisplayForeignTypeDisplayIntoHashMapStringString {
+    fn hashmap_display_foreign_type_display_into_hashmap_string_string(
+        self,
+    ) -> std::collections::HashMap<String, String>;
+}
+
+impl<HashMapKeyGeneric, HashMapValueGeneric>
+    HashMapDisplayForeignTypeDisplayIntoHashMapStringString
+    for std::collections::HashMap<HashMapKeyGeneric, HashMapValueGeneric>
+where
+    HashMapKeyGeneric:
+        crate::traits::display_foreign_type::DisplayForeignType + std::cmp::Eq + std::hash::Hash,
+    HashMapValueGeneric: std::fmt::Display,
+{
+    fn hashmap_display_foreign_type_display_into_hashmap_string_string(
+        self,
+    ) -> std::collections::HashMap<String, String> {
+        self.into_iter()
+            .map(|(k, v)| {
+                (
+                    k.display_foreign_type(),
+                    v.to_string(),
+                )
+            })
+            .collect()
+    }
+}
