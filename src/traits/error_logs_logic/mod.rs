d1e5992ebca6a65b70817f391f040fa09bad42e3
kuqmua <serezhashmakov@outlook.com>
2023-04-21 23:43:40 +0300

    pub trait VecDisplayIntoVecString

--- src/traits/error_logs_logic/mod.rs
+++ src/traits/error_logs_logic/mod.rs
@@ -22,6 +22,7 @@ pub mod source_to_string_without_config;
 pub mod test;
 pub mod to_string_with_config;
 pub mod to_string_without_config;
+pub mod vec_display_into_vec_string;
 pub mod vec_display_foreign_type_into_vec_string;
 pub mod vec_display_foreign_type_to_string;
 pub mod vec_display_to_string;
