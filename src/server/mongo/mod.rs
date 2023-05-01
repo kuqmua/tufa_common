d8c65542d64ded44e44c75e69673d20e4240b131
kuqmua <serezhashmakov@outlook.com>
2023-05-01 17:17:43 +0300

    remove useless function

--- src/server/mongo/mod.rs
+++ src/server/mongo/mod.rs
@@ -1,5 +1,4 @@
 pub mod mongo_check_availability;
 pub mod mongo_check_collection_is_empty;
 pub mod mongo_check_db_is_empty;
-pub mod mongo_client_with_options;
 pub mod mongo_insert_docs_in_empty_collection;
