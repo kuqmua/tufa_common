264e21ca0f4c64da8ba444fd24eae5566d172e84
kuqmua <serezhashmakov@outlook.com>
2023-05-02 07:26:00 +0300

    mongo_drop_db

--- src/server/mongo/mod.rs
+++ src/server/mongo/mod.rs
@@ -7,3 +7,4 @@ pub mod mongo_get_providers_link_parts;
 pub mod mongo_get_documents_as_string_vector;
 pub mod mongo_drop_empty_db;
 pub mod mongo_drop_empty_collection;
+pub mod mongo_drop_db;
---
+++ src/server/mongo/mongo_drop_db.rs
@@ -0,0 +1,8 @@
+#[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
+pub enum MongoDropDbErrorNamed<'a> {
+    MongoDB {
+        #[eo_display_foreign_type]
+        mongodb: mongodb::error::Error,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+}
