b56a4fb31647979e6706350049ff0ce171e1a348
kuqmua <serezhashmakov@outlook.com>
2023-05-02 05:53:00 +0300

    mongo_get_providers_link_parts refactoring

--- src/server/mongo/mongo_get_providers_link_parts.rs
+++ src/server/mongo/mongo_get_providers_link_parts.rs
@@ -1,13 +1,23 @@
-// #[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
-// pub enum MongoGetProvidersLinkPartsErrorNamed<'a> {
-//     MongoDB {
-//         #[eo_display_foreign_type]
-//         mongodb: mongodb::error::Error,
-//         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
-//     },
-//     CollectionIsNotEmpty {
-//         #[eo_display_with_serialize_deserialize]
-//         collection_is_not_empty: u64,
-//         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
-//     },
-// }
+#[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
+pub enum MongoGetProvidersLinkPartsErrorNamed<'a> {
+    MongoDB {
+        #[eo_display_foreign_type]
+        mongodb: mongodb::error::Error,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    NoSuchCollections {
+        #[eo_hashmap_key_display_with_serialize_deserialize_value_display_with_serialize_deserialize]
+        no_such_collections: std::collections::HashMap<std::string::String, std::string::String>,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    GetDocuments {
+        #[eo_hashmap_key_display_with_serialize_deserialize_value_error_occurence]
+        get_documents: std::collections::HashMap<std::string::String, MongoGetDocumentsAsStringVectorErrorUnnamed<'a>>,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+}
+
+#[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
+pub enum MongoGetDocumentsAsStringVectorErrorUnnamed<'a> {
+    MongoGetDocumentsAsStringVector(crate::server::mongo::mongo_get_documents_as_string_vector::MongoGetDocumentsAsStringVectorErrorNamed<'a>),
+}
