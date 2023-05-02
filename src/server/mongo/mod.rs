263ad6e19d65b36f1d28f20b3fc071f422f8e2bb
kuqmua <serezhashmakov@outlook.com>
2023-05-02 07:07:17 +0300

    mongo_drop_empty_collection refactoring

--- src/server/mongo/mod.rs
+++ src/server/mongo/mod.rs
@@ -6,3 +6,4 @@ pub mod mongo_insert_data;
 pub mod mongo_get_providers_link_parts;
 pub mod mongo_get_documents_as_string_vector;
 pub mod mongo_drop_empty_db;
+pub mod mongo_drop_empty_collection;
---
+++ src/server/mongo/mongo_drop_empty_collection.rs
@@ -0,0 +1,15 @@
+#[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
+pub enum MongoDropEmptyCollectionErrorNamed<'a> {
+    MongoDB {
+        #[eo_display_foreign_type]
+        mongodb: mongodb::error::Error,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    CollectionIsNotEmpty {
+        #[eo_display_with_serialize_deserialize]
+        collection_name: &'a str,
+        #[eo_display_with_serialize_deserialize]
+        collection_len: u64,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+}
