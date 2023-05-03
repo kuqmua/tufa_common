b5d31be967eee2c2f48aa66f492720b2177de7df
kuqmua <serezhashmakov@outlook.com>
2023-05-04 00:14:44 +0300

    refactoring

--- src/server/mongo/mongo_drop_empty_collection.rs
+++ src/server/mongo/mongo_drop_empty_collection.rs
@@ -7,7 +7,7 @@ pub enum MongoDropEmptyCollectionErrorNamed<'a> {
     },
     CollectionIsNotEmpty {
         #[eo_display_with_serialize_deserialize]
-        collection_name: &'a str,
+        collection_name: std::string::String,
         #[eo_display_with_serialize_deserialize]
         collection_len: u64,
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
