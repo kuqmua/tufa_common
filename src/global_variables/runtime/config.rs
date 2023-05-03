af143a8f978a290dcd1b228df2627303fc799401
kuqmua <serezhashmakov@outlook.com>
2023-05-03 23:24:19 +0300

    next steps

--- src/global_variables/runtime/config.rs
+++ src/global_variables/runtime/config.rs
@@ -1,8 +1,5 @@
-use crate::config_mods::config_struct::ConfigStruct;
-use once_cell::sync::Lazy;
-
-pub static CONFIG: Lazy<ConfigStruct> = Lazy::new(|| {
-    ConfigStruct::new()
+pub static CONFIG: once_cell::sync::Lazy<crate::config_mods::config_struct::ConfigStruct> = once_cell::sync::Lazy::new(|| {
+    crate::config_mods::config_struct::ConfigStruct::new()
         .expect("cannot create config")
         .wrap_config_checks()
         .expect("wrap_config_checks error")
