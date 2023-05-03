8fbe4f9ec001704d605d85c2e4bf329c123938cb
kuqmua <serezhashmakov@outlook.com>
2023-05-04 02:02:38 +0300

    move telemetry mod to tufa_common

---
+++ src/repositories_types/tufa_server/telemetry/get_subscriber.rs
@@ -0,0 +1,24 @@
+pub fn get_subscriber<Sink>(
+    name: String,
+    env_filter: String,
+    sink: Sink,
+) -> impl tracing::Subscriber + Send + Sync
+where
+    // This "weird" syntax is a higher-ranked trait bound (HRTB)
+    // It basically means that Sink implements the `MakeWriter`
+    // trait for all choices of the lifetime parameter `'a`
+    // Check out https://doc.rust-lang.org/nomicon/hrtb.html
+    // for more details.
+    Sink: for<'a> tracing_subscriber::fmt::MakeWriter<'a> + Send + Sync + 'static,
+{
+    let env_filter =
+        tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| tracing_subscriber::EnvFilter::new(env_filter));
+    let formatting_layer = tracing_bunyan_formatter::BunyanFormattingLayer::new(name, sink);
+    {
+        use tracing_subscriber::layer::SubscriberExt;
+        tracing_subscriber::Registry::default()
+        .with(env_filter)
+        .with(tracing_bunyan_formatter::JsonStorageLayer)
+        .with(formatting_layer)
+    }
+}
--- src/repositories_types/tufa_server/telemetry/mod.rs
+++ src/repositories_types/tufa_server/telemetry/mod.rs
@@ -1,1 +1,4 @@
 pub mod init_subcriber_error_enum;
+pub mod get_subscriber;
+pub mod init_subscriber;
+pub mod spawn_blocking_with_tracing;
