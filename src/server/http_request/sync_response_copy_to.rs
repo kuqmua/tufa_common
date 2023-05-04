573b977e09a01c1d9efde58ee20d00e1c906b17e
kuqmua <serezhashmakov@outlook.com>
2023-05-05 00:40:56 +0300

    refactoring

--- src/server/http_request/sync_response_copy_to.rs
+++ src/server/http_request/sync_response_copy_to.rs
@@ -1,50 +1,27 @@
-use crate::common::where_was::WhereWas;
-use crate::global_variables::runtime::config::CONFIG;
-use crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES;
-use crate::traits::init_error_with_possible_trace::InitErrorWithPossibleTrace;
-use crate::traits::where_was_methods::WhereWasMethods;
-use impl_error_with_tracing::ImplErrorWithTracingFromCrate;
-use impl_get_source::ImplGetSourceFromCrate;
-use impl_get_where_was_origin_or_wrapper::ImplGetWhereWasOriginOrWrapperFromCrate;
-use init_error::InitErrorFromCrate;
-
-#[derive(
-    Debug,
-    InitErrorFromCrate,
-    ImplErrorWithTracingFromCrate,
-    ImplGetWhereWasOriginOrWrapperFromCrate,
-    ImplGetSourceFromCrate,
-)]
-pub struct SyncResponseCopyToOriginError {
-    source: reqwest::Error,
-    where_was: WhereWas,
+#[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
+pub enum SyncResponseCopyToErrorNamed<'a> {
+    CopyTo {
+        #[eo_display_foreign_type]
+        reqwest_error: reqwest::Error,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
 }
 
-pub fn sync_copy_to<W: ?Sized>(
+pub fn sync_copy_to<'a, W: ?Sized>(
     mut response: reqwest::blocking::Response,
     w: &mut W,
     source_place_type: &crate::config_mods::source_place_type::SourcePlaceType,
     should_trace: bool,
-) -> Result<u64, Box<SyncResponseCopyToOriginError>>
+) -> Result<u64, Box<SyncResponseCopyToErrorNamed<'a>>>
 where
     W: std::io::Write,
 {
     match response.copy_to(w) {
         Err(e) => Err(Box::new(
-            SyncResponseCopyToOriginError::init_error_with_possible_trace(
-                e,
-                WhereWas {
-                    time: std::time::SystemTime::now()
-                        .duration_since(std::time::UNIX_EPOCH)
-                        .expect("cannot convert time to unix_epoch"),
-                    file: String::from(file!()),
-                    line: line!(),
-                    column: column!(),
-                    git_info: crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES.clone(),
-                },
-                source_place_type,
-                should_trace,
-            ),
+            SyncResponseCopyToErrorNamed::CopyTo {
+                reqwest_error: e,
+                code_occurence: crate::code_occurence_tufa_common!() ,
+            }
         )),
         Ok(copy_to) => Ok(copy_to),
     }
