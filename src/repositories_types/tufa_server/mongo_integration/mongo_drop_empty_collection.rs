bc93d4f47ef12a308f2b88543046e3c4be8c2491
kuqmua <serezhashmakov@outlook.com>
2023-05-04 12:31:13 +0300

    mongo_drop_empty_collection move

--- src/repositories_types/tufa_server/mongo_integration/mongo_drop_empty_collection.rs
+++ src/repositories_types/tufa_server/mongo_integration/mongo_drop_empty_collection.rs
@@ -13,3 +13,61 @@ pub enum MongoDropEmptyCollectionErrorNamed<'a> {
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
     },
 }
+
+pub async fn mongo_drop_empty_collection<'a>(
+    mongo_url: String,
+    db_name: &'a str,
+    db_collection_name: String,
+) -> Result<(), Box<crate::repositories_types::tufa_server::mongo_integration::mongo_drop_empty_collection::MongoDropEmptyCollectionErrorNamed<'a>>> {
+    match mongodb::options::ClientOptions::parse(&mongo_url).await {
+        Err(e) => Err(Box::new(
+            crate::repositories_types::tufa_server::mongo_integration::mongo_drop_empty_collection::MongoDropEmptyCollectionErrorNamed::MongoDB {
+                mongodb: e,
+                code_occurence: crate::code_occurence_tufa_common!(),
+            }
+        )),
+        Ok(client_options) => {
+            match mongodb::Client::with_options(client_options) {//todo - maybe move mongodb::Client value to global variables?
+                Err(e) => Err(Box::new(
+                    crate::repositories_types::tufa_server::mongo_integration::mongo_drop_empty_collection::MongoDropEmptyCollectionErrorNamed::MongoDB {
+                        mongodb: e,
+                        code_occurence: crate::code_occurence_tufa_common!(),
+                    }
+                )),
+                Ok(client) => {
+                    let collection: mongodb::Collection<mongodb::bson::Document> =
+                        client.database(db_name).collection(&db_collection_name);
+                    match collection.count_documents(None, None).await {
+                    Err(e) => Err(Box::new(
+                        crate::repositories_types::tufa_server::mongo_integration::mongo_drop_empty_collection::MongoDropEmptyCollectionErrorNamed::MongoDB {
+                            mongodb: e,
+                            code_occurence: crate::code_occurence_tufa_common!(),
+                        }
+                    )),
+                    Ok(documents_number) => {
+                        if documents_number > 0 {
+                            Err(Box::new(
+                                crate::repositories_types::tufa_server::mongo_integration::mongo_drop_empty_collection::MongoDropEmptyCollectionErrorNamed::CollectionIsNotEmpty {
+                                    collection_name: db_collection_name,
+                                    collection_len: documents_number,
+                                    code_occurence: crate::code_occurence_tufa_common!(),
+                                }
+                            ))
+                        } else {
+                            if let Err(e) = collection.drop(None).await {
+                                return Err(Box::new(
+                                    crate::repositories_types::tufa_server::mongo_integration::mongo_drop_empty_collection::MongoDropEmptyCollectionErrorNamed::MongoDB {
+                                        mongodb: e,
+                                        code_occurence: crate::code_occurence_tufa_common!(),
+                                    }
+                                ));
+                            }
+                            Ok(())
+                        }
+                    }
+                }
+                }
+            }
+        }
+    }
+}
