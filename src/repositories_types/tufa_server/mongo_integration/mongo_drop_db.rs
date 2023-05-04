66f069c7fa1b57bde8ddcd6e085f148d1a36c784
kuqmua <serezhashmakov@outlook.com>
2023-05-04 11:51:44 +0300

    mongo_drop_db move

--- src/repositories_types/tufa_server/mongo_integration/mongo_drop_db.rs
+++ src/repositories_types/tufa_server/mongo_integration/mongo_drop_db.rs
@@ -6,3 +6,36 @@ pub enum MongoDropDbErrorNamed<'a> {
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
     },
 }
+
+pub async fn mongo_drop_db<'a>(
+    mongo_url: &'a str,
+    db_name: &'a str,
+) -> Result<(), Box<crate::repositories_types::tufa_server::mongo_integration::mongo_drop_db::MongoDropDbErrorNamed<'a>>> {
+    match  mongodb::options::ClientOptions::parse(mongo_url).await {
+        Err(e) => Err(Box::new(
+            crate::repositories_types::tufa_server::mongo_integration::mongo_drop_db::MongoDropDbErrorNamed::MongoDB {
+                mongodb: e,
+                code_occurence: crate::code_occurence_tufa_common!(),
+            }
+        )),
+        Ok(client_options) => match mongodb::Client::with_options(client_options) {
+            Err(e) => Err(Box::new(
+                crate::repositories_types::tufa_server::mongo_integration::mongo_drop_db::MongoDropDbErrorNamed::MongoDB {
+                    mongodb: e,
+                    code_occurence: crate::code_occurence_tufa_common!(),
+                }
+            )),
+            Ok(client) => {
+                if let Err(e) = client.database(db_name).drop(None).await {
+                    return Err(Box::new(
+                        crate::repositories_types::tufa_server::mongo_integration::mongo_drop_db::MongoDropDbErrorNamed::MongoDB {
+                            mongodb: e,
+                            code_occurence: crate::code_occurence_tufa_common!(),
+                        }
+                    ));
+                }
+                Ok(())
+            }
+        },
+    }
+}
