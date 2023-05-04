bb1f38387158e0e45cb7007176b5a0ac8da71e87
kuqmua <serezhashmakov@outlook.com>
2023-05-04 19:22:04 +0300

    cleaning

--- src/traits/is_enabled_prints.rs
+++ src/traits/is_enabled_prints.rs
@@ -85,27 +85,34 @@ where
     }
 }
 
-// use tufa_common::config_mods::print_type::PrintType;
-// 
-// use tufa_common::repositories_types::tufa_server::providers::provider_kind::provider_kind_enum::ProviderKindFromConfig;
-// use tufa_common::traits::print_type::PrintType;
+// use crate::config_mods::print_type::PrintType;
+
+// use crate::repositories_types::tufa_server::providers::provider_kind::provider_kind_enum::ProviderKindFromConfig;
+// use crate::traits::print_type::PrintType;
 
 // impl ProviderKind {
 //     pub fn is_prints_for_print_type_enabled(&self, pt: &PrintType) -> bool {
 //         match *pt {
-//             tufa_common::config_mods::print_type::PrintType::WarningHigh => pt.is_prints_enabled() && self.is_error_prints_enabled(),
-//             tufa_common::config_mods::print_type::PrintType::WarningHigh => {
+//             crate::config_mods::print_type::PrintType::WarningHigh => pt.is_prints_enabled() && self.is_error_prints_enabled(),
+//             crate::config_mods::print_type::PrintType::WarningHigh => {
 //                 pt.is_prints_enabled() && self.is_warning_high_prints_enabled()
 //             }
-//             tufa_common::config_mods::print_type::PrintType::WarningLow => pt.is_prints_enabled() && self.is_warning_low_prints_enabled(),
-//             tufa_common::config_mods::print_type::PrintType::Success => pt.is_prints_enabled() && self.is_success_prints_enabled(),
-//             tufa_common::config_mods::print_type::PrintType::PartialSuccess => {
+//             crate::config_mods::print_type::PrintType::WarningLow => pt.is_prints_enabled() && self.is_warning_low_prints_enabled(),
+//             crate::config_mods::print_type::PrintType::Success => pt.is_prints_enabled() && self.is_success_prints_enabled(),
+//             crate::config_mods::print_type::PrintType::PartialSuccess => {
 //                 pt.is_prints_enabled() && self.is_partial_success_prints_enabled()
 //             }
-//             tufa_common::config_mods::print_type::PrintType::TimeMeasurement => {
+//             crate::config_mods::print_type::PrintType::TimeMeasurement => {
 //                 pt.is_prints_enabled() && self.is_time_measurement_prints_enabled()
 //             }
-//             tufa_common::config_mods::print_type::PrintType::Info => pt.is_prints_enabled() && self.is_info_prints_enabled(),
+//             crate::config_mods::print_type::PrintType::Info => pt.is_prints_enabled() && self.is_info_prints_enabled(),
+//             PrintType::WarningHigh => todo!(),
+//             PrintType::WarningLow => todo!(),
+//             PrintType::Success => todo!(),
+//             PrintType::PartialSuccess => todo!(),
+//             PrintType::Cleaning => todo!(),
+//             PrintType::TimeMeasurement => todo!(),
+//             PrintType::Info => todo!(),
 //         }
 //     }
 // }
