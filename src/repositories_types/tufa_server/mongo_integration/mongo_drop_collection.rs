b8b4336200e67db6c4b2bade70eb745ebf384184
kuqmua <serezhashmakov@outlook.com>
2023-05-04 11:35:33 +0300

    mongo drop collection move

--- src/repositories_types/tufa_server/mongo_integration/mongo_drop_collection.rs
+++ src/repositories_types/tufa_server/mongo_integration/mongo_drop_collection.rs
@@ -6,3 +6,39 @@ pub enum MongoDropCollectionErrorNamed<'a> {
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
     },
 }
+
+pub async fn mongo_drop_collection<'a>(
+    mongo_url: &'a str,
+    db_name: &'a str,
+    db_collection_name: &'a str,
+) -> Result<(), Box<crate::repositories_types::tufa_server::mongo_integration::mongo_drop_collection::MongoDropCollectionErrorNamed<'a>>> {
+    match mongodb::options::ClientOptions::parse(mongo_url).await {
+        Err(e) => Err(Box::new(
+            crate::repositories_types::tufa_server::mongo_integration::mongo_drop_collection::MongoDropCollectionErrorNamed::MongoDB {
+                mongodb: e,
+                code_occurence: crate::code_occurence_tufa_common!(),
+            }
+        )),
+        Ok(client_options) => match mongodb::Client::with_options(client_options) {
+            Err(e) => Err(Box::new(
+                crate::repositories_types::tufa_server::mongo_integration::mongo_drop_collection::MongoDropCollectionErrorNamed::MongoDB {
+                    mongodb: e,
+                    code_occurence: crate::code_occurence_tufa_common!(),
+                }
+            )),
+            Ok(client) => {
+                let collection: mongodb::Collection<mongodb::bson::Document> =
+                    client.database(db_name).collection(db_collection_name);
+                if let Err(e) = collection.drop(None).await {
+                    return Err(Box::new(
+                        crate::repositories_types::tufa_server::mongo_integration::mongo_drop_collection::MongoDropCollectionErrorNamed::MongoDB {
+                            mongodb: e,
+                            code_occurence: crate::code_occurence_tufa_common!(),
+                        }
+                    ));
+                }
+                Ok(())
+            }
+        },
+    }
+}
