665f05e81858a71ce78d2534c61cf0372339ed1a
kuqmua <serezhashmakov@outlook.com>
2023-05-04 15:50:11 +0300

    move

--- src/repositories_types/tufa_server/postgres_integration/postgres_check_providers_links_tables_length_rows_equal_initialization_data_length.rs
+++ src/repositories_types/tufa_server/postgres_integration/postgres_check_providers_links_tables_length_rows_equal_initialization_data_length.rs
@@ -50,3 +50,87 @@ pub enum PostgresCheckProvidersLinksTablesLengthRowsEqualInitializationDataLengt
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
     },
 }
+
+pub async fn postgres_check_providers_links_tables_length_rows_equal_initialization_data_length<'a>(
+    providers_json_local_data_hashmap: &std::collections::HashMap<crate::repositories_types::tufa_server::providers::provider_kind::provider_kind_enum::ProviderKind, Vec<String>>,
+    db: &sqlx::Pool<sqlx::Postgres>,
+    should_trace: bool,
+) -> Result<
+    (),
+    Box<PostgresCheckProvidersLinksTablesLengthRowsEqualInitializationDataLengthWrapperErrorNamed<'a>>,
+>{
+    let count_provider_links_tables_tasks_vec =
+        providers_json_local_data_hashmap
+            .iter()
+            .map(|(pk, string_vec)| async move {
+                let query_string = format!(
+                    "SELECT count(*) AS exact_count FROM {};",
+                    {
+                        use crate::repositories_types::tufa_server::traits::provider_kind_methods::ProviderKindMethods;
+                        pk.get_postgres_table_name()
+                    }
+                );
+                (
+                    pk,
+                    string_vec,
+                    sqlx::query_as(&query_string).fetch_one(db).await,
+                )
+            });
+    let count_provider_links_tables_error_vec: Vec<(
+        &crate::repositories_types::tufa_server::providers::provider_kind::provider_kind_enum::ProviderKind,
+        &Vec<String>,
+        Result<(i64,), sqlx::Error>,
+    )> = futures::future::join_all(count_provider_links_tables_tasks_vec).await;
+    let mut count_provider_links_tables_error_hashmap: std::collections::HashMap<std::string::String, PostgresCheckProvidersLinksTablesLengthRowsEqualInitializationDataLengthWrapperErrorSelectCountOriginErrorSqlxUnnamed> =
+        std::collections::HashMap::new();
+    let mut provider_links_tables_rows_length_not_equal_error_hashmap: std::collections::HashMap<
+        std::string::String,
+        PostgresCheckProvidersLinksTablesLengthRowsEqualInitializationDataLengthWrapperErrorSelectCountOriginErrorUnnamed,
+    > = std::collections::HashMap::new();
+    for (pk, string_vec, result) in count_provider_links_tables_error_vec {
+        match result {
+            Err(e) => {
+                count_provider_links_tables_error_hashmap.insert(
+                    pk.to_string(), 
+                 PostgresCheckProvidersLinksTablesLengthRowsEqualInitializationDataLengthWrapperErrorSelectCountOriginErrorSqlxUnnamed::Postgres(
+                        PostgresCheckProvidersLinksTablesLengthRowsEqualInitializationDataLengthWrapperErrorSelectCountOriginErrorSqlxNamed::Postgres {
+                            error: e,
+                            code_occurence: crate::code_occurence_tufa_common!()
+                        }
+                    )
+                );
+            }
+            Ok((count,)) => {
+                if count != string_vec.len() as i64 {
+                    provider_links_tables_rows_length_not_equal_error_hashmap.insert(
+                        pk.to_string(),
+                        PostgresCheckProvidersLinksTablesLengthRowsEqualInitializationDataLengthWrapperErrorSelectCountOriginErrorUnnamed::Postgres(
+                            PostgresCheckProvidersLinksTablesLengthRowsEqualInitializationDataLengthWrapperErrorSelectCountOriginErrorNamed::Postgres {
+                                table_rows_length: count,
+                                initialization_data_length: string_vec.len(),
+                                code_occurence: crate::code_occurence_tufa_common!()
+                            }
+                        )
+                    );
+                }
+            }
+        }
+    }
+    if !count_provider_links_tables_error_hashmap.is_empty() {
+        return Err(Box::new(
+            PostgresCheckProvidersLinksTablesLengthRowsEqualInitializationDataLengthWrapperErrorNamed::SelectCountOrigin {
+                inner_errors: count_provider_links_tables_error_hashmap,
+                code_occurence: crate::code_occurence_tufa_common!()
+            }
+        ));
+    }
+    if !provider_links_tables_rows_length_not_equal_error_hashmap.is_empty() {
+        return Err(Box::new(
+            PostgresCheckProvidersLinksTablesLengthRowsEqualInitializationDataLengthWrapperErrorNamed::ProviderLinksTablesRowsLengthNotEqualOrigin {
+                inner_errors: provider_links_tables_rows_length_not_equal_error_hashmap,
+                code_occurence: crate::code_occurence_tufa_common!()
+            }
+        ));
+    }
+    Ok(())
+}
