5e2dcec27d9055711d43964f79ffc938790885a8
kuqmua <serezhashmakov@outlook.com>
2023-05-04 10:59:37 +0300

    mongo_client_options_parse refactoring

--- src/repositories_types/tufa_server/mongo_integration/mongo_client_options_parse.rs
+++ src/repositories_types/tufa_server/mongo_integration/mongo_client_options_parse.rs
@@ -6,3 +6,20 @@ pub enum MongoClientOptionsParseOriginErrorNamed<'a> {
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
     },
 }
+
+pub async fn mongo_client_options_parse<'a>() -> Result<mongodb::options::ClientOptions, Box<crate::repositories_types::tufa_server::mongo_integration::mongo_client_options_parse::MongoClientOptionsParseOriginErrorNamed<'a>>>{
+    match mongodb::options::ClientOptions::parse(&
+        {
+            use crate::traits::get_mongo_url::GetMongoUrl;
+            crate::global_variables::runtime::config::CONFIG.get_mongo_url()
+        }
+        ).await {
+        Err(e) => Err(Box::new(
+            crate::repositories_types::tufa_server::mongo_integration::mongo_client_options_parse::MongoClientOptionsParseOriginErrorNamed::Mongo {
+                error: e,
+                code_occurence: crate::code_occurence_tufa_common!(),
+            },
+        )),
+        Ok(client_options) => Ok(client_options),
+    }
+}
