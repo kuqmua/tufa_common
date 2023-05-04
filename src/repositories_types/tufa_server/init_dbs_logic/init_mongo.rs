713183d4b1c89313fae06db361dfb49b51b0fa2e
kuqmua <serezhashmakov@outlook.com>
2023-05-04 22:59:34 +0300

    move

--- src/repositories_types/tufa_server/init_dbs_logic/init_mongo.rs
+++ src/repositories_types/tufa_server/init_dbs_logic/init_mongo.rs
@@ -21,3 +21,51 @@ pub enum InitMongoErrorNamed<'a> {
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
     }
 }
+
+pub async fn init_mongo<'a>(
+    providers_json_local_data_hashmap: std::collections::HashMap<crate::repositories_types::tufa_server::providers::provider_kind::provider_kind_enum::ProviderKind, Vec<String>>,
+) -> Result<(), Box<crate::repositories_types::tufa_server::init_dbs_logic::init_mongo::InitMongoErrorNamed<'a>>>{
+    match crate::repositories_types::tufa_server::mongo_integration::mongo_client_options_parse::mongo_client_options_parse().await {
+        Err(e) => Err(Box::new(
+            crate::repositories_types::tufa_server::init_dbs_logic::init_mongo::InitMongoErrorNamed::ClientOptionsParse {
+                client_options_parse: *e,
+                code_occurence: crate::code_occurence_tufa_common!()
+            }
+        )),
+        Ok(client_options) => match crate::repositories_types::tufa_server::mongo_integration::mongo_client_with_options::mongo_client_with_options(client_options) {
+            Err(e) => Err(Box::new(
+                crate::repositories_types::tufa_server::init_dbs_logic::init_mongo::InitMongoErrorNamed::ClientWithOptions {
+                    client_with_options: *e,
+                    code_occurence: crate::code_occurence_tufa_common!()
+                }
+            )),
+            Ok(client) => {
+                let db = client.database(&crate::global_variables::runtime::config::CONFIG.mongo_providers_link_parts_db_name);
+                if let Err(e) = crate::repositories_types::tufa_server::mongo_integration::mongo_check_collection_is_not_empty::mongo_check_collections_is_not_empty(
+                    providers_json_local_data_hashmap.clone(),
+                    &db,
+                )
+                .await
+                {
+                    return Err(Box::new(
+                        crate::repositories_types::tufa_server::init_dbs_logic::init_mongo::InitMongoErrorNamed::CollectionIsNotEmpty {
+                            collection_is_not_empty: *e,
+                            code_occurence: crate::code_occurence_tufa_common!()
+                        }
+                    ));
+                }
+                if let Err(e) =
+                    crate::repositories_types::tufa_server::mongo_integration::mongo_insert_many::mongo_insert_many(providers_json_local_data_hashmap, db, false).await
+                {
+                    return Err(Box::new(
+                        crate::repositories_types::tufa_server::init_dbs_logic::init_mongo::InitMongoErrorNamed::InsertMany {
+                            insert_many: *e,
+                            code_occurence: crate::code_occurence_tufa_common!()
+                        }
+                    ));
+                }
+                Ok(())
+            }
+        },
+    }
+}
--- src/repositories_types/tufa_server/init_dbs_logic/init_postgres.rs
+++ src/repositories_types/tufa_server/init_dbs_logic/init_postgres.rs
@@ -31,3 +31,86 @@ pub enum PostgresInitErrorNamed<'a> {
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
     },
 }
+
+pub async fn init_postgres<'a>(
+    providers_json_local_data_hashmap: std::collections::HashMap<crate::repositories_types::tufa_server::providers::provider_kind::provider_kind_enum::ProviderKind, Vec<String>>,
+) -> Result<(), Box<crate::repositories_types::tufa_server::init_dbs_logic::init_postgres::PostgresInitErrorNamed<'a>>> {
+    match crate::repositories_types::tufa_server::postgres_integration::postgres_establish_connection::postgres_establish_connection(providers_json_local_data_hashmap.len() as u32).await {
+        Err(e) => Err(Box::new(
+            crate::repositories_types::tufa_server::init_dbs_logic::init_postgres::PostgresInitErrorNamed::EstablishConnection { 
+                establish_connection: *e, 
+                code_occurence: crate::code_occurence_tufa_common!()
+            }
+        )),
+        Ok(pool) => {
+            if let Err(e) = crate::repositories_types::tufa_server::postgres_integration::postgres_create_providers_tables_if_not_exists::postgres_create_providers_tables_if_not_exists(
+                &providers_json_local_data_hashmap,
+                &pool,
+            )
+            .await
+            {
+                return Err(Box::new(
+                    crate::repositories_types::tufa_server::init_dbs_logic::init_postgres::PostgresInitErrorNamed::CreateTableQueries { 
+                        create_table_queries: *e,
+                        code_occurence: crate::code_occurence_tufa_common!() 
+                    }
+                ));
+            }
+            if let Err(e) = crate::repositories_types::tufa_server::postgres_integration::postgres_check_providers_link_parts_tables_are_empty::postgres_check_providers_link_parts_tables_are_empty(
+                &providers_json_local_data_hashmap,
+                &pool,
+            )
+            .await
+            {
+                return Err(Box::new(
+                    crate::repositories_types::tufa_server::init_dbs_logic::init_postgres::PostgresInitErrorNamed::CheckProviderLinksTablesAreEmpty { 
+                        check_provider_links_tables_are_empty: *e, 
+                        code_occurence: crate::code_occurence_tufa_common!()  
+                    }
+                ));
+            }
+            if let Err(e) = crate::repositories_types::tufa_server::postgres_integration::postgres_delete_all_from_providers_link_parts_tables::postgres_delete_all_from_providers_link_parts_tables(
+                &providers_json_local_data_hashmap,
+                &pool,
+                false,
+            )
+            .await
+            {
+                return Err(Box::new(
+                    crate::repositories_types::tufa_server::init_dbs_logic::init_postgres::PostgresInitErrorNamed::DeleteAllFromProvidersTables { 
+                        delete_all_from_providers_tables: *e, 
+                        code_occurence: crate::code_occurence_tufa_common!()   
+                    }
+                ));
+            }
+            if let Err(e) = crate::repositories_types::tufa_server::postgres_integration::postgres_check_providers_links_tables_length_rows_equal_initialization_data_length::postgres_check_providers_links_tables_length_rows_equal_initialization_data_length(
+                &providers_json_local_data_hashmap,
+                &pool,
+                false,
+            )
+            .await {
+                return Err(Box::new(
+                    crate::repositories_types::tufa_server::init_dbs_logic::init_postgres::PostgresInitErrorNamed::CheckProvidersLinksTablesLengthRowsEqualInitializationDataLength {          
+                        check_providers_links_tables_length_rows_equal_initialization_data_length: *e, 
+                        code_occurence: crate::code_occurence_tufa_common!()   
+                    }
+            ));
+            }
+            if let Err(e) = crate::repositories_types::tufa_server::postgres_integration::postgres_insert_link_parts_into_providers_tables::postgres_insert_link_parts_into_providers_tables(
+                &providers_json_local_data_hashmap,
+                &pool,
+                false,
+            )
+            .await
+            {
+                return Err(Box::new(
+                    crate::repositories_types::tufa_server::init_dbs_logic::init_postgres::PostgresInitErrorNamed::InsertLinkPartsIntoProvidersTables { 
+                        insert_link_parts_into_providers_tables: *e, 
+                        code_occurence: crate::code_occurence_tufa_common!()    
+                    }
+                ));
+            }
+            Ok(())
+        }
+    }
+}
