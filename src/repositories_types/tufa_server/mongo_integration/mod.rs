63c6346087df9577881f51f6e79d5dc0fdf339a2
kuqmua <serezhashmakov@outlook.com>
2023-05-04 15:34:10 +0300

    move

--- src/repositories_types/tufa_server/mongo_integration/mod.rs
+++ src/repositories_types/tufa_server/mongo_integration/mod.rs
@@ -1,4 +1,5 @@
 pub mod mongo_check_availability;
+pub mod mongo_check_collection_is_empty;
 pub mod mongo_check_collection_is_not_empty;
 pub mod mongo_client_options_parse;
 pub mod mongo_client_with_options;
--- src/server/mongo/mod.rs
+++ src/server/mongo/mod.rs
@@ -1,3 +1,2 @@
-pub mod mongo_check_collection_is_empty;
 pub mod mongo_check_db_is_empty;
 pub mod mongo_insert_docs_in_empty_collection;
