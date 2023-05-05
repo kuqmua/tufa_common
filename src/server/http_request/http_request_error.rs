85f9c547c34ca71b27da897dc2018774254c26b5
kuqmua <serezhashmakov@outlook.com>
2023-05-05 03:41:14 +0300

    next steps

--- src/server/http_request/http_request_error.rs
+++ src/server/http_request/http_request_error.rs
@@ -17,3 +17,13 @@ pub struct HttpRequestOriginError {
     pub source: reqwest::Error,
     pub where_was: WhereWas,
 }
+
+//whats duplication of HttpRequestOriginError. need to use this instead of HttpRequestOriginError later
+#[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
+pub enum HttpRequestOriginErrorNamed<'a> {
+    Reqwest {
+        #[eo_display_foreign_type]
+        reqwest_error: reqwest::Error,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+}
