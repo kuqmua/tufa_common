581f5cac3ffab36a4cc9f19379e7dea3de6afeec
kuqmua <serezhashmakov@outlook.com>
2023-05-05 11:52:24 +0300

    cleaning

--- src/server/file_system/create_dir_if_it_doesnt_exist.rs
+++ src/server/file_system/create_dir_if_it_doesnt_exist.rs
@@ -1,52 +1,26 @@
-use crate::common::where_was::WhereWas;
-use crate::global_variables::runtime::config::CONFIG;
-use crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES;
-use crate::traits::init_error_with_possible_trace::InitErrorWithPossibleTrace;
-use crate::traits::where_was_methods::WhereWasMethods;
-use impl_display_for_error::ImplDisplayForError;
-use impl_error_with_tracing::ImplErrorWithTracingFromCrate;
-use impl_get_source::ImplGetSourceFromCrate;
-use impl_get_where_was_origin_or_wrapper::ImplGetWhereWasOriginOrWrapperFromCrate;
-use init_error::InitErrorFromCrate;
-use std::io::Error;
-
-#[derive(
-    Debug,
-    InitErrorFromCrate,
-    ImplErrorWithTracingFromCrate,
-    ImplGetWhereWasOriginOrWrapperFromCrate,
-    ImplGetSourceFromCrate,
-    ImplDisplayForError,
-)]
-pub struct CreateDirIfItDoesntExistOriginError {
-    pub source: Error,
-    pub where_was: WhereWas,
+#[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
+pub enum CreateDirIfItDoesntExistErrorNamed<'a> {
+    CreateDirAll {
+        #[eo_display]
+        error: std::io::Error,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    }
 }
 
-pub fn create_dir_if_it_doesnt_exist(
+pub fn create_dir_if_it_doesnt_exist<'a>(
     path: &str,
     source_place_type: &crate::config_mods::source_place_type::SourcePlaceType,
     should_trace: bool,
-) -> Result<(), Box<CreateDirIfItDoesntExistOriginError>> {
+) -> Result<(), Box<CreateDirIfItDoesntExistErrorNamed<'a>>> {
     if std::path::Path::new(path).exists() {
         return Ok(());
     }
     if let Err(e) = std::fs::create_dir_all(path) {
         return Err(Box::new(
-            CreateDirIfItDoesntExistOriginError::init_error_with_possible_trace(
-                e,
-                WhereWas {
-                    time: std::time::SystemTime::now()
-                        .duration_since(std::time::UNIX_EPOCH)
-                        .expect("cannot convert time to unix_epoch"),
-                    file: String::from(file!()),
-                    line: line!(),
-                    column: column!(),
-                    git_info: crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES.clone(),
-                },
-                source_place_type,
-                should_trace,
-            ),
+            CreateDirIfItDoesntExistErrorNamed::CreateDirAll {
+                error: e,
+                code_occurence: crate::code_occurence_tufa_common!()
+            }
         ));
     }
     Ok(())
