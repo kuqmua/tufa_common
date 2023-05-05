b7f842b1fb040a79c8bf8fc66f595bb8f3b04831
kuqmua <serezhashmakov@outlook.com>
2023-05-05 13:31:36 +0300

    refactoring

--- src/server/file_system/write_bytes_into_file/write_bytes_into_file_sync.rs
+++ src/server/file_system/write_bytes_into_file/write_bytes_into_file_sync.rs
@@ -1,108 +1,55 @@
-use crate::common::where_was::WhereWas;
-use crate::global_variables::runtime::config::CONFIG;
-use crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES;
-use crate::traits::init_error_with_possible_trace::InitErrorWithPossibleTrace;
-use crate::traits::where_was_methods::WhereWasMethods;
-use impl_display_for_error::ImplDisplayForError;
-use impl_error_with_tracing::ImplErrorWithTracingFromCrate;
-use impl_get_source::ImplGetSourceFromCrate;
-use impl_get_where_was_origin_or_wrapper::ImplGetWhereWasOriginOrWrapperFromCrate;
-use init_error::InitErrorFromCrate;
-use std::io::Write;
-
-#[derive(
-    Debug,
-    InitErrorFromCrate,
-    ImplErrorWithTracingFromCrate,
-    ImplGetWhereWasOriginOrWrapperFromCrate,
-    ImplGetSourceFromCrate,
-    ImplDisplayForError,
-)]
-pub struct WriteBytesIntoFileSyncOriginError {
-    pub source: std::io::Error,
-    pub where_was: WhereWas,
+#[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
+pub enum WriteBytesIntoFileSyncErrorNamed<'a> {
+    StdIo {
+        #[eo_display]
+        std_io_error: std::io::Error,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    }
 }
 
-pub fn write_bytes_into_file_sync(
-    path: &std::path::Path,
-    bytes: &[u8],
-    source_place_type: &crate::config_mods::source_place_type::SourcePlaceType,
+pub fn write_bytes_into_file_sync<'a>(
+    path: &'a std::path::Path,
+    bytes: std::string::String,
+    source_place_type: &'a crate::config_mods::source_place_type::SourcePlaceType,
     should_trace: bool,
-) -> Result<(), Box<WriteBytesIntoFileSyncOriginError>> {
+) -> Result<(), Box<WriteBytesIntoFileSyncErrorNamed<'a>>> {
     if let Some(prefix) = path.parent() {
         if let Err(e) = std::fs::create_dir_all(prefix) {
             return Err(Box::new(
-                WriteBytesIntoFileSyncOriginError::init_error_with_possible_trace(
-                    e,
-                    WhereWas {
-                        time: std::time::SystemTime::now()
-                            .duration_since(std::time::UNIX_EPOCH)
-                            .expect("cannot convert time to unix_epoch"),
-                        file: String::from(file!()),
-                        line: line!(),
-                        column: column!(),
-                        git_info: crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES.clone(),
-                    },
-                    source_place_type,
-                    should_trace,
-                ),
+                WriteBytesIntoFileSyncErrorNamed::StdIo {
+                    std_io_error: e,
+                    code_occurence: crate::code_occurence_tufa_common!()
+                }
             ));
         }
     }
     match std::fs::File::create(path) {
         Err(e) => {
             return Err(Box::new(
-                WriteBytesIntoFileSyncOriginError::init_error_with_possible_trace(
-                    e,
-                    WhereWas {
-                        time: std::time::SystemTime::now()
-                            .duration_since(std::time::UNIX_EPOCH)
-                            .expect("cannot convert time to unix_epoch"),
-                        file: String::from(file!()),
-                        line: line!(),
-                        column: column!(),
-                        git_info: crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES.clone(),
-                    },
-                    source_place_type,
-                    should_trace,
-                ),
+                WriteBytesIntoFileSyncErrorNamed::StdIo {
+                    std_io_error: e,
+                    code_occurence: crate::code_occurence_tufa_common!()
+                }
             ));
         }
         Ok(mut file) => {
-            if let Err(e) = file.write_all(bytes) {
+            if let Err(e) = {
+                use std::io::Write;
+                file.write_all(bytes.as_bytes())
+            } {
                 return Err(Box::new(
-                    WriteBytesIntoFileSyncOriginError::init_error_with_possible_trace(
-                        e,
-                        WhereWas {
-                            time: std::time::SystemTime::now()
-                                .duration_since(std::time::UNIX_EPOCH)
-                                .expect("cannot convert time to unix_epoch"),
-                            file: String::from(file!()),
-                            line: line!(),
-                            column: column!(),
-                            git_info: crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES.clone(),
-                        },
-                        source_place_type,
-                        should_trace,
-                    ),
+                    WriteBytesIntoFileSyncErrorNamed::StdIo {
+                        std_io_error: e,
+                        code_occurence: crate::code_occurence_tufa_common!()
+                    }
                 ));
             }
             if let Err(e) = file.sync_all() {
                 return Err(Box::new(
-                    WriteBytesIntoFileSyncOriginError::init_error_with_possible_trace(
-                        e,
-                        WhereWas {
-                            time: std::time::SystemTime::now()
-                                .duration_since(std::time::UNIX_EPOCH)
-                                .expect("cannot convert time to unix_epoch"),
-                            file: String::from(file!()),
-                            line: line!(),
-                            column: column!(),
-                            git_info: crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES.clone(),
-                        },
-                        source_place_type,
-                        should_trace,
-                    ),
+                    WriteBytesIntoFileSyncErrorNamed::StdIo {
+                        std_io_error: e,
+                        code_occurence: crate::code_occurence_tufa_common!()
+                    }
                 ));
             }
             Ok(())
--- src/server/file_system/write_json_into_file/write_json_into_file_sync.rs
+++ src/server/file_system/write_json_into_file/write_json_into_file_sync.rs
@@ -1,82 +1,43 @@
-use crate::common::where_was::WhereWas;
-use crate::global_variables::runtime::config::CONFIG;
-use crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES;
-use crate::traits::get_source::GetSource;
-use crate::traits::init_error_with_possible_trace::InitErrorWithPossibleTrace;
-use crate::traits::where_was_methods::WhereWasMethods;
-use impl_display_for_error::ImplDisplayForError;
-use impl_error_with_tracing::ImplErrorWithTracingFromCrate;
-use impl_get_source::ImplGetSourceFromCrate;
-use impl_get_where_was_origin_or_wrapper::ImplGetWhereWasOriginOrWrapperFromCrate;
-use init_error::InitErrorFromCrate;
-
-#[derive(
-    Debug,
-    InitErrorFromCrate,
-    ImplErrorWithTracingFromCrate,
-    ImplGetWhereWasOriginOrWrapperFromCrate,
-    ImplGetSourceFromCrate,
-    ImplDisplayForError,
-)]
-pub struct WriteJsonIntoFileSyncWrapperError {
-    source: WriteJsonIntoFileSyncOriginErrorEnum,
-    where_was: WhereWas,
+#[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
+pub enum WriteJsonIntoFileSyncErrorNamed<'a> {
+    SerdeJson{
+        #[eo_display]
+        serde_json: serde_json::Error,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    WriteBytesIntoFile {
+        #[eo_error_occurence]
+        write_bytes_into_file: crate::server::file_system::write_bytes_into_file::write_bytes_into_file_sync::WriteBytesIntoFileSyncErrorNamed<'a>,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
 }
 
-#[derive(
-    Debug, ImplGetSourceFromCrate, ImplDisplayForError, ImplGetWhereWasOriginOrWrapperFromCrate,
-)]
-pub enum WriteJsonIntoFileSyncOriginErrorEnum {
-    SerdeJsonOrigin(serde_json::Error),
-    StdIoOrigin(std::io::Error),
-}
-
-pub fn write_json_into_file_async(
-    path: &std::path::Path,
+pub fn write_json_into_file_async<'a>(
+    path: &'a std::path::Path,
     json_object: serde_json::Value,
-    source_place_type: &crate::config_mods::source_place_type::SourcePlaceType,
+    source_place_type: &'a crate::config_mods::source_place_type::SourcePlaceType,
     should_trace: bool,
-) -> Result<(), Box<WriteJsonIntoFileSyncWrapperError>> {
+) -> Result<(), Box<WriteJsonIntoFileSyncErrorNamed<'a>>> {
     match serde_json::to_string_pretty(&json_object) {
         Err(e) => Err(Box::new(
-            WriteJsonIntoFileSyncWrapperError::init_error_with_possible_trace(
-                WriteJsonIntoFileSyncOriginErrorEnum::SerdeJsonOrigin(e),
-                WhereWas {
-                    time: std::time::SystemTime::now()
-                        .duration_since(std::time::UNIX_EPOCH)
-                        .expect("cannot convert time to unix_epoch"),
-                    file: String::from(file!()),
-                    line: line!(),
-                    column: column!(),
-                    git_info: crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES.clone(),
-                },
-                source_place_type,
-                should_trace,
-            ),
+            WriteJsonIntoFileSyncErrorNamed::SerdeJson{
+                serde_json: e,
+                code_occurence: crate::code_occurence_tufa_common!()
+            }
         )),
         Ok(stringified_json) => {
             if let Err(e) = crate::server::file_system::write_bytes_into_file::write_bytes_into_file_sync::write_bytes_into_file_sync(
                 path,
-                stringified_json.as_bytes(),
+                stringified_json,
                 source_place_type,
                 should_trace,
             ) {
                 return Err(
                     Box::new(
-                        WriteJsonIntoFileSyncWrapperError::init_error_with_possible_trace(
-                            WriteJsonIntoFileSyncOriginErrorEnum::StdIoOrigin(e.source),
-                            WhereWas {
-                                time: std::time::SystemTime::now()
-                                .duration_since(std::time::UNIX_EPOCH)
-                                .expect("cannot convert time to unix_epoch"),
-                                file: String::from(file!()),
-                                line: line!(),
-                                column: column!(),
-                                git_info: crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES.clone(),
-                            },
-                            source_place_type,
-                           should_trace,
-                        ),
+                        WriteJsonIntoFileSyncErrorNamed::WriteBytesIntoFile{
+                            write_bytes_into_file: *e,
+                            code_occurence: crate::code_occurence_tufa_common!()
+                        }
                     )
                 );
             }
