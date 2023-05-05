726c249a66861f8cda69ce5a358e2a82c2c7486b
kuqmua <serezhashmakov@outlook.com>
2023-05-05 11:37:05 +0300

    cleaning

--- src/repositories_types/tufa_server/providers/providers_info/get_local_providers_link_parts.rs
+++ src/repositories_types/tufa_server/providers/providers_info/get_local_providers_link_parts.rs
@@ -1,26 +1,8 @@
-use crate::global_variables::runtime::config::CONFIG;
-use crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES;
-// use crate::repositories_types::tufa_server::providers::provider_kind::provider_kind_enum::GetLinkPartsFromLocalJsonFileErrorNamed;
-
-use crate::repositories_types::tufa_server::traits::provider_kind_methods::ProviderKindMethods;
-use futures::future::join_all;
-use impl_error_with_tracing::ImplErrorWithTracingFromCrate;
-use impl_get_source::ImplGetSourceFromCrate;
-use impl_get_where_was_origin_or_wrapper::ImplGetWhereWasOriginOrWrapperFromCrate;
-use init_error::InitErrorFromCrate;
-use std::collections::HashMap;
-use crate::common::where_was::WhereWas;
-use crate::traits::get_log_where_was::GetLogWhereWas;
-use crate::traits::get_source::GetSource;
-use crate::traits::init_error_with_possible_trace::InitErrorWithPossibleTrace;
-use crate::traits::where_was_methods::WhereWasMethods;
-use valuable::Valuable;
-
 #[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
 pub enum GetLocalProvidersLinkPartsErrorNamed<'a> {
     GetLinkPartsFromLocalJsonFile {
         #[eo_hashmap_key_display_with_serialize_deserialize_value_error_occurence]
-        get_link_parts_from_localJson_file: HashMap<std::string::String, GetLocalProvidersLinkPartsErrorUnnamed<'a>>,
+        get_link_parts_from_localJson_file: std::collections::HashMap<std::string::String, GetLocalProvidersLinkPartsErrorUnnamed<'a>>,
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
     },
 }
@@ -30,16 +12,19 @@ pub enum GetLocalProvidersLinkPartsErrorUnnamed<'a> {
     GetLinkPartsFromLocalJsonFile(crate::repositories_types::tufa_server::providers::provider_kind::provider_kind_enum::GetLinkPartsFromLocalJsonFileErrorNamed<'a>),
 }
 
-#[derive(Clone, Debug, Valuable)]
+#[derive(Clone, Debug, valuable::Valuable)]
 pub struct TracingVec {
     pub vec: Vec<String>,
 }
 
 pub async fn get_local_providers_link_parts<'a>(
     should_trace: bool,
-) -> Result<HashMap<crate::repositories_types::tufa_server::providers::provider_kind::provider_kind_enum::ProviderKind, Vec<String>>, Box<GetLocalProvidersLinkPartsErrorNamed<'a>>> {
-    let result_vec = join_all(
-        crate::repositories_types::tufa_server::providers::provider_kind::provider_kind_enum::ProviderKind::get_enabled_providers_vec() //maybe its not exactly correct
+) -> Result<std::collections::HashMap<crate::repositories_types::tufa_server::providers::provider_kind::provider_kind_enum::ProviderKind, Vec<String>>, Box<GetLocalProvidersLinkPartsErrorNamed<'a>>> {
+    let result_vec = futures::future::join_all(
+            {
+                use crate::repositories_types::tufa_server::traits::provider_kind_methods::ProviderKindMethods;
+                crate::repositories_types::tufa_server::providers::provider_kind::provider_kind_enum::ProviderKind::get_enabled_providers_vec() //maybe its not exactly correct
+            }
             .into_iter()
             .map(|pk| async move {
                 (
@@ -49,10 +34,10 @@ pub async fn get_local_providers_link_parts<'a>(
             }),
     )
     .await;
-    let mut errors_hashmap: HashMap<std::string::String, GetLocalProvidersLinkPartsErrorUnnamed<'a>> =
-        HashMap::new();
-    let mut success_hashmap: HashMap<crate::repositories_types::tufa_server::providers::provider_kind::provider_kind_enum::ProviderKind, Vec<String>> =
-        HashMap::with_capacity(result_vec.len());
+    let mut errors_hashmap: std::collections::HashMap<std::string::String, GetLocalProvidersLinkPartsErrorUnnamed<'a>> =
+        std::collections::HashMap::new();
+    let mut success_hashmap: std::collections::HashMap<crate::repositories_types::tufa_server::providers::provider_kind::provider_kind_enum::ProviderKind, Vec<String>> =
+        std::collections::HashMap::with_capacity(result_vec.len());
     for (pk, result) in result_vec {
         match result {
             Err(e) => {
