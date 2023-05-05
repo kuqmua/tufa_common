f83f610bd6329d3c713cc39e5b803dafc64595c2
kuqmua <serezhashmakov@outlook.com>
2023-05-05 12:58:59 +0300

    refactoring

--- src/server/file_system/write_bytes_into_file/write_bytes_into_file_async_tokio.rs
+++ src/server/file_system/write_bytes_into_file/write_bytes_into_file_async_tokio.rs
@@ -1,26 +1,10 @@
-use crate::common::where_was::WhereWas;
-use crate::global_variables::runtime::config::CONFIG;
-use crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES;
-use crate::traits::init_error_with_possible_trace::InitErrorWithPossibleTrace;
-use crate::traits::where_was_methods::WhereWasMethods;
-use impl_display_for_error::ImplDisplayForError;
-use impl_error_with_tracing::ImplErrorWithTracingFromCrate;
-use impl_get_source::ImplGetSourceFromCrate;
-use impl_get_where_was_origin_or_wrapper::ImplGetWhereWasOriginOrWrapperFromCrate;
-use init_error::InitErrorFromCrate;
-use tokio::io::AsyncWriteExt;
-
-#[derive(
-    Debug,
-    InitErrorFromCrate,
-    ImplErrorWithTracingFromCrate,
-    ImplGetWhereWasOriginOrWrapperFromCrate,
-    ImplGetSourceFromCrate,
-    ImplDisplayForError,
-)]
-pub struct WriteBytesIntoFileAsyncTokioOriginError {
-    pub source: std::io::Error,
-    pub where_was: WhereWas,
+#[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
+pub enum WriteBytesIntoFileAsyncTokioErrorNamed<'a> {
+    StdIoError {
+        #[eo_display]
+        std_io_error: std::io::Error,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    }
 }
 
 pub async fn write_bytes_into_file_async_tokio<'a>(
@@ -28,63 +12,36 @@ pub async fn write_bytes_into_file_async_tokio<'a>(
     bytes: &[u8],
     source_place_type: &crate::config_mods::source_place_type::SourcePlaceType,
     should_trace: bool,
-) -> Result<(), Box<WriteBytesIntoFileAsyncTokioOriginError>> {
+) -> Result<(), Box<WriteBytesIntoFileAsyncTokioErrorNamed<'a>>> {
     if let Some(prefix) = path.parent() {
         if let Err(e) = std::fs::create_dir_all(prefix) {
             return Err(Box::new(
-                WriteBytesIntoFileAsyncTokioOriginError::init_error_with_possible_trace(
-                    e,
-                    WhereWas {
-                        time: std::time::SystemTime::now()
-                            .duration_since(std::time::UNIX_EPOCH)
-                            .expect("cannot convert time to unix_epoch"),
-                        file: String::from(file!()),
-                        line: line!(),
-                        column: column!(),
-                        git_info: crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES.clone(),
-                    },
-                    source_place_type,
-                    should_trace,
-                ),
+                WriteBytesIntoFileAsyncTokioErrorNamed::StdIoError {        
+                    std_io_error: e,
+                    code_occurence: crate::code_occurence_tufa_common!()
+                }
             ));
         }
     }
     match tokio::fs::File::open(path).await {
         Err(e) => {
             return Err(Box::new(
-                WriteBytesIntoFileAsyncTokioOriginError::init_error_with_possible_trace(
-                    e,
-                    WhereWas {
-                        time: std::time::SystemTime::now()
-                            .duration_since(std::time::UNIX_EPOCH)
-                            .expect("cannot convert time to unix_epoch"),
-                        file: String::from(file!()),
-                        line: line!(),
-                        column: column!(),
-                        git_info: crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES.clone(),
-                    },
-                    source_place_type,
-                    should_trace,
-                ),
+                WriteBytesIntoFileAsyncTokioErrorNamed::StdIoError {        
+                    std_io_error: e,
+                    code_occurence: crate::code_occurence_tufa_common!()
+                }
             ));
         }
         Ok(mut file) => {
-            if let Err(e) = file.write_all(bytes).await {
+            if let Err(e) = {
+                use tokio::io::AsyncWriteExt;
+                file.write_all(bytes)
+            }.await {
                 return Err(Box::new(
-                    WriteBytesIntoFileAsyncTokioOriginError::init_error_with_possible_trace(
-                        e,
-                        WhereWas {
-                            time: std::time::SystemTime::now()
-                                .duration_since(std::time::UNIX_EPOCH)
-                                .expect("cannot convert time to unix_epoch"),
-                            file: String::from(file!()),
-                            line: line!(),
-                            column: column!(),
-                            git_info: crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES.clone(),
-                        },
-                        source_place_type,
-                        should_trace,
-                    ),
+                    WriteBytesIntoFileAsyncTokioErrorNamed::StdIoError {        
+                        std_io_error: e,
+                        code_occurence: crate::code_occurence_tufa_common!()
+                    }
                 ));
             }
             Ok(())
--- src/server/file_system/write_json_into_file/write_json_into_file_async_tokio.rs
+++ src/server/file_system/write_json_into_file/write_json_into_file_async_tokio.rs
@@ -1,59 +1,30 @@
-use crate::common::where_was::WhereWas;
-use crate::global_variables::runtime::config::CONFIG;
-use crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES;
-use crate::traits::get_source::GetSource;
-use crate::traits::init_error_with_possible_trace::InitErrorWithPossibleTrace;
-use crate::traits::where_was_methods::WhereWasMethods;
-use impl_display_for_error::ImplDisplayForError;
-use impl_error_with_tracing::ImplErrorWithTracingFromCrate;
-use impl_get_source::ImplGetSourceFromCrate;
-use impl_get_where_was_origin_or_wrapper::ImplGetWhereWasOriginOrWrapperFromCrate;
-use init_error::InitErrorFromCrate;
-
-#[derive(
-    Debug,
-    InitErrorFromCrate,
-    ImplErrorWithTracingFromCrate,
-    ImplGetWhereWasOriginOrWrapperFromCrate,
-    ImplGetSourceFromCrate,
-    ImplDisplayForError,
-)]
-pub struct WriteJsonIntoFileAsyncTokioWrapperError {
-    source: WriteJsonIntoFileAsyncTokioOriginErrorEnum,
-    where_was: WhereWas,
+#[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
+pub enum WriteJsonIntoFileAsyncTokioErrorNamed<'a> {
+    SerdeJson {
+        #[eo_display]
+        serde_json_error: serde_json::Error,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    WriteBytesIntoFile {
+        #[eo_error_occurence] 
+        write_bytes_into_file: crate::server::file_system::write_bytes_into_file::write_bytes_into_file_async_tokio::WriteBytesIntoFileAsyncTokioErrorNamed<'a>,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    }, 
 }
 
-#[derive(
-    Debug, ImplGetSourceFromCrate, ImplDisplayForError, ImplGetWhereWasOriginOrWrapperFromCrate,
-)]
-pub enum WriteJsonIntoFileAsyncTokioOriginErrorEnum {
-    SerdeJsonOrigin(serde_json::Error),
-    StdIoOrigin(std::io::Error),
-}
-
-pub async fn write_json_into_file_async_tokio(
-    path: &std::path::Path,
+pub async fn write_json_into_file_async_tokio<'a>(
+    path: &'a std::path::Path,
     json_object: serde_json::Value,
     source_place_type: &crate::config_mods::source_place_type::SourcePlaceType,
     should_trace: bool,
-) -> Result<(), Box<WriteJsonIntoFileAsyncTokioWrapperError>> {
+) -> Result<(), Box<WriteJsonIntoFileAsyncTokioErrorNamed<'a>>> {
     match serde_json::to_string_pretty(&json_object) {
         Err(e) => {
             return Err(Box::new(
-                WriteJsonIntoFileAsyncTokioWrapperError::init_error_with_possible_trace(
-                    WriteJsonIntoFileAsyncTokioOriginErrorEnum::SerdeJsonOrigin(e),
-                        WhereWas {
-                            time: std::time::SystemTime::now()
-                                .duration_since(std::time::UNIX_EPOCH)
-                                .expect("cannot convert time to unix_epoch"),
-                            file: String::from(file!()),
-                            line: line!(),
-                            column: column!(),
-                            git_info: crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES.clone(),
-                        },
-                    source_place_type,
-                    should_trace,
-                ),
+                WriteJsonIntoFileAsyncTokioErrorNamed::SerdeJson {
+                    serde_json_error: e, 
+                    code_occurence: crate::code_occurence_tufa_common!() 
+                },
             ));
         }
         Ok(stringified_json) => {
@@ -66,20 +37,10 @@ pub async fn write_json_into_file_async_tokio(
             .await {
                 Err(e) => {
                     return Err(Box::new(
-                        WriteJsonIntoFileAsyncTokioWrapperError::init_error_with_possible_trace(
-                            WriteJsonIntoFileAsyncTokioOriginErrorEnum::StdIoOrigin(e.source),
-                            WhereWas {
-                                time: std::time::SystemTime::now()
-                                    .duration_since(std::time::UNIX_EPOCH)
-                                    .expect("cannot convert time to unix_epoch"),
-                                file: String::from(file!()),
-                                line: line!(),
-                                column: column!(),
-                                git_info: crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES.clone(),
-                            },
-                            source_place_type,
-                            should_trace,
-                        ),
+                        WriteJsonIntoFileAsyncTokioErrorNamed::WriteBytesIntoFile {
+                            write_bytes_into_file: *e, 
+                            code_occurence: crate::code_occurence_tufa_common!() 
+                        },
                     ));
                 },
                 Ok(_) => Ok(())
