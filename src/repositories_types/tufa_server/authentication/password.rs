60eed22f7b78716b3cd6485798b6abf7c2d4f9ef
kuqmua <serezhashmakov@outlook.com>
2023-05-12 08:02:31 +0300

    pub enum GetStoredCredentialsErrorNamed

--- src/repositories_types/tufa_server/authentication/password.rs
+++ src/repositories_types/tufa_server/authentication/password.rs
@@ -6,13 +6,21 @@ pub enum AuthError {
     UnexpectedError(#[from] anyhow::Error),
 }
 
+#[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
+pub enum GetStoredCredentialsErrorNamed<'a> {
+    FetchOptional {
+        #[eo_display]
+        fetch_optional: sqlx::Error,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+}
+
 #[tracing::instrument(name = "Get stored credentials", skip(username, pool))]
-async fn get_stored_credentials(
+async fn get_stored_credentials<'a>(
     username: &str,
     pool: &sqlx::PgPool,
-) -> Result<Option<(uuid::Uuid, secrecy::Secret<String>)>, anyhow::Error> {
-    use anyhow::Context;
-    let row = sqlx::query!(
+) -> Result<Option<(uuid::Uuid, secrecy::Secret<String>)>, GetStoredCredentialsErrorNamed<'a>> {
+    match sqlx::query!(
         r#"
         SELECT user_id, password_hash
         FROM users
@@ -22,61 +30,86 @@ async fn get_stored_credentials(
     )
     .fetch_optional(pool)
     .await
-    .context("Failed to performed a query to retrieve stored credentials.")?
-    .map(|row| (row.user_id, secrecy::Secret::new(row.password_hash)));
-    Ok(row)
+    {
+        Err(e) => Err(GetStoredCredentialsErrorNamed::FetchOptional {
+            fetch_optional: e,
+            code_occurence: crate::code_occurence_tufa_common!()
+        }),
+        Ok(option_record) => Ok(option_record.map(|row| (row.user_id, secrecy::Secret::new(row.password_hash)))),
+    }
 }
 
-#[tracing::instrument(name = "Validate credentials", skip(credentials, pool))]
 pub async fn validate_credentials(
     credentials: crate::common::postgres_credentials::PostgresCredentials,
     pool: &sqlx::PgPool,
 ) -> Result<uuid::Uuid, AuthError> {
-    let mut user_id = None;
-    let mut expected_password_hash = secrecy::Secret::new(
-        "$argon2id$v=19$m=15000,t=2,p=1$\
-        gZiV/M1gPc22ElAH/Jh1Hw$\
-        CWOrkoo7oJBQ/iyh7uJ0LO2aLEfrHwTWllSAxT0zRno"
-            .to_string(),
-    );
-    if let Some((stored_user_id, stored_password_hash)) =
-        get_stored_credentials(&credentials.username, pool).await?
-    {
-        user_id = Some(stored_user_id);
-        expected_password_hash = stored_password_hash;
+    todo!()
+    // let mut user_id = None;
+    // let mut expected_password_hash = secrecy::Secret::new(
+    //     "$argon2id$v=19$m=15000,t=2,p=1$\
+    //     gZiV/M1gPc22ElAH/Jh1Hw$\
+    //     CWOrkoo7oJBQ/iyh7uJ0LO2aLEfrHwTWllSAxT0zRno"
+    //         .to_string(),
+    // );
+    // // //
+
+    // // match get_stored_credentials(&credentials.username, pool).await {
+    // //     Err(e) => todo!(),
+    // //     Ok(_) => todo!(),
+    // // }
+
+    // // 
+    // if let Some((stored_user_id, stored_password_hash)) =
+    //     get_stored_credentials(&credentials.username, pool).await?
+    // {
+    //     user_id = Some(stored_user_id);
+    //     expected_password_hash = stored_password_hash;
+    // }
+    // {   
+    //     use anyhow::Context;
+    //     crate::repositories_types::tufa_server::telemetry::spawn_blocking_with_tracing::spawn_blocking_with_tracing(move || {
+    //         verify_password_hash(expected_password_hash, credentials.password)
+    //     })
+    //     .await
+    //     .context("Failed to spawn blocking task.")
+    // }??;
+    // user_id
+    //     .ok_or_else(|| anyhow::anyhow!("Unknown username."))
+    //     .map_err(AuthError::InvalidCredentials)
+}
+
+
+#[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
+pub enum VerifyPasswordHashErrorNamed<'a> {
+    ExposeSecret {
+        #[eo_display]
+        expose_secret: argon2::password_hash::Error,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    InvalidPassword {
+        #[eo_display]
+        invalid_password: argon2::password_hash::Error,//todo - should add here or just addd message?
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
     }
-    {   
-        use anyhow::Context;
-        crate::repositories_types::tufa_server::telemetry::spawn_blocking_with_tracing::spawn_blocking_with_tracing(move || {
-            verify_password_hash(expected_password_hash, credentials.password)
-        })
-        .await
-        .context("Failed to spawn blocking task.")
-    }??;
-    user_id
-        .ok_or_else(|| anyhow::anyhow!("Unknown username."))
-        .map_err(AuthError::InvalidCredentials)
 }
 
 #[tracing::instrument(
     name = "Validate credentials",
     skip(expected_password_hash, password_candidate)
 )]
-fn verify_password_hash(
+fn verify_password_hash<'a>(
     expected_password_hash: secrecy::Secret<String>,
     password_candidate: secrecy::Secret<String>,
-) -> Result<(), AuthError> {
-    let expected_password_hash = {
-        use anyhow::Context;
-        argon2::PasswordHash::new({
-            use secrecy::ExposeSecret;
-            expected_password_hash.expose_secret()
-        })
-        .context("Failed to parse hash in PHC string format.")
-    }?;
-    {
-        use anyhow::Context;
-        {
+) -> Result<(), VerifyPasswordHashErrorNamed<'a>> {
+    match argon2::PasswordHash::new({
+        use secrecy::ExposeSecret;
+        expected_password_hash.expose_secret()
+    }) {
+        Err(e) => Err(VerifyPasswordHashErrorNamed::ExposeSecret {
+            expose_secret: e,
+            code_occurence: crate::code_occurence_tufa_common!()
+        }),
+        Ok(expected_password_hash) => match {
             use argon2::PasswordVerifier;
             argon2::Argon2::default()
             .verify_password(
@@ -86,10 +119,38 @@ fn verify_password_hash(
                 }.as_bytes(),
                 &expected_password_hash,
             )
+        } {
+            Err(e) => Err(VerifyPasswordHashErrorNamed::InvalidPassword {
+                invalid_password: e,
+                code_occurence: crate::code_occurence_tufa_common!()
+            }),
+            Ok(_) => Ok(())
         }
-        .context("Invalid password.")
     }
-    .map_err(AuthError::InvalidCredentials)
+    // let expected_password_hash = {
+    //     use anyhow::Context;
+    //     argon2::PasswordHash::new({
+    //         use secrecy::ExposeSecret;
+    //         expected_password_hash.expose_secret()
+    //     })
+    //     .context("Failed to parse hash in PHC string format.")
+    // }?;
+    // {
+    //     use anyhow::Context;
+    //     {
+    //         use argon2::PasswordVerifier;
+    //         argon2::Argon2::default()
+    //         .verify_password(
+    //             {
+    //                 use secrecy::ExposeSecret;
+    //                 password_candidate.expose_secret()
+    //             }.as_bytes(),
+    //             &expected_password_hash,
+    //         )
+    //     }
+    //     .context("Invalid password.")
+    // }
+    // .map_err(AuthError::InvalidCredentials)
 }
 
 #[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
