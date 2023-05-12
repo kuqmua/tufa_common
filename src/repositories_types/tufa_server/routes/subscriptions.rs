0b0324e984676998de27e1ed481187b75081b71e
kuqmua <serezhashmakov@outlook.com>
2023-05-12 23:50:39 +0300

    move query!

--- src/repositories_types/tufa_server/routes/subscriptions.rs
+++ src/repositories_types/tufa_server/routes/subscriptions.rs
@@ -1,7 +1,7 @@
 #[derive(serde::Deserialize)]
 pub struct FormData {
-    email: String,
-    name: String,
+    pub email: String,
+    pub name: String,
 }
 
 impl std::convert::TryFrom<FormData> for crate::repositories_types::tufa_server::domain::NewSubscriber {
@@ -60,73 +60,7 @@ impl<'a> actix_web::ResponseError for SubscribeErrorNamed<'a> {
     }
 }
 
-pub async fn subscribe<'a>(
-    form: actix_web::web::Form<FormData>,
-    pool: actix_web::web::Data<sqlx::PgPool>,
-    email_client: actix_web::web::Data<crate::repositories_types::tufa_server::email_client::EmailClient>,
-    base_url: actix_web::web::Data<crate::repositories_types::tufa_server::startup::ApplicationBaseUrl>,
-) -> Result<actix_web::HttpResponse, SubscribeErrorNamed<'a>> {
-    let new_subscriber: crate::repositories_types::tufa_server::domain::NewSubscriber = match form.0.try_into() {
-        Err(e) => {
-            return Err(SubscribeErrorNamed::TryIntoNewSubscriber {
-                try_into_new_subscriber: e,
-                code_occurence: crate::code_occurence_tufa_common!(),
-            });
-        },
-        Ok(new_subscriber) => new_subscriber,
-    };
-    //"Failed to acquire a Postgres connection from the pool"
-    let mut transaction = match pool.begin().await {
-        Err(e) => {
-            return Err(SubscribeErrorNamed::PostgresPoolBegin {
-                postgres_pool_begin: e,
-                code_occurence: crate::code_occurence_tufa_common!(),
-            });
-        },
-        Ok(transaction) => transaction,
-    };
-    //"Failed to insert new subscriber in the database."
-    let subscriber_id = match insert_subscriber(&mut transaction, &new_subscriber).await {
-        Err(e) => {
-            return Err(SubscribeErrorNamed::InsertSubscriber {
-                insert_subscriber: e,
-                code_occurence: crate::code_occurence_tufa_common!(),
-            });
-        },
-        Ok(subscriber_id) => subscriber_id,
-    };
-    let subscription_token = generate_subscription_token();
-    //"Failed to store the confirmation token for a new subscriber."
-    if let Err(e) = store_token(&mut transaction, subscriber_id, &subscription_token)
-    .await {
-        return Err(SubscribeErrorNamed::StoreToken {
-            store_token: e,
-            code_occurence: crate::code_occurence_tufa_common!(),
-        });
-    }
-    //"Failed to commit SQL transaction to store a new subscriber."
-    if let Err(e) = transaction.commit().await {
-        return Err(SubscribeErrorNamed::PostgresTransactionCommit {
-            postgres_transaction_commit: e,
-            code_occurence: crate::code_occurence_tufa_common!(),
-        });
-    }
-    //"Failed to send a confirmation email."
-    if let Err(e) = send_confirmation_email(
-        &email_client,
-        new_subscriber,
-        &base_url.0,
-        &subscription_token,
-    ).await {
-        return Err(SubscribeErrorNamed::SendConfirmationEmail {
-            send_confirmation_email: e,
-            code_occurence: crate::code_occurence_tufa_common!(),
-        });
-    }
-    Ok(actix_web::HttpResponse::Ok().finish())
-}
-
-fn generate_subscription_token() -> String {
+pub fn generate_subscription_token() -> String {
     let mut rng = rand::thread_rng();
     std::iter::repeat_with(|| {
         use rand::Rng;
@@ -164,54 +98,7 @@ pub async fn send_confirmation_email(
         .await
 }
 
-#[tracing::instrument(
-    name = "Saving new subscriber details in the database",
-    skip(new_subscriber, transaction)
-)]
-pub async fn insert_subscriber(
-    transaction: &mut sqlx::Transaction<'_, sqlx::Postgres>,
-    new_subscriber: &crate::repositories_types::tufa_server::domain::NewSubscriber,
-) -> Result<uuid::Uuid, sqlx::Error> {
-    let subscriber_id = uuid::Uuid::new_v4();
-    sqlx::query!(
-        r#"
-    INSERT INTO subscriptions (id, email, name, subscribed_at, status)
-    VALUES ($1, $2, $3, $4, 'pending_confirmation')
-            "#,
-        subscriber_id,
-        new_subscriber.email.as_ref(),
-        new_subscriber.name.as_ref(),
-        chrono::Utc::now()
-    )
-    .execute(transaction)
-    .await?;
-    Ok(subscriber_id)
-}
-
-#[tracing::instrument(
-    name = "Store subscription token in the database",
-    skip(subscription_token, transaction)
-)]
-pub async fn store_token(
-    transaction: &mut sqlx::Transaction<'_, sqlx::Postgres>,
-    subscriber_id: uuid::Uuid,
-    subscription_token: &str,
-) -> Result<(), StoreTokenError> {
-    sqlx::query!(
-        r#"
-    INSERT INTO subscription_tokens (subscription_token, subscriber_id)
-    VALUES ($1, $2)
-        "#,
-        subscription_token,
-        subscriber_id
-    )
-    .execute(transaction)
-    .await
-    .map_err(StoreTokenError)?;
-    Ok(())
-}
-
-pub struct StoreTokenError(sqlx::Error);
+pub struct StoreTokenError(pub sqlx::Error);
 
 impl std::error::Error for StoreTokenError {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
