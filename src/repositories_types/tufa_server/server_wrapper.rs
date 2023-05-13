482b43065a48e437b8f0724f7e165dc9f60ea868
kuqmua <serezhashmakov@outlook.com>
2023-05-13 03:46:55 +0300

    next steps

--- src/repositories_types/tufa_server/server_wrapper.rs
+++ src/repositories_types/tufa_server/server_wrapper.rs
@@ -1,8 +1,20 @@
+use tokio::task::JoinError;
+
 #[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
 pub enum ServerWrapperErrorNamed<'a> {
     ApplicationBuild {
         #[eo_error_occurence]
         application_build: crate::repositories_types::tufa_server::startup::ApplicationBuildErrorNamed<'a>,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    TokioSpawn {
+        #[eo_display]
+        tokio_spawn: JoinError,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    RunUntilStopped {
+        #[eo_error_occurence]
+        run_until_stopped: crate::repositories_types::tufa_server::startup::RunUntilStoppedErrorNamed<'a>,
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
     },
 }
