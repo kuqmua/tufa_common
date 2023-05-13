abe8a428a932272409173547b539b8c84a7c3101
kuqmua <serezhashmakov@outlook.com>
2023-05-13 04:29:09 +0300

    remove config.get_base_url() from application settings

--- src/repositories_types/tufa_server/configuration.rs
+++ src/repositories_types/tufa_server/configuration.rs
@@ -39,8 +39,6 @@ impl EmailClientSettings {
 pub struct ApplicationSettings {
     #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
-    // pub host: &'a str,
-    pub base_url: String,
     pub hmac_secret: secrecy::Secret<String>,
 }
 
