ea935cc0fae72da053f595fa799e28136e9de568
kuqmua <serezhashmakov@outlook.com>
2023-05-16 18:05:35 +0300

    remove mongo from check availability

--- src/repositories_types/tufa_server/preparation/check_availability.rs
+++ src/repositories_types/tufa_server/preparation/check_availability.rs
@@ -12,11 +12,11 @@ pub enum CheckAvailabilityErrorNamed<'a> {
         >,
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
     },
-    Mongo {
-        #[eo_error_occurence]
-        mongo: crate::repositories_types::tufa_server::mongo_integration::mongo_check_availability::MongoCheckAvailabilityErrorNamed<'a>,
-        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
-    },
+    // Mongo {
+    //     #[eo_error_occurence]
+    //     mongo: crate::repositories_types::tufa_server::mongo_integration::mongo_check_availability::MongoCheckAvailabilityErrorNamed<'a>,
+    //     code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    // },
     NetAndPostgres {
         #[eo_error_occurence]
         net: crate::server::net::net_check_availability::NetCheckAvailabilityErrorNamed<'a>,
@@ -24,29 +24,29 @@ pub enum CheckAvailabilityErrorNamed<'a> {
         postgres: crate::repositories_types::tufa_server::postgres_integration::postgres_check_availability::PostgresCheckAvailabilityErrorNamed<'a>,
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
     },
-    NetAndMongo {
-        #[eo_error_occurence]
-        net: crate::server::net::net_check_availability::NetCheckAvailabilityErrorNamed<'a>,
-        #[eo_error_occurence]
-        mongo: crate::repositories_types::tufa_server::mongo_integration::mongo_check_availability::MongoCheckAvailabilityErrorNamed<'a>,
-        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
-    },
-    PostgresAndMongo {
-        #[eo_error_occurence]
-        postgres: crate::repositories_types::tufa_server::postgres_integration::postgres_check_availability::PostgresCheckAvailabilityErrorNamed<'a>,
-        #[eo_error_occurence]
-        mongo: crate::repositories_types::tufa_server::mongo_integration::mongo_check_availability::MongoCheckAvailabilityErrorNamed<'a>,
-        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
-    },
-    NetAndPostgresAndMongo {
-        #[eo_error_occurence]
-        net: crate::server::net::net_check_availability::NetCheckAvailabilityErrorNamed<'a>,
-        #[eo_error_occurence]
-        postgres: crate::repositories_types::tufa_server::postgres_integration::postgres_check_availability::PostgresCheckAvailabilityErrorNamed<'a>,
-        #[eo_error_occurence]
-        mongo: crate::repositories_types::tufa_server::mongo_integration::mongo_check_availability::MongoCheckAvailabilityErrorNamed<'a>,
-        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
-    },
+    // NetAndMongo {
+    //     #[eo_error_occurence]
+    //     net: crate::server::net::net_check_availability::NetCheckAvailabilityErrorNamed<'a>,
+    //     #[eo_error_occurence]
+    //     mongo: crate::repositories_types::tufa_server::mongo_integration::mongo_check_availability::MongoCheckAvailabilityErrorNamed<'a>,
+    //     code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    // },
+    // PostgresAndMongo {
+    //     #[eo_error_occurence]
+    //     postgres: crate::repositories_types::tufa_server::postgres_integration::postgres_check_availability::PostgresCheckAvailabilityErrorNamed<'a>,
+    //     #[eo_error_occurence]
+    //     mongo: crate::repositories_types::tufa_server::mongo_integration::mongo_check_availability::MongoCheckAvailabilityErrorNamed<'a>,
+    //     code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    // },
+    // NetAndPostgresAndMongo {
+    //     #[eo_error_occurence]
+    //     net: crate::server::net::net_check_availability::NetCheckAvailabilityErrorNamed<'a>,
+    //     #[eo_error_occurence]
+    //     postgres: crate::repositories_types::tufa_server::postgres_integration::postgres_check_availability::PostgresCheckAvailabilityErrorNamed<'a>,
+    //     #[eo_error_occurence]
+    //     mongo: crate::repositories_types::tufa_server::mongo_integration::mongo_check_availability::MongoCheckAvailabilityErrorNamed<'a>,
+    //     code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    // },
 }
 
 pub async fn check_availability<'a, SelfGeneric>(
@@ -62,45 +62,67 @@ pub async fn check_availability<'a, SelfGeneric>(
         crate::repositories_types::tufa_server::postgres_integration::postgres_check_availability::postgres_check_availability(
             config.get_postgres_url(), 
             *config.get_postgres_connection_timeout()
-        ),
-        crate::repositories_types::tufa_server::mongo_integration::mongo_check_availability::mongo_check_availability(
-            mongodb_options_client_options,
-            "logs"
-        ),
+        )
     ) {
-        (Ok(_), Ok(_), Ok(_)) => Ok(()),
-        (Ok(_), Ok(_), Err(m)) => Err(Box::new(crate::repositories_types::tufa_server::preparation::check_availability::CheckAvailabilityErrorNamed::Mongo {
-            mongo: *m,
-            code_occurence: crate::code_occurence_tufa_common!(),
-        })),
-        (Ok(_), Err(p), Ok(_)) => Err(Box::new(crate::repositories_types::tufa_server::preparation::check_availability::CheckAvailabilityErrorNamed::Postgres {
+        (Ok(_), Ok(_)) => Ok(()),
+        (Ok(_), Err(p)) => Err(Box::new(crate::repositories_types::tufa_server::preparation::check_availability::CheckAvailabilityErrorNamed::Postgres {
             postgres: *p,
             code_occurence: crate::code_occurence_tufa_common!(),
         })),
-        (Ok(_), Err(p), Err(m)) => Err(Box::new(crate::repositories_types::tufa_server::preparation::check_availability::CheckAvailabilityErrorNamed::PostgresAndMongo {
-            postgres: *p,
-            mongo: *m,
-            code_occurence: crate::code_occurence_tufa_common!(),
-        })),
-        (Err(n), Ok(_), Ok(_)) => Err(Box::new(crate::repositories_types::tufa_server::preparation::check_availability::CheckAvailabilityErrorNamed::Net {
+        (Err(n), Ok(_)) => Err(Box::new(crate::repositories_types::tufa_server::preparation::check_availability::CheckAvailabilityErrorNamed::Net {
             net: *n,
             code_occurence: crate::code_occurence_tufa_common!(),
         })),
-        (Err(n), Ok(_), Err(m)) => Err(Box::new(crate::repositories_types::tufa_server::preparation::check_availability::CheckAvailabilityErrorNamed::NetAndMongo {
-            net: *n,
-            mongo: *m,
-            code_occurence: crate::code_occurence_tufa_common!(),
-        })),
-        (Err(n), Err(p), Ok(_)) => Err(Box::new(crate::repositories_types::tufa_server::preparation::check_availability::CheckAvailabilityErrorNamed::NetAndPostgres {
+        (Err(n), Err(p)) => Err(Box::new(crate::repositories_types::tufa_server::preparation::check_availability::CheckAvailabilityErrorNamed::NetAndPostgres {
             net: *n,
             postgres: *p,
-            code_occurence: crate::code_occurence_tufa_common!(),
-        })),
-        (Err(n), Err(p), Err(m)) => Err(Box::new(crate::repositories_types::tufa_server::preparation::check_availability::CheckAvailabilityErrorNamed::NetAndPostgresAndMongo {
-            net: *n,
-            postgres: *p,
-            mongo: *m,
             code_occurence: crate::code_occurence_tufa_common!(),
         })),
     }
+    // match futures::join!(
+    //     crate::server::net::net_check_availability::net_check_availability(config.get_starting_check_link()),
+    //     crate::repositories_types::tufa_server::postgres_integration::postgres_check_availability::postgres_check_availability(
+    //         config.get_postgres_url(), 
+    //         *config.get_postgres_connection_timeout()
+    //     ),
+    //     crate::repositories_types::tufa_server::mongo_integration::mongo_check_availability::mongo_check_availability(
+    //         mongodb_options_client_options,
+    //         "logs"
+    //     ),
+    // ) {
+    //     (Ok(_), Ok(_), Ok(_)) => Ok(()),
+    //     (Ok(_), Ok(_), Err(m)) => Err(Box::new(crate::repositories_types::tufa_server::preparation::check_availability::CheckAvailabilityErrorNamed::Mongo {
+    //         mongo: *m,
+    //         code_occurence: crate::code_occurence_tufa_common!(),
+    //     })),
+    //     (Ok(_), Err(p), Ok(_)) => Err(Box::new(crate::repositories_types::tufa_server::preparation::check_availability::CheckAvailabilityErrorNamed::Postgres {
+    //         postgres: *p,
+    //         code_occurence: crate::code_occurence_tufa_common!(),
+    //     })),
+    //     (Ok(_), Err(p), Err(m)) => Err(Box::new(crate::repositories_types::tufa_server::preparation::check_availability::CheckAvailabilityErrorNamed::PostgresAndMongo {
+    //         postgres: *p,
+    //         mongo: *m,
+    //         code_occurence: crate::code_occurence_tufa_common!(),
+    //     })),
+    //     (Err(n), Ok(_), Ok(_)) => Err(Box::new(crate::repositories_types::tufa_server::preparation::check_availability::CheckAvailabilityErrorNamed::Net {
+    //         net: *n,
+    //         code_occurence: crate::code_occurence_tufa_common!(),
+    //     })),
+    //     (Err(n), Ok(_), Err(m)) => Err(Box::new(crate::repositories_types::tufa_server::preparation::check_availability::CheckAvailabilityErrorNamed::NetAndMongo {
+    //         net: *n,
+    //         mongo: *m,
+    //         code_occurence: crate::code_occurence_tufa_common!(),
+    //     })),
+    //     (Err(n), Err(p), Ok(_)) => Err(Box::new(crate::repositories_types::tufa_server::preparation::check_availability::CheckAvailabilityErrorNamed::NetAndPostgres {
+    //         net: *n,
+    //         postgres: *p,
+    //         code_occurence: crate::code_occurence_tufa_common!(),
+    //     })),
+    //     (Err(n), Err(p), Err(m)) => Err(Box::new(crate::repositories_types::tufa_server::preparation::check_availability::CheckAvailabilityErrorNamed::NetAndPostgresAndMongo {
+    //         net: *n,
+    //         postgres: *p,
+    //         mongo: *m,
+    //         code_occurence: crate::code_occurence_tufa_common!(),
+    //     })),
+    // }
 }
