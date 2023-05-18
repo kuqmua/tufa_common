d23c4b086fcbaf5a4744f3b7d7a9b08cdc99ce38
kuqmua <serezhashmakov@outlook.com>
2023-05-18 04:56:09 +0300

    next steps

--- src/repositories_types/tufa_server/settings.rs
+++ src/repositories_types/tufa_server/settings.rs
@@ -72,22 +72,6 @@ impl PostgresDatabaseSettings {
     }
 }
 
-pub fn get_settings() -> Result<Settings, config::ConfigError> {
-    let mut settings = config::Config::default();
-    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
-    let configuration_directory = base_path.join("configuration");
-    settings.merge(config::File::from(configuration_directory.join("base")).required(true))?;
-    let environment: Environment = std::env::var("APP_ENVIRONMENT")
-        .unwrap_or_else(|_| "local".into())
-        .try_into()
-        .expect("Failed to parse APP_ENVIRONMENT.");
-    settings.merge(
-        config::File::from(configuration_directory.join(environment.as_str())).required(true),
-    )?;
-    settings.merge(config::Environment::with_prefix("app").separator("__"))?;
-    settings.try_into()
-}
-
 pub enum Environment {
     Local,
     Production,
