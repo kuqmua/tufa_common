1b4e4903ecc808790ab26fb619ff75aae3c9b485
kuqmua <serezhashmakov@outlook.com>
2023-05-20 03:18:47 +0300

    lil comment

--- src/dev.rs
+++ src/dev.rs
@@ -1,28 +1,28 @@
-#[derive(Clone, Debug, valuable::Valuable)]
-struct User {
-    name: String,
-    age: u32,
-    something: Vec<bool>,
-    address: Address,
-}
+// #[derive(Clone, Debug, valuable::Valuable)]
+// struct User {
+//     name: String,
+//     age: u32,
+//     something: Vec<bool>,
+//     address: Address,
+// }
 
-#[derive(Clone, Debug, valuable::Valuable)]
-struct Address {
-    country: String,
-    city: String,
-    street: String,
-}
+// #[derive(Clone, Debug, valuable::Valuable)]
+// struct Address {
+//     country: String,
+//     city: String,
+//     street: String,
+// }
 
 pub fn dev() {
-    let user = User {
-        name: "Arwen Undomiel".to_string(),
-        age: 3000,
-        something: vec![true, false],
-        address: Address {
-            country: "Middle Earth".to_string(),
-            city: "Rivendell".to_string(),
-            street: "leafy lane".to_string(),
-        },
-    };
-    tracing::error!(valuable = false, user = ?user);
+    // let user = User {
+    //     name: "Arwen Undomiel".to_string(),
+    //     age: 3000,
+    //     something: vec![true, false],
+    //     address: Address {
+    //         country: "Middle Earth".to_string(),
+    //         city: "Rivendell".to_string(),
+    //         street: "leafy lane".to_string(),
+    //     },
+    // };
+    // tracing::error!(valuable = false, user = ?user);
 }
