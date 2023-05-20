cb9e705eb187de09aaf12f4ef4989354b8f5937a
kuqmua <serezhashmakov@outlook.com>
2023-05-20 23:15:17 +0300

    next steps

--- src/repositories_types/tufa_server/server_wrapper.rs
+++ src/repositories_types/tufa_server/server_wrapper.rs
@@ -12,6 +12,11 @@ pub enum ServerWrapperErrorNamed<'a> {
         try_get_postgres_pool: crate::traits::try_get_postgres_pool::TryGetPostgresPoolError,
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
     },
+    TryGetRedisSessionStorage {
+        #[eo_display]
+        try_get_redis_session_storage: crate::traits::try_get_redis_session_storage::TryGetRedisSessionStorageError,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
     ApplicationBuild {
         #[eo_error_occurence]
         application_build: crate::repositories_types::tufa_server::try_build_actix_web_dev_server::TryBuildActixWebDevServer<'a>,
--- src/traits/try_get_redis_session_storage.rs
+++ src/traits/try_get_redis_session_storage.rs
@@ -1,17 +1,17 @@
-#[derive(Debug)]
+#[derive(Debug, strum_macros::Display)]
 pub enum TryGetRedisSessionStorageError {
     Redis(std::string::String),//anyhow::Error
 }
 
 pub trait TryGetRedisSessionStorage {
-    async fn mongo_try_get_client(&self) -> Result<actix_session::storage::RedisSessionStore, TryGetRedisSessionStorageError>;
+    async fn try_get_redis_session_storage(&self) -> Result<actix_session::storage::RedisSessionStore, TryGetRedisSessionStorageError>;
 }
 
 impl<SelfGeneric> TryGetRedisSessionStorage for SelfGeneric
 where
     Self: crate::traits::config_fields::GetRedisUrl,
 {
-    async fn mongo_try_get_client(&self) -> Result<actix_session::storage::RedisSessionStore, TryGetRedisSessionStorageError> {
+    async fn try_get_redis_session_storage(&self) -> Result<actix_session::storage::RedisSessionStore, TryGetRedisSessionStorageError> {
         match actix_session::storage::RedisSessionStore::new(self.get_redis_url()).await {
             Ok(redis_session_store) => Ok(redis_session_store),
             Err(e) => {
