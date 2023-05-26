a029b1f9aa3c8b3733363f1b4efea18e7b3f130f
kuqmua <serezhashmakov@outlook.com>
2023-05-26 13:19:43 +0300

    next steps

--- src/repositories_types/tufa_server/routes/cats/get.rs
+++ src/repositories_types/tufa_server/routes/cats/get.rs
@@ -1,5 +1,16 @@
 #[derive(Debug, serde_derive::Serialize, serde_derive::Deserialize)]
-pub enum GetResponse {
+pub enum GetResponse<'a> {
     Ok(Vec<super::Cat>),
-    Err(std::string::String)
+    #[serde(borrow)]
+    Err(PostgresSelectCatsErrorNamedWithSerializeDeserialize<'a>)
+}
+
+
+#[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
+pub enum PostgresSelectCatsErrorNamed<'a> {
+    SelectCats {
+        #[eo_display]
+        select_cats: sqlx::Error,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
 }
