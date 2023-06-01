7a5d4ad5985670d1fb4788efb3cc05d8083e8b90
kuqmua <serezhashmakov@outlook.com>
2023-06-02 01:27:24 +0300

    lil git_info usage tweaks

--- src/global_variables/compile_time/git_info.rs
+++ src/global_variables/compile_time/git_info.rs
@@ -1,2 +1,2 @@
 pub static GIT_INFO: crate::common::git::git_info::GitInformation =
-    compile_time_git_info::compile_time_git_info!(tufa_common, crate);
+    compile_time_git_info::compile_time_git_info_tufa_common!();
