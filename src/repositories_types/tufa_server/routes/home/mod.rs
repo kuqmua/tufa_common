7af50b24ca2cc202e122073789164c6797c4671c
kuqmua <serezhashmakov@outlook.com>
2023-06-04 09:48:16 +0300

    remove home route

--- src/repositories_types/tufa_server/routes/home/mod.rs
+++
@@ -1,5 +0,0 @@
-pub async fn home() -> actix_web::HttpResponse {
-    actix_web::HttpResponse::Ok()
-        .content_type(actix_web::http::header::ContentType::html())
-        .body(include_str!("home.html"))
-}
--- src/repositories_types/tufa_server/routes/mod.rs
+++ src/repositories_types/tufa_server/routes/mod.rs
@@ -1,13 +1,11 @@
 pub mod admin;
 pub mod get_providers_posts_route;
 mod health_check;
-pub mod home;
 pub mod login;
 pub mod subscriptions;
 pub mod subscriptions_confirm;
 // pub use admin::*;
 pub use health_check::*;
-pub use home::*;
 pub use login::*;
 pub use subscriptions::*;
 pub use subscriptions_confirm::*;
