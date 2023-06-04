aaf29eb25a5dca047603a4b506d9eefac58afcee
kuqmua <serezhashmakov@outlook.com>
2023-06-04 15:02:39 +0300

    project_git_info lil tweaks

--- src/server/routes/project_commit.rs
+++ src/server/routes/project_commit.rs
@@ -3,12 +3,16 @@ pub struct ProjectGitInfo {
     project_commit: std::string::String,
 }
 
-pub async fn project_git_info<'a>() -> impl actix_web::Responder {
+#[actix_web::get("/project_git_info")]
+pub async fn project_git_info(
+    app_info: actix_web::web::Data<
+        crate::repositories_types::tufa_server::try_build_actix_web_dev_server::AppInfo<'_>,
+    >,
+) -> impl actix_web::Responder {
     actix_web::HttpResponse::Ok().json(actix_web::web::Json(ProjectGitInfo {
         project_commit: {
             use crate::common::git::get_git_commit_link::GetGitCommitLink;
-            crate::global_variables::compile_time::project_git_info::PROJECT_GIT_INFO
-                .get_git_commit_link()
+            app_info.project_git_info.get_git_commit_link()
         },
     }))
 }
