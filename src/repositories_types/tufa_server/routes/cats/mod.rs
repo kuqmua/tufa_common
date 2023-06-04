a7dce5e78c011fc824cec73f9df5b3cd96acd083
kuqmua <serezhashmakov@outlook.com>
2023-06-04 17:12:28 +0300

    try_patch

--- src/repositories_types/tufa_server/routes/cats/mod.rs
+++ src/repositories_types/tufa_server/routes/cats/mod.rs
@@ -399,6 +399,77 @@ pub enum PatchErrorNamed<'a> {
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
     },
 }
+
+#[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
+pub enum TryPatchErrorNamed<'a> {
+    BelowZero {
+        #[eo_display_with_serialize_deserialize]
+        below_zero: i64,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    SerdeJsonToString {
+        #[eo_display]
+        serde_json_to_string: serde_json::Error,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    UnexpectedStatusCode {
+        #[eo_display]
+        unexpected_status_code: http::StatusCode,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    Reqwest {
+        #[eo_display_foreign_type]
+        reqwest: reqwest::Error,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+}
+
+pub async fn try_patch<'a>(
+    server_location: std::string::String,
+    cat: CatToPatch,
+) -> Result<(), TryPatchErrorNamed<'a>> {
+    if let true = cat.id.is_negative() {
+        return Err(TryPatchErrorNamed::BelowZero {
+            below_zero: cat.id,
+            code_occurence: crate::code_occurence_tufa_common!(),
+        });
+    }
+    let stringified_json = match serde_json::to_string(&cat) {
+        Ok(stringified_json) => stringified_json,
+        Err(e) => {
+            return Err(TryPatchErrorNamed::SerdeJsonToString {
+                serde_json_to_string: e,
+                code_occurence: crate::code_occurence_tufa_common!(),
+            });
+        }
+    };
+    let url = format!(
+        "{server_location}/api/cats/?project_commit={}",
+        crate::global_variables::compile_time::project_git_info::PROJECT_GIT_INFO.project_commit
+    );
+    match reqwest::Client::new()
+        .patch(&url)
+        .header(reqwest::header::CONTENT_TYPE, "application/json")
+        .body(stringified_json)
+        .send()
+        .await
+    {
+        Ok(r) => {
+            let response_status = r.status();
+            match response_status == http::StatusCode::OK {
+                true => Ok(()),
+                false => Err(TryPatchErrorNamed::UnexpectedStatusCode {
+                    unexpected_status_code: response_status,
+                    code_occurence: crate::code_occurence_tufa_common!(),
+                }),
+            }
+        }
+        Err(e) => Err(TryPatchErrorNamed::Reqwest {
+            reqwest: e,
+            code_occurence: crate::code_occurence_tufa_common!(),
+        }),
+    }
+}
 //////////////////////////////////////
 #[derive(serde::Deserialize)]
 pub struct DeleteQueryParameters {
