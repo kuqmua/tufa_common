f9c4d61c000aca8a572fb78c1a09555ca53f4928
kuqmua <serezhashmakov@outlook.com>
2023-06-05 09:29:07 +0300

    put cat header project_commit

--- src/repositories_types/tufa_server/routes/cats/mod.rs
+++ src/repositories_types/tufa_server/routes/cats/mod.rs
@@ -1,7 +1,7 @@
 //todo server_location: std::string::String, 0 maybe change it to ip port
 #[derive(Debug, serde_derive::Serialize, serde_derive::Deserialize)]
 pub struct Cat {
-    pub id: i64,
+    pub id: i64, //todo - if using js JSON.parse() - must be two variants - for usage and deserialization - coz json number type capacity less than i64::MAX
     pub name: String,
     pub color: String,
 }
@@ -290,16 +290,21 @@ pub async fn try_post<'a>(
 }
 //
 //////////////////////////////////////
-#[derive(serde::Deserialize)]
-pub struct PutQueryParameters {
-    pub project_commit: String,
-}
-
 #[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
 pub enum PutErrorNamed<'a> {
     CheckApiUsage {
         #[eo_display_with_serialize_deserialize]
         project_commit: &'a str,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    CannotConvertProjectCommitToStr {
+        #[eo_display_with_serialize_deserialize]
+        cannot_convert_project_commit_to_str: std::string::String,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    NoProjectCommitHeader {
+        #[eo_display_with_serialize_deserialize]
+        no_project_commit_header: &'a str,
         code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
     },
     Bigserial {
