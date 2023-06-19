4bcca3831318b4cc1ca1752f317e95643e0fc447
kuqmua <serezhashmakov@outlook.com>
2023-06-19 20:25:13 +0300

    impl<'a> std::convert::From<sqlx::Error> for SqlxPostgresErrorErrorName

--- src/server/postgres/mod.rs
+++ src/server/postgres/mod.rs
@@ -6,3 +6,4 @@ pub mod postgres_create_providers_tables_if_not_exists;
 pub mod postgres_delete_all_from_providers_link_parts_tables;
 pub mod postgres_insert_link_parts_into_providers_tables;
 pub mod rows_per_table;
+pub mod sqlx_postgres_error;
---
+++ src/server/postgres/sqlx_postgres_error.rs
@@ -0,0 +1,171 @@
+#[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
+pub enum SqlxPostgresErrorErrorNamed<'a> {
+    Configuration {
+        #[eo_display_with_serialize_deserialize]
+        configuration_box_dyn_error: std::string::String,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    Database {
+        #[eo_display_with_serialize_deserialize]
+        box_dyn_database_error: std::string::String,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    Io {
+        #[eo_display]
+        io_error: std::io::Error,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    Tls {
+        #[eo_display_with_serialize_deserialize]
+        box_dyn_error: std::string::String,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    Protocol {
+        #[eo_display_with_serialize_deserialize]
+        protocol: std::string::String,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    RowNotFound {
+        #[eo_display_with_serialize_deserialize]
+        row_not_found: std::string::String,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    TypeNotFound {
+        #[eo_display_with_serialize_deserialize]
+        type_not_found: std::string::String,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    ColumnIndexOutOfBounds {
+        #[eo_display_with_serialize_deserialize]
+        column_index_out_of_bounds: usize,
+        #[eo_display_with_serialize_deserialize]
+        len: usize,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    ColumnNotFound {
+        #[eo_display_with_serialize_deserialize]
+        column_not_found: std::string::String,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    ColumnDecode {
+        #[eo_display_with_serialize_deserialize]
+        column_decode_index: std::string::String,
+        #[eo_display_with_serialize_deserialize]
+        source_handle: std::string::String,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    Decode {
+        #[eo_display_with_serialize_deserialize]
+        decode_box_dyn_error: std::string::String,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    PoolTimedOut {
+        #[eo_display_with_serialize_deserialize]
+        pool_timed_out: std::string::String,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    PoolClosed {
+        #[eo_display_with_serialize_deserialize]
+        pool_closed: std::string::String,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    WorkerCrashed {
+        #[eo_display_with_serialize_deserialize]
+        worker_crashed: std::string::String,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    Migrate {
+        #[eo_display]
+        migrate: sqlx::migrate::MigrateError,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    //#[non_exhaustive] case
+    UnexpectedCase {
+        #[eo_display_with_serialize_deserialize]
+        unexpected_case: std::string::String,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+}
+
+impl<'a> std::convert::From<sqlx::Error> for SqlxPostgresErrorErrorNamed<'a> {
+    fn from(e: sqlx::Error) -> SqlxPostgresErrorErrorNamed<'a> {
+        match e {
+            sqlx::Error::Configuration(box_dyn_error) => {
+                SqlxPostgresErrorErrorNamed::Configuration {
+                    configuration_box_dyn_error: box_dyn_error.to_string(),
+                    code_occurence: crate::code_occurence_tufa_common!(),
+                }
+            }
+            sqlx::Error::Database(box_dyn_database_error) => {
+                SqlxPostgresErrorErrorNamed::Database {
+                    box_dyn_database_error: box_dyn_database_error.message().to_string(),
+                    code_occurence: crate::code_occurence_tufa_common!(),
+                }
+            }
+            sqlx::Error::Io(io_error) => SqlxPostgresErrorErrorNamed::Io {
+                io_error,
+                code_occurence: crate::code_occurence_tufa_common!(),
+            },
+            sqlx::Error::Tls(box_dyn_error) => SqlxPostgresErrorErrorNamed::Tls {
+                box_dyn_error: box_dyn_error.to_string(),
+                code_occurence: crate::code_occurence_tufa_common!(),
+            },
+            sqlx::Error::Protocol(string) => SqlxPostgresErrorErrorNamed::Protocol {
+                protocol: string,
+                code_occurence: crate::code_occurence_tufa_common!(),
+            },
+            sqlx::Error::RowNotFound => SqlxPostgresErrorErrorNamed::RowNotFound {
+                row_not_found: std::string::String::from("row_not_found"),
+                code_occurence: crate::code_occurence_tufa_common!(),
+            },
+            sqlx::Error::TypeNotFound { type_name } => SqlxPostgresErrorErrorNamed::TypeNotFound {
+                type_not_found: type_name,
+                code_occurence: crate::code_occurence_tufa_common!(),
+            },
+            sqlx::Error::ColumnIndexOutOfBounds { index, len } => {
+                SqlxPostgresErrorErrorNamed::ColumnIndexOutOfBounds {
+                    column_index_out_of_bounds: index,
+                    len,
+                    code_occurence: crate::code_occurence_tufa_common!(),
+                }
+            }
+            sqlx::Error::ColumnNotFound(column_not_found) => {
+                SqlxPostgresErrorErrorNamed::ColumnNotFound {
+                    column_not_found,
+                    code_occurence: crate::code_occurence_tufa_common!(),
+                }
+            }
+            sqlx::Error::ColumnDecode { index, source } => {
+                SqlxPostgresErrorErrorNamed::ColumnDecode {
+                    column_decode_index: index,
+                    source_handle: source.to_string(),
+                    code_occurence: crate::code_occurence_tufa_common!(),
+                }
+            }
+            sqlx::Error::Decode(decode_box_dyn_error) => SqlxPostgresErrorErrorNamed::Decode {
+                decode_box_dyn_error: decode_box_dyn_error.to_string(),
+                code_occurence: crate::code_occurence_tufa_common!(),
+            },
+            sqlx::Error::PoolTimedOut => SqlxPostgresErrorErrorNamed::PoolTimedOut {
+                pool_timed_out: std::string::String::from("pool timed out"),
+                code_occurence: crate::code_occurence_tufa_common!(),
+            },
+            sqlx::Error::PoolClosed => SqlxPostgresErrorErrorNamed::PoolClosed {
+                pool_closed: std::string::String::from("pool closed"),
+                code_occurence: crate::code_occurence_tufa_common!(),
+            },
+            sqlx::Error::WorkerCrashed => SqlxPostgresErrorErrorNamed::WorkerCrashed {
+                worker_crashed: std::string::String::from("worker crashed"),
+                code_occurence: crate::code_occurence_tufa_common!(),
+            },
+            sqlx::Error::Migrate(migrate_error) => SqlxPostgresErrorErrorNamed::Migrate {
+                migrate: *migrate_error,
+                code_occurence: crate::code_occurence_tufa_common!(),
+            },
+            _ => SqlxPostgresErrorErrorNamed::UnexpectedCase {
+                unexpected_case: std::string::String::from("unexpected_case"),
+                code_occurence: crate::code_occurence_tufa_common!(),
+            },
+        }
+    }
+}
