6bc7ddcd4c82ee9448d323767ba357c65646ebda
kuqmua <serezhashmakov@outlook.com>
2023-07-01 11:36:14 +0300

    next steps

--- src/repositories_types/tufa_server/routes/api/cats/get/request.rs
+++ src/repositories_types/tufa_server/routes/api/cats/get/request.rs
@@ -1,85 +1,102 @@
 #[derive(Debug, serde::Serialize, serde::Deserialize, type_variants_from_reqwest_response::TypeVariantsFromReqwestResponse)]
 pub enum GetHttpResponseVariants {
-    
+    #[tvfrr_200_ok]
     Cats(Vec<crate::repositories_types::tufa_server::routes::api::cats::Cat>),
-    //
-    #[tvfrr_404]
+    #[tvfrr_400_bad_request]
     ProjectCommitExtractorNotEqual {
         project_commit_not_equal: std::string::String,
         project_commit_to_use: std::string::String,
         code_occurence: crate::common::code_occurence::CodeOccurenceWithSerializeDeserialize,
     },
+    #[tvfrr_400_bad_request]
     ProjectCommitExtractorToStrConversion {
         project_commit_to_str_conversion: std::string::String,
         code_occurence: crate::common::code_occurence::CodeOccurenceWithSerializeDeserialize,
     },
+    #[tvfrr_400_bad_request]
     NoProjectCommitExtractorHeader {
         no_project_commit_header: std::string::String,
         code_occurence: crate::common::code_occurence::CodeOccurenceWithSerializeDeserialize,
     },
     //
+    #[tvfrr_500_internal_server_error]
     Configuration {
         configuration_box_dyn_error: std::string::String,
         code_occurence: crate::common::code_occurence::CodeOccurenceWithSerializeDeserialize,
     },
+    #[tvfrr_500_internal_server_error]
     Database {
         box_dyn_database_error: std::string::String,
         code_occurence: crate::common::code_occurence::CodeOccurenceWithSerializeDeserialize,
     },
+    #[tvfrr_500_internal_server_error]
     Io {
         io_error: std::string::String,
         code_occurence: crate::common::code_occurence::CodeOccurenceWithSerializeDeserialize,
     },
+    #[tvfrr_500_internal_server_error]
     Tls {
         box_dyn_error: std::string::String,
         code_occurence: crate::common::code_occurence::CodeOccurenceWithSerializeDeserialize,
     },
+    #[tvfrr_500_internal_server_error]
     Protocol {
         protocol: std::string::String,
         code_occurence: crate::common::code_occurence::CodeOccurenceWithSerializeDeserialize,
     },
+    #[tvfrr_404_not_found]
     RowNotFound {
         row_not_found: std::string::String,
         code_occurence: crate::common::code_occurence::CodeOccurenceWithSerializeDeserialize,
     },
+    #[tvfrr_400_bad_request]
     TypeNotFound {
         type_not_found: std::string::String,
         code_occurence: crate::common::code_occurence::CodeOccurenceWithSerializeDeserialize,
     },
+    #[tvfrr_500_internal_server_error]
     ColumnIndexOutOfBounds {
         column_index_out_of_bounds: usize,
         len: usize,
         code_occurence: crate::common::code_occurence::CodeOccurenceWithSerializeDeserialize,
     },
+    #[tvfrr_400_bad_request]
     ColumnNotFound {
         column_not_found: std::string::String,
         code_occurence: crate::common::code_occurence::CodeOccurenceWithSerializeDeserialize,
     },
+    #[tvfrr_500_internal_server_error]
     ColumnDecode {
         column_decode_index: std::string::String,
         source_handle: std::string::String,
         code_occurence: crate::common::code_occurence::CodeOccurenceWithSerializeDeserialize,
     },
+    #[tvfrr_500_internal_server_error]
     Decode {
         decode_box_dyn_error: std::string::String,
         code_occurence: crate::common::code_occurence::CodeOccurenceWithSerializeDeserialize,
     },
+    #[tvfrr_408_request_timeout]
     PoolTimedOut {
         pool_timed_out: std::string::String,
         code_occurence: crate::common::code_occurence::CodeOccurenceWithSerializeDeserialize,
     },
+    #[tvfrr_500_internal_server_error]
     PoolClosed {
         pool_closed: std::string::String,
         code_occurence: crate::common::code_occurence::CodeOccurenceWithSerializeDeserialize,
     },
+    #[tvfrr_500_internal_server_error]
     WorkerCrashed {
         worker_crashed: std::string::String,
         code_occurence: crate::common::code_occurence::CodeOccurenceWithSerializeDeserialize,
     },
+    #[tvfrr_500_internal_server_error]
     Migrate {
         migrate: std::string::String,
         code_occurence: crate::common::code_occurence::CodeOccurenceWithSerializeDeserialize,
     },
+    #[tvfrr_500_internal_server_error]
     UnexpectedCase {
         unexpected_case: std::string::String,
         code_occurence: crate::common::code_occurence::CodeOccurenceWithSerializeDeserialize,
