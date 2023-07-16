d10539f6e9076d8da6731c42510d7b532b54b0ad
kuqmua <serezhashmakov@outlook.com>
2023-07-16 06:28:35 +0300

    remove one case futures::executor::block_on

--- src/repositories_types/tufa_server/routes/api/cats/get/mod.rs
+++ src/repositories_types/tufa_server/routes/api/cats/get/mod.rs
@@ -1,3 +1,5 @@
+type GetDesirableType = Vec<crate::repositories_types::tufa_server::routes::api::cats::Cat>;
+
 #[derive(serde::Deserialize)]
 pub struct GetQueryParameters {
     pub limit: Option<crate::server::postgres::rows_per_table::RowsPerTable>,
@@ -43,8 +45,6 @@ impl crate::common::url_encode::UrlEncode for GetQueryParameters {
     }
 }
 
-pub type GetDesirableType = Vec<crate::repositories_types::tufa_server::routes::api::cats::Cat>;
-
 #[derive(
     Debug,
     thiserror::Error,
@@ -53,7 +53,7 @@ pub type GetDesirableType = Vec<crate::repositories_types::tufa_server::routes::
     type_variants_from_reqwest_response::TypeVariantsFromReqwestResponse,
 )]
 #[type_variants_from_reqwest_response::type_variants_from_reqwest_response_attribute(
-    crate::repositories_types::tufa_server::routes::api::cats::get::GetDesirableType,
+    GetDesirableType,
     tvfrr_200_ok
 )]
 pub enum TryGet<'a> {
@@ -183,12 +183,9 @@ pub enum TryGet<'a> {
 
 pub async fn try_get<'a>(
     server_location: &str,
-    query_parameters: crate::repositories_types::tufa_server::routes::api::cats::get::GetQueryParameters,
-) -> Result<
-    crate::repositories_types::tufa_server::routes::api::cats::get::GetDesirableType,
-    TryGetErrorNamed<'a>,
-> {
-    get_extraction_logic(
+    query_parameters: GetQueryParameters,
+) -> Result<GetDesirableType, TryGetErrorNamed<'a>> {
+    extraction_logic(
         reqwest::Client::new()
             .get(&format!(
                 "{server_location}/api/{}/{}",
