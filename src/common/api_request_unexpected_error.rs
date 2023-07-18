969810b09dfbec0ec1337a7d9c44721f5f2b55bb
kuqmua <serezhashmakov@outlook.com>
2023-07-18 22:27:07 +0300

    add reqwest::Error to FailedToGetResponseText

--- src/common/api_request_unexpected_error.rs
+++ src/common/api_request_unexpected_error.rs
@@ -6,6 +6,7 @@ pub enum ApiRequestUnexpectedError {
         response_text: std::string::String,
     },
     FailedToGetResponseText {
+        reqwest: reqwest::Error,
         status_code: http::StatusCode,
         headers: reqwest::header::HeaderMap,
     },
@@ -16,20 +17,3 @@ pub enum ApiRequestUnexpectedError {
         response_text: std::string::String,
     },
 }
-
-// match response.text().await {
-//     Ok(response_text) => match serde_json :: from_str :: <
-// TryGetResponseVariantsTvfrr200Ok > (& response_text)
-// {
-//     Ok(value) => Ok(TryGetResponseVariants :: from(value)), Err(e) =>
-//     Err(crate :: common :: api_request_unexpected_error ::
-//     ApiRequestUnexpectedError :: DeserializeBody
-//     { serde : e, status_code, headers, response_text }),
-// },
-//     Err(e) => Err(
-//         crate::common::api_request_unexpected_error::ApiRequestUnexpectedError::FailedToGetResponseText {
-//             status_code,
-//             headers,
-//         }
-//     ),
-// }
