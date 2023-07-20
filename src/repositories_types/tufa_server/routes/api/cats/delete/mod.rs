14d9023f56e650e598b178eca412e35e72945b36
kuqmua <serezhashmakov@outlook.com>
2023-07-20 17:47:03 +0300

    try_delete works

--- src/repositories_types/tufa_server/routes/api/cats/delete/mod.rs
+++ src/repositories_types/tufa_server/routes/api/cats/delete/mod.rs
@@ -1,6 +1,3 @@
-pub mod request;
-pub mod route;
-
 #[derive(serde::Deserialize)]
 pub struct DeleteQueryParameters {
     pub name: Option<String>,
@@ -23,5 +20,220 @@ impl crate::common::url_encode::UrlEncode for DeleteQueryParameters {
             true => String::from(""),
             false => format!("?{parameters}"),
         }
+    }
+}
+
+// pub async fn try_delete<'a>(
+//     server_location: std::string::String,
+//     query_parameters: crate::repositories_types::tufa_server::routes::api::cats::delete::DeleteQueryParameters,
+// ) -> Result<(), TryDeleteErrorNamed<'a>> {
+//     let url = format!(
+//         "{server_location}/api/{}/{}",
+//         crate::repositories_types::tufa_server::routes::api::cats::CATS,
+//         crate::common::url_encode::UrlEncode::url_encode(&query_parameters)
+//     );
+//     match reqwest::Client::new()
+//         .delete(&url)
+//         .header(
+//             crate::common::git::project_git_info::PROJECT_COMMIT,
+//             crate::global_variables::compile_time::project_git_info::PROJECT_GIT_INFO
+//                 .project_commit,
+//         )
+//         .send()
+//         .await
+//     {
+//         Ok(r) => {
+//             let response_status = r.status();
+//             match response_status == http::StatusCode::OK {
+//                 true => Ok(()),
+//                 false => Err(TryDeleteErrorNamed::UnexpectedStatusCode {
+//                     unexpected_status_code: response_status,
+//                     code_occurence: crate::code_occurence_tufa_common!(),
+//                 }),
+//             }
+//         }
+//         Err(e) => Err(TryDeleteErrorNamed::Reqwest {
+//             reqwest: e,
+//             code_occurence: crate::code_occurence_tufa_common!(),
+//         }),
+//     }
+// }
+
+#[derive(
+    Debug,
+    thiserror::Error,
+    error_occurence::ErrorOccurence,
+    from_sqlx_postgres_error::FromSqlxPostgresError,
+    type_variants_from_reqwest_response::TypeVariantsFromReqwestResponse,
+)]
+#[type_variants_from_reqwest_response::type_variants_from_reqwest_response_attribute(
+    (),
+    tvfrr_200_ok
+)]
+pub enum TryDelete<'a> {
+    #[tvfrr_400_bad_request]
+    ProjectCommitExtractorNotEqual {
+        #[eo_display_with_serialize_deserialize]
+        project_commit_not_equal: &'a str,
+        #[eo_display_with_serialize_deserialize]
+        project_commit_to_use: std::string::String,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    #[tvfrr_400_bad_request]
+    ProjectCommitExtractorToStrConversion {
+        #[eo_display]
+        project_commit_to_str_conversion: http::header::ToStrError,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    #[tvfrr_400_bad_request]
+    NoProjectCommitExtractorHeader {
+        #[eo_display_with_serialize_deserialize]
+        no_project_commit_header: &'a str,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    //
+    #[tvfrr_500_internal_server_error]
+    Configuration {
+        #[eo_display_with_serialize_deserialize]
+        configuration_box_dyn_error: std::string::String,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    #[tvfrr_500_internal_server_error]
+    Database {
+        #[eo_display_with_serialize_deserialize]
+        box_dyn_database_error: std::string::String,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    #[tvfrr_500_internal_server_error]
+    Io {
+        #[eo_display]
+        io_error: std::io::Error,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    #[tvfrr_500_internal_server_error]
+    Tls {
+        #[eo_display_with_serialize_deserialize]
+        box_dyn_error: std::string::String,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    #[tvfrr_500_internal_server_error]
+    Protocol {
+        #[eo_display_with_serialize_deserialize]
+        protocol: std::string::String,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    #[tvfrr_404_not_found]
+    RowNotFound {
+        #[eo_display_with_serialize_deserialize]
+        row_not_found: std::string::String,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    #[tvfrr_400_bad_request]
+    TypeNotFound {
+        #[eo_display_with_serialize_deserialize]
+        type_not_found: std::string::String,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    #[tvfrr_500_internal_server_error]
+    ColumnIndexOutOfBounds {
+        #[eo_display_with_serialize_deserialize]
+        column_index_out_of_bounds: usize,
+        #[eo_display_with_serialize_deserialize]
+        len: usize,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    #[tvfrr_400_bad_request]
+    ColumnNotFound {
+        #[eo_display_with_serialize_deserialize]
+        column_not_found: std::string::String,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    #[tvfrr_500_internal_server_error]
+    ColumnDecode {
+        #[eo_display_with_serialize_deserialize]
+        column_decode_index: std::string::String,
+        #[eo_display_with_serialize_deserialize]
+        source_handle: std::string::String,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    #[tvfrr_500_internal_server_error]
+    Decode {
+        #[eo_display_with_serialize_deserialize]
+        decode_box_dyn_error: std::string::String,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    #[tvfrr_408_request_timeout]
+    PoolTimedOut {
+        #[eo_display_with_serialize_deserialize]
+        pool_timed_out: std::string::String,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    #[tvfrr_500_internal_server_error]
+    PoolClosed {
+        #[eo_display_with_serialize_deserialize]
+        pool_closed: std::string::String,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    #[tvfrr_500_internal_server_error]
+    WorkerCrashed {
+        #[eo_display_with_serialize_deserialize]
+        worker_crashed: std::string::String,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    #[tvfrr_500_internal_server_error]
+    Migrate {
+        #[eo_display]
+        migrate: sqlx::migrate::MigrateError,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    //#[non_exhaustive] case
+    #[tvfrr_500_internal_server_error]
+    UnexpectedCase {
+        #[eo_display_with_serialize_deserialize]
+        unexpected_case: std::string::String,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+    #[tvfrr_400_bad_request]
+    NoParameters {
+        #[eo_display_with_serialize_deserialize]
+        no_parameters: std::string::String,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+}
+
+#[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
+pub enum TryDeleteErrorNamed<'a> {
+    RequestError {
+        #[eo_error_occurence]
+        request_error: TryDeleteRequestError<'a>,
+        code_occurence: crate::common::code_occurence::CodeOccurence<'a>,
+    },
+}
+
+pub async fn try_delete<'a>(
+    server_location: &str,
+    query_parameters: crate::repositories_types::tufa_server::routes::api::cats::delete::DeleteQueryParameters,
+) -> Result<(), TryDeleteErrorNamed<'a>> {
+    match tvfrr_extraction_logic(
+        reqwest::Client::new()
+            .delete(&format!(
+                "{server_location}/api/{}/{}",
+                crate::repositories_types::tufa_server::routes::api::cats::CATS,
+                crate::common::url_encode::UrlEncode::url_encode(&query_parameters)
+            ))
+            .header(
+                crate::common::git::project_git_info::PROJECT_COMMIT,
+                crate::global_variables::compile_time::project_git_info::PROJECT_GIT_INFO
+                    .project_commit,
+            )
+            .send(),
+    )
+    .await
+    {
+        Ok(value) => Ok(value),
+        Err(e) => Err(TryDeleteErrorNamed::RequestError {
+            request_error: e,
+            code_occurence: crate::code_occurence_tufa_common!(),
+        }),
     }
 }
