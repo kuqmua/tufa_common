419a4ddfd3f8fe4485ad46dd3580c987f627e48b
kuqmua <serezhashmakov@outlook.com>
2023-07-22 22:15:23 +0300

    find out how to do nesting

---
+++ src/server/routes/common_routes.rs
@@ -0,0 +1,9 @@
+// pub fn common_routes() -> axum::Router
+// // <
+// //     std::sync::Arc<
+// //         crate::repositories_types::tufa_server::try_build_actix_web_dev_server::AppInfo<'static>,
+// //     >,
+// // >
+// {
+//     axum::Router::new()
+// }
--- src/server/routes/mod.rs
+++ src/server/routes/mod.rs
@@ -1,2 +1,3 @@
+pub mod common_routes;
 pub mod git_info;
 pub mod helpers;
