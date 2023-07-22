cbb9725899863e5e253017df67363b2436b9fc94
kuqmua <serezhashmakov@outlook.com>
2023-07-22 19:17:00 +0300

    git info

--- src/server/routes/git_info.rs
+++ src/server/routes/git_info.rs
@@ -4,6 +4,14 @@ pub struct GitInfo {
     repository_commit: std::string::String,
 }
 
+impl axum::response::IntoResponse for GitInfo {
+    fn into_response(self) -> axum::response::Response {
+        let mut res = axum::Json(self).into_response();
+        *res.status_mut() = http::StatusCode::OK;
+        res
+    }
+}
+
 #[actix_web::get("/git_info")]
 pub async fn git_info(
     app_info: actix_web::web::Data<
@@ -20,4 +28,23 @@ pub async fn git_info(
             app_info.repository_git_info.get_git_commit_link()
         },
     }))
+}
+
+pub async fn git_info_axum(
+    axum::extract::State(app_info): axum::extract::State<
+        std::sync::Arc<
+            crate::repositories_types::tufa_server::try_build_actix_web_dev_server::AppInfo<'_>,
+        >,
+    >,
+) -> GitInfo {
+    GitInfo {
+        project_commit:
+            crate::common::git::get_git_commit_link::GetGitCommitLink::get_git_commit_link(
+                app_info.project_git_info,
+            ),
+        repository_commit:
+            crate::common::git::get_git_commit_link::GetGitCommitLink::get_git_commit_link(
+                app_info.repository_git_info,
+            ),
+    }
 }
