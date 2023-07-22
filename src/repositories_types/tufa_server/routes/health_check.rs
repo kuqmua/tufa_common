987fc3985827269d4f1dc19737a40f2c35726765
kuqmua <serezhashmakov@outlook.com>
2023-07-22 19:42:00 +0300

    health_check

--- src/repositories_types/tufa_server/routes/health_check.rs
+++ src/repositories_types/tufa_server/routes/health_check.rs
@@ -2,3 +2,8 @@ pub async fn health_check() -> actix_web::HttpResponse {
     println!("health_check");
     actix_web::HttpResponse::Ok().finish()
 }
+
+pub async fn health_check_axum() -> axum::http::StatusCode {
+    println!("health_check");
+    axum::http::StatusCode::OK
+}
