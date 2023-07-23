959228aac4ea3b720c8c2429e13dee4130ce1ff5
kuqmua <serezhashmakov@outlook.com>
2023-07-23 12:20:53 +0300

    tokio feature "rt-multi-thread"

--- src/server/routes/common_routes.rs
+++ src/server/routes/common_routes.rs
@@ -7,3 +7,15 @@
 // {
 //     axum::Router::new()
 // }
+
+// pub fn health_check_route() -> axum::Router<
+//     std::sync::Arc<
+//         crate::repositories_types::tufa_server::try_build_actix_web_dev_server::AppInfo<'_>,
+//     >,
+//     _, // Router<Arc<AppInfo<'_>>, _>
+// > {
+//     axum::Router::new().route(
+//         "/health_check",
+//         axum::routing::get(crate::repositories_types::tufa_server::routes::health_check_axum),
+//     )
+// }
