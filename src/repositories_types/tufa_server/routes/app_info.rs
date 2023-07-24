f515524d0ca53103c1ad04d7e83f13fed3c0df32
kuqmua <serezhashmakov@outlook.com>
2023-07-24 11:20:43 +0300

    tufa_common::repositories_types::tufa_server::routes::app_info::DynArcGetAppInfoSendSync usage

--- src/repositories_types/tufa_server/routes/app_info.rs
+++ src/repositories_types/tufa_server/routes/app_info.rs
@@ -1,4 +1,4 @@
-pub type DynArcGetPostgresPoolSendSync = std::sync::Arc<
+pub type DynArcGetAppInfoSendSync = std::sync::Arc<
     dyn crate::repositories_types::tufa_server::try_build_actix_web_dev_server::GetAppInfo
         + Send
         + Sync,
