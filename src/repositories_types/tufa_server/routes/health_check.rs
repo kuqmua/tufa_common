3e54a2c5eff486ddb020911a2cdcf4082b7dde17
kuqmua <serezhashmakov@outlook.com>
2023-07-26 18:37:44 +0300

    health_check route

--- src/repositories_types/tufa_server/routes/health_check.rs
+++ src/repositories_types/tufa_server/routes/health_check.rs
@@ -7,3 +7,7 @@ pub async fn health_check_axum() -> axum::http::StatusCode {
     println!("health_check");
     axum::http::StatusCode::OK
 }
+
+pub fn health_check_route() -> axum::Router {
+    axum::Router::new().route("/health_check", axum::routing::get(health_check_axum))
+}
