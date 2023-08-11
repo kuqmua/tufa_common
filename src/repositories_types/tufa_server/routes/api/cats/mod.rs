66a9e07f1f844d7e8859bcedabfd5231cc907051
kuqmua <serezhashmakov@outlook.com>
2023-08-11 09:27:52 +0300

    deserialize_bigserial_ids cleaning

--- src/repositories_types/tufa_server/routes/api/cats/mod.rs
+++ src/repositories_types/tufa_server/routes/api/cats/mod.rs
@@ -219,88 +219,87 @@ pub struct CatToPut {
 }
 
 #[derive(Debug, serde::Serialize, serde::Deserialize, Clone)]
-pub struct BigserialIds (#[serde(deserialize_with = "deserialize_bigserial_ids")] pub Vec<crate::server::postgres::bigserial::Bigserial>);
+pub struct BigserialIds(
+    #[serde(deserialize_with = "deserialize_bigserial_ids")]
+    pub  Vec<crate::server::postgres::bigserial::Bigserial>,
+);
 
-fn deserialize_bigserial_ids<'de, D>(deserializer: D) -> Result<Vec<crate::server::postgres::bigserial::Bigserial>, D::Error>
+fn deserialize_bigserial_ids<'de, D>(
+    deserializer: D,
+) -> Result<Vec<crate::server::postgres::bigserial::Bigserial>, D::Error>
 where
     D: serde::de::Deserializer<'de>,
 {
-    use serde::Deserialize;
-    // #[derive(Debug, PartialEq, Deserialize)]
-    // struct DeserializeBigserialIds(Option<std::string::String>);
-    let str_sequence = String::deserialize(deserializer)?;//Result<i64, std::num::ParseIntError>
-    // let str_sequence = if let Some(str_handle) = str_sequences.0 {
-    //     str_handle
-    // }
-    // else {
-    //     // return Err(serde::de::Error::custom(
-    //     // &format!(
-    //     //         "invalid type: Postgresql Bigserial 000, expected Postgresql Bigserial as rust i64, there 1 <= *your value* <= 9223372036854775807(only positive part of rust i64)")
-    //     //     )
-    //     // );
-    //     return Ok(None);
-    // };
-    println!("str_sequence, {str_sequence}");
-    let f: Vec<std::string::String> = str_sequence
-        .split(',')
-        .map(|item| 
-        //     {
-        //     let f: Result<i64, std::num::ParseIntError> = std::str::FromStr::from_str(item);
-        //     f
-        // }
-        item.to_string()
-        )
-        .collect();
-    let g: Vec<Result<i64, std::num::ParseIntError>> = f.into_iter().map(|h|{
-        let f: Result<i64, std::num::ParseIntError> = std::str::FromStr::from_str(&h);
-        f
-    }).collect();
-    let mut v = Vec::with_capacity(g.len());
-    for i in g {
-        match i {
-            Ok(p) => {
-                match crate::server::postgres::bigserial::Bigserial::try_from_i64(p) {
-                    Ok(o) => {
-                        v.push(o);
-                    },
-                    Err(e) => {
-//
-                        return Err(serde::de::Error::custom(
-                            &format!(
-                                "invalid type: Postgresql Bigserial 111 `{e}`, expected Postgresql Bigserial as rust i64, there 1 <= *your value* <= 9223372036854775807(only positive part of rust i64)")
-                            )
-                        );
-//
-                    },
+    let (vec_values, mut stringified_parse_fails, mut stringified_bigserial_fails) = {
+        use serde::Deserialize;
+        String::deserialize(deserializer)?
+    }
+    .split(',')
+    .fold(
+        (
+            Vec::new(),
+            std::string::String::from(""),
+            std::string::String::from(""),
+        ),
+        |mut acc, element| {
+            match element.parse::<i64>() {
+                Ok(value) => {
+                    match crate::server::postgres::bigserial::Bigserial::try_from_i64(value) {
+                        Ok(bigserial) => {
+                            acc.0.push(bigserial);
+                        }
+                        Err(_) => {
+                            acc.1.push_str(&format!("{element},"));
+                        }
+                    }
                 }
-            },
-            Err(e) => {
-                return Err(serde::de::Error::custom(
-                    &format!(
-                        "invalid type: Postgresql Bigserial 222 `{e}`, expected Postgresql Bigserial as rust i64, there 1 <= *your value* <= 9223372036854775807(only positive part of rust i64)")
-                    )
-                );
-            },
+                Err(_) => {
+                    acc.1.push_str(&format!("{element},"));
+                }
+            }
+            acc
+        },
+    );
+    let default_message = "invalid type (expected array Postgresql Bigserial as rust Vec<i64>):";
+    let stringified_parse_fails_message = "failed to parse each element into rust i64";
+    let stringified_bigserial_fails_message = "failed to convert each element into Postgresql Bigserial - must be in range 1 <= *your value* <= 9223372036854775807(only positive part of rust i64)";
+    match (
+        stringified_parse_fails.is_empty(),
+        stringified_bigserial_fails.is_empty(),
+    ) {
+        (true, true) => Ok(vec_values),
+        (true, false) => {
+            stringified_bigserial_fails.pop();
+            Err(serde::de::Error::custom(
+                &format!(
+                    "{default_message} `{stringified_bigserial_fails}`, {stringified_bigserial_fails_message}")
+                )
+            )
+        }
+        (false, true) => {
+            stringified_parse_fails.pop();
+            Err(serde::de::Error::custom(&format!(
+                "{default_message} `{stringified_parse_fails}`, {stringified_parse_fails_message}"
+            )))
+        }
+        (false, false) => {
+            stringified_parse_fails.pop();
+            stringified_bigserial_fails.pop();
+            Err(serde::de::Error::custom(
+                &format!(
+                    "{default_message} 1) `{stringified_parse_fails}`, {stringified_parse_fails_message}. 2) `{stringified_bigserial_fails}`, {stringified_bigserial_fails_message}")
+                )
+            )
         }
     }
-
-    Ok(v)
-    // match possible_bigserial.is_positive() {
-    //     true => Ok(possible_bigserial),
-    //     false => Err(
-    //         serde::de::Error::custom(&format!(
-    //             "invalid type: Postgresql Bigserial `{possible_bigserial}`, expected Postgresql Bigserial as rust i64, there 1 <= *your value* <= 9223372036854775807(only positive part of rust i64)"
-    //         )),
-    //     )
-    // }
 }
 //
 #[derive(Debug, serde::Deserialize)]
 pub struct GetQueryParameters {
     pub limit: crate::server::postgres::rows_per_table::RowsPerTable,
     // #[serde(deserialize_with = "deserialize_bigserial_ids")]
-    pub id: Option<BigserialIds>,//BigserialIds, //Option<Vec<crate::server::postgres::bigserial::Bigserial>>
-    pub name: Option<std::string::String>,
+    pub id: Option<BigserialIds>, //BigserialIds, //Option<Vec<crate::server::postgres::bigserial::Bigserial>>
+    pub name: Option<Vec<std::string::String>>,
     pub color: Option<std::string::String>,
     pub select: Option<GetSelect>,
 }
@@ -320,32 +319,42 @@ impl crate::common::url_encode::UrlEncode for GetQueryParameters {
             }
         }
         if let Some(id) = &self.id {
-        //     //
-        let ids_stringified = {
-            let mut ids_stringified =
-                id.0.iter().fold(String::from(""), |mut acc, bigserial| {
-                    acc.push_str(&format!("{bigserial},"));
+            let ids_stringified = {
+                let mut ids_stringified =
+                    id.0.iter().fold(String::from(""), |mut acc, bigserial| {
+                        acc.push_str(&format!("{bigserial},"));
+                        acc
+                    });
+                if let false = ids_stringified.is_empty() {
+                    ids_stringified.pop();
+                }
+                ids_stringified
+            };
+            let query_parameter_handle = format!("id={}", urlencoding::encode(&ids_stringified));
+            match stringified_query_parameters.len() > 1 {
+                true => {
+                    stringified_query_parameters.push_str(&query_parameter_handle);
+                }
+                false => {
+                    stringified_query_parameters.push_str(&format!("&{query_parameter_handle}"));
+                }
+            }
+        }
+        if let Some(names) = &self.name {
+            //
+            let names_stringified = {
+                let mut names_stringified = names.iter().fold(String::from(""), |mut acc, name| {
+                    acc.push_str(&format!("{name},"));
                     acc
                 });
-            if let false = ids_stringified.is_empty() {
-                ids_stringified.pop();
-            }
-            ids_stringified
-        };
-
-        //
-        let query_parameter_handle = format!("id={}", urlencoding::encode(&ids_stringified));
-        match stringified_query_parameters.len() > 1 {
-            true => {
-                stringified_query_parameters.push_str(&query_parameter_handle);
-            }
-            false => {
-                stringified_query_parameters.push_str(&format!("&{query_parameter_handle}"));
-            }
-        }
-        }
-        if let Some(name) = &self.name {
-            let query_parameter_handle = format!("name={}", urlencoding::encode(name));
+                if let false = names_stringified.is_empty() {
+                    names_stringified.pop();
+                }
+                names_stringified
+            };
+            //
+            let query_parameter_handle =
+                format!("name={}", urlencoding::encode(&names_stringified));
             match stringified_query_parameters.len() > 1 {
                 true => {
                     stringified_query_parameters.push_str(&query_parameter_handle);
@@ -392,9 +401,9 @@ impl crate::server::routes::helpers::bind_sqlx_query::BindSqlxQuery for GetQuery
         >,
     ) -> sqlx::query::QueryAs<'q, sqlx::Postgres, TableScheme, sqlx::postgres::PgArguments> {
         if let Some(id) = self.id {
-        for id_handle in id.0 {
-            query = query.bind(id_handle.into_inner());
-        }
+            for id_handle in id.0 {
+                query = query.bind(id_handle.into_inner());
+            }
         }
         if let Some(name) = self.name {
             query = query.bind(name);
