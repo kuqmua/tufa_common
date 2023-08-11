f3f12152129554a2f6531c718552a37475a92c4a
kuqmua <serezhashmakov@outlook.com>
2023-08-11 17:03:17 +0300

    next steps

--- src/server/postgres/constants.rs
+++ src/server/postgres/constants.rs
@@ -1,1 +1,7 @@
-
+pub static SELECT_NAME: &str = "SELECT";
+pub static FROM_NAME: &str = "FROM";
+pub static WHERE_NAME: &str = "WHERE";
+pub static AND_NAME: &str = "AND";
+pub static LIMIT_NAME: &str = "LIMIT";
+pub static ANY_NAME: &str = "ANY";
+pub static ARRAY_NAME: &str = "ARRAY";
