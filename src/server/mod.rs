8e615cf4345c6358c06640588d0f782a72b8dbfb
kuqmua <serezhashmakov@outlook.com>
2023-08-16 07:22:40 +0300

    comment out something

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,11 +1,9 @@
 pub mod extractors;
-pub mod file_system;
+// pub mod file_system;
 pub mod middleware;
-pub mod mongo;
+// pub mod mongo;
 pub mod net;
 pub mod postgres;
-pub mod prints;
-pub mod redis;
 pub mod resource;
 pub mod routes;
 pub mod tracing_type;
