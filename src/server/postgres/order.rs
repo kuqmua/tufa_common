97bf729ccf805c3f36a0da07a90752d4738b4985
kuqmua <serezhashmakov@outlook.com>
2023-08-26 13:57:15 +0300

    impl std::fmt::Display + Default for Order

--- src/server/postgres/order.rs
+++ src/server/postgres/order.rs
@@ -1,9 +1,24 @@
-#[derive(Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, strum_macros::Display)]
+#[derive(Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq)]
 pub enum Order {
     #[serde(rename(serialize = "asc", deserialize = "asc"))]
     Asc,
     #[serde(rename(serialize = "desc", deserialize = "desc"))]
     Desc,
+}
+
+impl std::fmt::Display for Order {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Order::Asc => write!(f, "asc"),
+            Order::Desc => write!(f, "desc"),
+        }
+    }
+}
+
+impl Default for Order {
+    fn default() -> Self {
+        Self::Asc
+    }
 }
 
 impl crate::common::url_encode::UrlEncode for Order {
