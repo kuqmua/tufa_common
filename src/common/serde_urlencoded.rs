20238c0993a89b8bd5eae3b7178b84133c76732b
kuqmua <serezhashmakov@outlook.com>
2023-10-24 18:54:55 +0300

    next steps

--- src/common/serde_urlencoded.rs
+++ src/common/serde_urlencoded.rs
@@ -7,3 +7,15 @@ impl SerdeUrlencodedParameter for std::string::String {
         self
     }
 }
+
+impl SerdeUrlencodedParameter for Vec<std::string::String> {
+    fn serde_urlencoded_parameter(self) -> std::string::String {
+        let len = self.len();
+        let mut handle = self.into_iter().fold(std::string::String::default(), |mut acc, element| {
+            acc.push_str(&format!("{element},"));
+            acc
+        });
+        handle.pop();
+        handle
+    }
+}
--- src/repositories_types/tufa_server/routes/api/cats/mod.rs
+++ src/repositories_types/tufa_server/routes/api/cats/mod.rs
@@ -1433,7 +1433,7 @@ pub enum TryReadMany {
     #[tvfrr_400_bad_request]
     NotUniquePrimaryKey {
         #[eo_vec_display_with_serialize_deserialize]
-        not_unique_primary_keys: Vec<i64>,
+        not_unique_primary_keys: Vec<std::string::String>,
         code_occurence: crate::common::code_occurence::CodeOccurence,
     },
     #[tvfrr_400_bad_request]
@@ -1884,4 +1884,3 @@ pub enum TryUpdateMany {
 }
 //////
 // https://learn.microsoft.com/en-us/rest/api/storageservices/table-service-rest-api
-
