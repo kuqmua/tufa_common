15c8423a88fd8b3779439b211b0aab6f1e126f37
kuqmua <serezhashmakov@outlook.com>
2023-10-29 12:50:40 +0300

    next steps

--- src/server/postgres/uuid_wrapper.rs
+++ src/server/postgres/uuid_wrapper.rs
@@ -61,6 +61,43 @@ impl std::convert::TryFrom<PossibleUuidWrapper> for UuidWrapper {
     }
 }
 
+impl crate::server::postgres::bind_query::BindQuery for UuidWrapper {
+    fn try_increment(
+        &self,
+        increment: &mut u64,
+    ) -> Result<(), crate::server::postgres::bind_query::TryGenerateBindIncrementsErrorNamed> {
+        match increment.checked_add(1) {
+            Some(incr) => {
+                *increment = incr;
+                Ok(())
+            }
+            None => Err(crate::server::postgres::bind_query::TryGenerateBindIncrementsErrorNamed::CheckedAdd {
+                checked_add: std::string::String::from("checked_add is None"),
+                code_occurence: crate::code_occurence_tufa_common!(),
+            })
+        }
+    }
+    fn try_generate_bind_increments(&self, increment: &mut u64) -> Result<std::string::String, crate::server::postgres::bind_query::TryGenerateBindIncrementsErrorNamed> {
+        match increment.checked_add(1) {
+            Some(incr) => {
+                *increment = incr;
+                Ok(format!("${increment}"))
+            },
+            None => Err(crate::server::postgres::bind_query::TryGenerateBindIncrementsErrorNamed::CheckedAdd { 
+                checked_add: std::string::String::from("checked_add is None"), 
+                code_occurence: crate::code_occurence_tufa_common!(), 
+            }),
+        }
+    }
+    fn bind_value_to_query(
+        self,
+        mut query: sqlx::query::Query<sqlx::Postgres, sqlx::postgres::PgArguments>,
+    ) -> sqlx::query::Query<sqlx::Postgres, sqlx::postgres::PgArguments> {
+        query = query.bind(self.into_inner());
+        query
+    }
+}
+
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct PossibleUuidWrapper(std::string::String);
 
