5d827fbf886c4d19f9bcc34bee3b141d285e8417
kuqmua <serezhashmakov@outlook.com>
2023-11-01 21:23:42 +0300

    next steps

--- src/repositories_types/tufa_server/routes/api/cats/mod.rs
+++ src/repositories_types/tufa_server/routes/api/cats/mod.rs
@@ -1919,13 +1919,6 @@ pub enum TryUpdateMany {
 }
 //////
 // https://learn.microsoft.com/en-us/rest/api/storageservices/table-service-rest-api
-#[derive(Debug, serde :: Serialize, serde :: Deserialize)]
-pub struct DeleteManyWithBodyPayloadWithSerializeDeserialize {
-    pub id: Option<Vec<crate::server::postgres::uuid_wrapper::PossibleUuidWrapper>>,
-    pub name: Option<Vec<crate::server::postgres::regex_filter::RegexFilter>>,
-    pub color: Option<Vec<crate::server::postgres::regex_filter::RegexFilter>>,
-}
-//
 #[derive(Debug, thiserror::Error, error_occurence::ErrorOccurence)]
 pub enum DeleteManyWithBodyPayloadTryFromDeleteManyWithBodyPayloadWithSerializeDeserializeErrorNamed {
     NotUuid {
