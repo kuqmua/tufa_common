064b4e9147ab1f51da318abdda012eecb5f385e1
kuqmua <serezhashmakov@outlook.com>
2023-11-06 13:09:12 +0300

    next steps

--- src/server/postgres/uuid_wrapper.rs
+++ src/server/postgres/uuid_wrapper.rs
@@ -116,6 +116,12 @@ impl std::fmt::Display for PossibleUuidWrapper {
     }
 }
 
+impl std::convert::From<&str> for PossibleUuidWrapper {
+    fn from(value: &str) -> Self {
+        Self(value.to_string())
+    }
+}
+
 impl std::convert::From<std::string::String> for PossibleUuidWrapper {
     fn from(value: std::string::String) -> Self {
         Self(value)
