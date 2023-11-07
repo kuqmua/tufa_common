55ada02019fca1c5e060c78c81d4ea411ccb7109
kuqmua <serezhashmakov@outlook.com>
2023-11-07 20:56:01 +0300

    next steps

--- src/repositories_types/tufa_server/routes/api/cats/mod.rs
+++ src/repositories_types/tufa_server/routes/api/cats/mod.rs
@@ -37,7 +37,7 @@ pub struct Dog {
     type_variants_from_reqwest_response::TypeVariantsFromReqwestResponse,
 )]
 #[type_variants_from_reqwest_response::type_variants_from_reqwest_response_attribute(
-    (),
+    std::vec::Vec::<crate::server::postgres::uuid_wrapper::PossibleUuidWrapper>,
     tvfrr_201_created
 )]
 pub enum TryCreateMany {
@@ -186,6 +186,12 @@ pub enum TryCreateMany {
     BindQuery {
         #[eo_error_occurence]
         checked_add: crate::server::postgres::bind_query::TryGenerateBindIncrementsErrorNamed,
+        code_occurence: crate::common::code_occurence::CodeOccurence,
+    },
+    #[tvfrr_500_internal_server_error]//todo what status should be there?
+    CreatedButCannotConvertUuidWrapperFromPossibleUuidWrapperInServer {
+        #[eo_display]
+        uuid_wrapper_try_from_possible_uuid_wrapper_in_server: sqlx::Error,
         code_occurence: crate::common::code_occurence::CodeOccurence,
     },
     //#[non_exhaustive] case
@@ -1944,3 +1950,198 @@ pub enum TryUpdateMany {
 }
 //////
 // https://learn.microsoft.com/en-us/rest/api/storageservices/table-service-rest-api
+#[derive(Debug, serde_derive :: Serialize, serde_derive :: Deserialize)]
+pub struct CreateManyParameters {
+    pub payload: Vec<CreateManyPayloadElement>,
+}
+#[derive(Debug, serde :: Serialize, serde :: Deserialize)]
+pub struct CreateManyPayloadElement {
+    pub name: String,
+    pub color: String,
+}
+#[derive(Debug, thiserror :: Error, error_occurence :: ErrorOccurence)]
+pub enum TryCreateManyErrorNamed {
+    RequestError {
+        #[eo_error_occurence]
+        request_error: TryCreateManyRequestError,
+        code_occurence: crate::common::code_occurence::CodeOccurence,
+    },
+    SerdeJsonToString {
+        #[eo_display]
+        serde_json_to_string: serde_json::Error,
+        code_occurence: crate::common::code_occurence::CodeOccurence,
+    },
+    CreatedButCannotConvertUuidWrapperFromPossibleUuidWrapperInClient {
+        #[eo_vec_error_occurence]
+        uuid_wrapper_try_from_possible_uuid_wrapper_in_client: Vec::<crate::server::postgres::uuid_wrapper::UuidWrapperTryFromPossibleUuidWrapperErrorNamed>,
+        code_occurence: crate::common::code_occurence::CodeOccurence,
+    },
+}
+pub async fn try_create_many<'a>(
+    server_location: &str,
+    parameters: CreateManyParameters,
+) -> Result<Vec<crate::server::postgres::uuid_wrapper::UuidWrapper>, TryCreateManyErrorNamed> {
+    let payload = match serde_json::to_string(&parameters.payload) {
+        Ok(value) => value,
+        Err(e) => {
+            return Err(TryCreateManyErrorNamed::SerdeJsonToString {
+                serde_json_to_string: e,
+                code_occurence: crate::code_occurence_tufa_common!(),
+            });
+        }
+    };
+    let url = format!("{}/dogs", server_location,);
+    match tvfrr_extraction_logic_try_create_many(
+        reqwest::Client::new()
+            .post(&url)
+            .header(
+                crate::common::git::project_git_info::PROJECT_COMMIT,
+                crate::global_variables::compile_time::project_git_info::PROJECT_GIT_INFO
+                    .project_commit,
+            )
+            .header(reqwest::header::CONTENT_TYPE, "application/json")
+            .body(payload)
+            .send(),
+    )
+    .await
+    {
+        Ok(value) => {
+            let mut vec_values = Vec::with_capacity(value.len());
+            let mut vec_errors = Vec::with_capacity(value.len());
+            for element in value {
+                match crate::server::postgres::uuid_wrapper::UuidWrapper::try_from(element) {
+                    Ok(value) => {
+                        vec_values.push(value);
+                    },
+                    Err(e) => {
+                        vec_errors.push(e);
+                    }
+                }
+            }
+            if let false = vec_errors.is_empty() {
+                return Err(TryCreateManyErrorNamed::CreatedButCannotConvertUuidWrapperFromPossibleUuidWrapperInClient {
+                    uuid_wrapper_try_from_possible_uuid_wrapper_in_client: vec_errors,
+                    code_occurence: crate::code_occurence_tufa_common!(),
+                });
+            }    
+            Ok(vec_values)
+        },
+        Err(e) => Err(TryCreateManyErrorNamed::RequestError {
+            request_error: e,
+            code_occurence: crate::code_occurence_tufa_common!(),
+        }),
+    }
+}
+pub async fn create_many(
+    app_info_state : axum :: extract :: State < crate ::
+repositories_types :: tufa_server :: routes :: api :: cats ::
+DynArcGetConfigGetPostgresPoolSendSync >,
+    payload_extraction_result: Result<
+        axum::Json<Vec<CreateManyPayloadElement>>,
+        axum::extract::rejection::JsonRejection,
+    >,
+) -> impl axum::response::IntoResponse {
+    let parameters = CreateManyParameters {
+        payload:
+            match crate::server::routes::helpers::json_extractor_error::JsonValueResultExtractor::<
+                Vec<CreateManyPayloadElement>,
+                TryCreateManyResponseVariants,
+            >::try_extract_value(payload_extraction_result, &app_info_state)
+            {
+                Ok(value) => value,
+                Err(err) => {
+                    return err;
+                }
+            },
+    };
+    println!("{:#?}", parameters);
+    {
+        let query_string = {
+            "insert into dogs (name, color) select name, color from unnest($1, $2) as a(name, color) returing id"
+        };
+        println!("{}", query_string);
+        let binded_query = {
+            let mut query = sqlx::query::<sqlx::Postgres>(&query_string);
+            let current_vec_len = parameters.payload.len();
+            let (name_vec, color_vec) = parameters.payload.into_iter().fold(
+                (
+                    Vec::with_capacity(current_vec_len),
+                    Vec::with_capacity(current_vec_len),
+                ),
+                |mut acc, element| {
+                    acc.0.push(element.name);
+                    acc.1.push(element.color);
+                    acc
+                },
+            );
+            query = query.bind(name_vec);
+            query = query.bind(color_vec);
+            query
+        };
+        let mut pool_connection = match app_info_state.get_postgres_pool().acquire().await {
+            Ok(value) => value,
+            Err(e) => {
+                let error = TryCreateMany::from(e);
+                crate::common::error_logs_logic::error_log::ErrorLog::error_log(
+                    &error,
+                    app_info_state.as_ref(),
+                );
+                return TryCreateManyResponseVariants::from(error);
+            }
+        };
+        let pg_connection = match sqlx::Acquire::acquire(&mut pool_connection).await {
+            Ok(value) => value,
+            Err(e) => {
+                let error = TryCreateMany::from(e);
+                crate::common::error_logs_logic::error_log::ErrorLog::error_log(
+                    &error,
+                    app_info_state.as_ref(),
+                );
+                return TryCreateManyResponseVariants::from(error);
+            }
+        };
+        let mut rows = binded_query.fetch(pg_connection.as_mut());
+        let mut vec_values = Vec::new();
+        while let Some(row) = {
+            match {
+                use futures::TryStreamExt;
+                rows.try_next()
+            }
+            .await
+            {
+                Ok(value) => value,
+                Err(e) => {
+                    let error = TryCreateMany::from(e);
+                    crate::common::error_logs_logic::error_log::ErrorLog::error_log(
+                        &error,
+                        app_info_state.as_ref(),
+                    );
+                    return TryCreateManyResponseVariants::from(error);
+                }
+            }
+        } {
+            match {
+                use sqlx::Row;
+                row.try_get::<sqlx::types::Uuid, &str>("id")
+            } {
+                Ok(value) => {
+                    vec_values.push(crate::server::postgres::uuid_wrapper::PossibleUuidWrapper::from(value));
+                },
+                Err(e) => {
+                    let error = TryCreateMany::CreatedButCannotConvertUuidWrapperFromPossibleUuidWrapperInServer
+                    {
+                        uuid_wrapper_try_from_possible_uuid_wrapper_in_server : e,
+                        code_occurence : crate :: code_occurence_tufa_common! (),
+                    } ;
+                    crate::common::error_logs_logic::error_log::ErrorLog::error_log(
+                        &error,
+                        app_info_state.as_ref(),
+                    );
+                    return TryCreateManyResponseVariants::from(error);
+                }
+            }
+        }
+        println!("{vec_values:#?}");
+        TryCreateManyResponseVariants::Desirable(vec_values)
+    }
+}
