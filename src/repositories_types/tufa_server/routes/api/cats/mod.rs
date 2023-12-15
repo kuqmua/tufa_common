5be8cbdc13183390f1df47cc384b3b160a7f80a6
kuqmua <serezhashmakov@outlook.com>
2023-12-16 01:49:36 +0300

    next steps

--- src/repositories_types/tufa_server/routes/api/cats/mod.rs
+++ src/repositories_types/tufa_server/routes/api/cats/mod.rs
@@ -18,6 +18,9 @@ pub type DynArcGetConfigGetPostgresPoolSendSync = std::sync::Arc<
     Debug,
     generate_postgresql_crud::GeneratePostgresqlCrud,
 )]
+#[generate_postgresql_crud::generate_postgresql_crud_additional_http_status_codes_error_variants(
+    something_test
+)]
 pub struct Dog {
     #[generate_postgresql_crud_primary_key]
     pub id: sqlx::types::Uuid, //todo make it UuidWrapper todo - if using js JSON.parse() - must be two variants - for usage and deserialization - coz json number type capacity less than i64::MAX
@@ -25,31 +28,4 @@ pub struct Dog {
     pub name: std::string::String,
     #[generate_postgresql_crud_varchar]
     pub color: std::string::String,
-}
-
-//
-#[derive(
-    Debug,
-    thiserror::Error,
-    error_occurence::ErrorOccurence,
-    // from_sqlx_postgres_error::FromSqlxPostgresError,
-    type_variants_from_reqwest_response::TypeVariantsFromReqwestResponse,
-)]
-#[type_variants_from_reqwest_response::type_variants_from_reqwest_response_attribute(
-    std::vec::Vec::<crate::server::postgres::uuid_wrapper::PossibleUuidWrapper>,
-    tvfrr_201_created
-)]
-pub enum Kekw {
-    // #[tvfrr_400_bad_request]
-    // FailedToDeserializeQueryString {
-    //     #[eo_display_with_serialize_deserialize]
-    //     failed_to_deserialize_query_string: std::string::String,
-    //     code_occurence: crate::common::code_occurence::CodeOccurence,
-    // },
-    #[tvfrr_400_bad_request]
-    VariantName {
-        #[eo_error_occurence]
-        variant_field_name: crate::server::postgres::bind_query::TryGenerateBindIncrementsErrorNamed,
-        code_occurence: crate::common::code_occurence::CodeOccurence,
-    },
 }
