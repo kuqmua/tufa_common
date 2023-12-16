90a60309c8141bd5b0ece1461867686a05dc299a
kuqmua <serezhashmakov@outlook.com>
2023-12-16 05:41:49 +0300

    progress with proc_macro_attribute

--- src/repositories_types/tufa_server/routes/api/cats/mod.rs
+++ src/repositories_types/tufa_server/routes/api/cats/mod.rs
@@ -17,10 +17,11 @@ pub type DynArcGetConfigGetPostgresPoolSendSync = std::sync::Arc<
 #[derive(
     Debug,
     generate_postgresql_crud::GeneratePostgresqlCrud,
-)]
-#[generate_postgresql_crud::generate_postgresql_crud_additional_http_status_codes_error_variants(
-    vec![
-        crate::server::extractors::project_commit_extractor::ProjectCommitExtractorCheckErrorNamed {
+)]//[u8; sds]
+#[generate_postgresql_crud::generate_postgresql_crud_additional_http_status_codes_error_variants{
+    // vec!
+    // [
+        enum ProjectCommitExtractorCheckErrorNamed {//crate::server::extractors::project_commit_extractor::
             #[tvfrr_400_bad_request]
             ProjectCommitExtractorNotEqual {
                 #[eo_display_with_serialize_deserialize]
@@ -29,18 +30,18 @@ pub type DynArcGetConfigGetPostgresPoolSendSync = std::sync::Arc<
                 project_commit_to_use: std::string::String,
                 code_occurence: crate::common::code_occurence::CodeOccurence,
             },
-            #[tvfrr_400_bad_request]
-            ProjectCommitExtractorToStrConversion {
-                #[eo_display]
-                project_commit_to_str_conversion: http::header::ToStrError,
-                code_occurence: crate::common::code_occurence::CodeOccurence,
-            },
-            #[tvfrr_400_bad_request]
-            NoProjectCommitExtractorHeader {
-                #[eo_display_with_serialize_deserialize]
-                no_project_commit_header: std::string::String,
-                code_occurence: crate::common::code_occurence::CodeOccurence,
-            },
+            // #[tvfrr_400_bad_request]
+            // ProjectCommitExtractorToStrConversion {
+            //     #[eo_display]
+            //     project_commit_to_str_conversion: http::header::ToStrError,
+            //     code_occurence: crate::common::code_occurence::CodeOccurence,
+            // },
+            // #[tvfrr_400_bad_request]
+            // NoProjectCommitExtractorHeader {
+            //     #[eo_display_with_serialize_deserialize]
+            //     no_project_commit_header: std::string::String,
+            //     code_occurence: crate::common::code_occurence::CodeOccurence,
+            // },
         }
         // ,
         // crate::server::extractors::project_commit_extractor::ProjectCommitExtractorCheckErrorNamed {
@@ -52,21 +53,15 @@ pub type DynArcGetConfigGetPostgresPoolSendSync = std::sync::Arc<
         //         project_commit_to_use: std::string::String,
         //         code_occurence: crate::common::code_occurence::CodeOccurence,
         //     },
-        //     #[tvfrr_400_bad_request]
-        //     ProjectCommitExtractorToStrConversion {
-        //         #[eo_display]
-        //         project_commit_to_str_conversion: http::header::ToStrError,
-        //         code_occurence: crate::common::code_occurence::CodeOccurence,
-        //     },
-        //     #[tvfrr_400_bad_request]
-        //     NoProjectCommitExtractorHeader {
-        //         #[eo_display_with_serialize_deserialize]
-        //         no_project_commit_header: std::string::String,
-        //         code_occurence: crate::common::code_occurence::CodeOccurence,
-        //     },
+        //     // #[tvfrr_400_bad_request]
+        //     // ProjectCommitExtractorToStrConversion {
+        //     //     #[eo_display]
+        //     //     project_commit_to_str_conversion: http::header::ToStrError,
+        //     //     code_occurence: crate::common::code_occurence::CodeOccurence,
+        //     // },
         // },
-    ]
-)]
+    // ]
+}]
 pub struct Dog {
     #[generate_postgresql_crud_primary_key]
     pub id: sqlx::types::Uuid, //todo make it UuidWrapper todo - if using js JSON.parse() - must be two variants - for usage and deserialization - coz json number type capacity less than i64::MAX
